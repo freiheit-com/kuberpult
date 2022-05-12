130c770ec3d3128fdb0d88ce16f531b701953e71
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-12 10:13:45 +0200

    update plan

--- infrastructure/scripts/make/plan.sh
+++ infrastructure/scripts/make/plan.sh
@@ -1,1 +1,1 @@
-git diff main --name-only | docker run -i -v $(pwd):/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.1
+echo "$FILES_CHANGED" | docker run -i -v $(pwd):/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.1
