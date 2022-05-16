2dcf6ff8006e0fe713b2e84c905aef5e2bebe963
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-16 14:21:29 +0200

    switch to env instead of files

--- infrastructure/scripts/make/plan.sh
+++ infrastructure/scripts/make/plan.sh
@@ -1,3 +1,4 @@
-echo "changedfileenv"
-echo "$CHANGED_FILES"
-cat .changedFiles  | docker run -i -v $(pwd):/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.2
+echo "Changed files"
+echo "$FILES_CHANGED"
+echo "Running execution-plan"
+echo "$FILES_CHANGED"  | docker run -i -v $(pwd):/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.2
