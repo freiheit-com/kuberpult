cbbab63dd5ad7158ff266524e30f8138cc995211
Mohammed Nisham Kolakkat <104761739+mnishamk-freiheit@users.noreply.github.com>
2022-05-17 18:14:14 +0200

    Update infrastructure/scripts/execution-plan/plan.sh
    Co-authored-by: Dennis Schridde <dennis.schridde@freiheit.com>

--- infrastructure/scripts/execution-plan/plan.sh
+++ infrastructure/scripts/execution-plan/plan.sh
@@ -7,5 +7,4 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 base="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
 head="${2:-$(git rev-parse HEAD)}"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 git diff --diff-filter=ACMRT --name-only "$base" "$head" | docker run -i -v "$(pwd)":/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.2
