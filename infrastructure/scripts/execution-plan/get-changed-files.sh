5a9f84654a69a873f7f1a204fa72ca91547fdde2
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 14:21:17 +0200

    update with common code for pr and branch

--- infrastructure/scripts/execution-plan/get-changed-files.sh
+++ infrastructure/scripts/execution-plan/get-changed-files.sh
@@ -2,13 +2,12 @@
 set -ueo pipefail
 main_branch="main"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
+last_commit_hash="$(git rev-parse HEAD)"
+common_ancestor_hash="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
 
-if [[ "$main_branch" = "$current_branch" ]]; then
-    # In main branch find the diff between current and previous commit only 
-    common_ancestor_hash="HEAD^"
-else
-    # Running for pull request, find the common ancestor and find changes till present
-    common_ancestor_hash="$(git merge-base "${main_branch}" "${current_branch}")"
+if [[ "$common_ancestor_hash" = "$last_commit_hash" ]]; then
+  # This means that the build is happening on local machine on main branch, so build everything from scratch
+  common_ancestor_hash="$(git rev-list --max-parents=0 HEAD)"
 fi
 git diff --diff-filter=ACMRT --name-only "$common_ancestor_hash"
 
