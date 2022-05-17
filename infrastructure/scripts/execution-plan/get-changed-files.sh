b272513e9019b9a6c7344f025a7c9cc0f9c0c12c
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 15:25:26 +0200

    add github sha as well

--- infrastructure/scripts/execution-plan/get-changed-files.sh
+++ infrastructure/scripts/execution-plan/get-changed-files.sh
@@ -2,8 +2,8 @@
 set -ueo pipefail
 main_branch="main"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
-last_commit_hash="$(git rev-parse HEAD)"
 common_ancestor_hash="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
+last_commit_hash="${2:-$(git rev-parse HEAD)}"
 
 if [[ "$common_ancestor_hash" = "$last_commit_hash" ]]; then
   # This means that the build is happening on local machine on main branch, so build everything from scratch
