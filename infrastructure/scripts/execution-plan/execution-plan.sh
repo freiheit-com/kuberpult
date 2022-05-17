2e2c93d52b18a52810e1f66d823792b1c036d4cf
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 17:26:23 +0200

    cr comments

--- infrastructure/scripts/execution-plan/execution-plan.sh
+++ infrastructure/scripts/execution-plan/execution-plan.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-set -ueo pipefail
-docker run -i -v $(pwd):/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.2
+exec docker run -i -v "$(pwd)":/repo eu.gcr.io/freiheit-core/services/execution-plan/execution-plan:0.0.2
