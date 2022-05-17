aeb8e1d866eaf1fc0177eee9435d4380f034e6e0
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 17:13:10 +0200

    Only do pull request pipeline

--- infrastructure/scripts/execution-plan/plan.sh
+++ infrastructure/scripts/execution-plan/plan.sh
@@ -2,4 +2,4 @@
 set -ueo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-"$SCRIPT_DIR"/get-changed-files.sh | "$SCRIPT_DIR"/execution-plan.sh
+"$SCRIPT_DIR"/get-changed-files.sh ${1:-"main"} ${2:-"HEAD"} | "$SCRIPT_DIR"/execution-plan.sh
