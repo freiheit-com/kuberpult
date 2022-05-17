e5c3ed4d9e31c9e07547755caee1ae18f76e0267
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 17:28:58 +0200

    documentation

--- infrastructure/scripts/execution-plan/get-changed-files.sh
+++ infrastructure/scripts/execution-plan/get-changed-files.sh
@@ -7,6 +7,7 @@ last_commit_hash="${2:-$(git rev-parse HEAD)}"
 
 if [[ "$common_ancestor_hash" = "$last_commit_hash" ]]; then
   # This means that the build is happening on local machine on main branch, so build everything from scratch
+  # for github actions, this is onlly called from pull request, and pull request base and head commit sha are passed to $common_ancestor_hash and $last_commit_hash
   git ls-files | grep 'Buildfile$'
 else
   git diff --diff-filter=ACMRT --name-only "$common_ancestor_hash" "$last_commit_hash"
