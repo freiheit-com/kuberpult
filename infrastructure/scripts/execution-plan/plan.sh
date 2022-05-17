47d61db3819cff5d5eae67005d62689ba1a04873
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-05-17 17:37:47 +0200

    Update with cr

--- infrastructure/scripts/execution-plan/plan.sh
+++ infrastructure/scripts/execution-plan/plan.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -ueo pipefail
 
+main_branch="main"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+# When called locally with Makefile, the default parameters are used
+base="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
+head="${2:-$(git rev-parse HEAD)}"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-"$SCRIPT_DIR"/get-changed-files.sh ${1:-"main"} ${2:-"HEAD"} | "$SCRIPT_DIR"/execution-plan.sh
+"$SCRIPT_DIR"/get-changed-files-pr.sh "$base" "$head" | "$SCRIPT_DIR"/execution-plan.sh
