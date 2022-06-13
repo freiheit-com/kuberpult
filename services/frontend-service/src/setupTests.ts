2aaaa342eae7ea2513b5f0199c7ded071175a9df
Dennis Schridde <dennis.schridde@freiheit.com>
2022-06-13 19:15:28 +0200

    Use new setupTests global.nextTick function by Viktor Luft
    Suggested-by: Viktor Luft <viktor.luft@freiheit.com>

--- services/frontend-service/src/setupTests.ts
+++ services/frontend-service/src/setupTests.ts
@@ -17,4 +17,4 @@ Copyright 2021 freiheit.com*/
 import '@testing-library/jest-dom/extend-expect';
 
 // test utility to await all running promises
-global.nextTick = (): Promise<void> => new Promise((resolve) => process.nextTick(resolve));
+global.nextTick = (): Promise<void> => new Promise((resolve) => setTimeout(resolve, 0));
