6cea455d7029aebee30ee45952bfe018a53e741e
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-06-13 14:24:09 +0200

    update exec plan in plan-main

--- infrastructure/scripts/execution-plan/plan-main.sh
+++ infrastructure/scripts/execution-plan/plan-main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ueo pipefail
-find -name Buildfile | docker run -i -v "$(pwd)":/repo eu.gcr.io/freiheit-core/images/execution-plan:2.0-scratch-NG-6 build-main
+find -name Buildfile | docker run -i -v "$(pwd)":/repo eu.gcr.io/freiheit-core/images/execution-plan:2.0-scratch-NG-8 build-main
