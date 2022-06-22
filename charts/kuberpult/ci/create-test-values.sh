67651ee06081cf9e5c6291f3bd8b75a895a74a5e
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-06-22 17:59:53 +0200

    remove test code

--- charts/kuberpult/ci/create-test-values.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-# yaml file override keeps comments and removes whitelines, so lint fails. Removing comments for lint
-grep -o '^[^#]*' "$script_dir"/../values.yaml > "$script_dir"/test-values.yaml
-yq eval-all -i 'select(fileIndex == 0) * select(fileIndex == 1)' "$script_dir"/test-values.yaml "$script_dir"/test-values-override.yaml 
