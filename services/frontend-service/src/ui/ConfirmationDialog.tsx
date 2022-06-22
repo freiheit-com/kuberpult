freiheit-com/kuberpult

commit e4cfa8348dcb97a917daf1d97400a80d1620ed3c

Support per-application ArgoCD sync windows (#180)
Co-authored-by: Sven Urbanski <sven.urbanski@freiheit.com>

services/frontend-service/src/ui/ConfirmationDialog.tsx
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License
 along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright 2021 freiheit.com*/
-import { BatchAction, Lock } from '../api/api';
+import { BatchAction, Environment_Application_ArgoCD_SyncWindow, Lock } from '../api/api';
 import * as React from 'react';
 import { Button, Dialog, DialogTitle, IconButton, Typography, Snackbar, Alert, AlertTitle } from '@material-ui/core';
 import { useCallback, useContext } from 'react';
@@ -28,6 +28,7 @@ import {
     MoveToInboxRounded,
 } from '@material-ui/icons';
 import { ActionsCartContext } from './App';
+import { SyncWindow } from './ReleaseDialog';
 
 enum ActionTypes {
     Deploy,
@@ -95,10 +96,11 @@ export interface ConfirmationDialogProviderProps {
     locks?: [string, Lock][];
     undeployedUpstream?: string;
     fin?: () => void;
+    syncWindows?: Environment_Application_ArgoCD_SyncWindow[];
 }
 
 export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProps) => {
-    const { action, locks, fin, undeployedUpstream } = props;
+    const { action, locks, fin, undeployedUpstream, syncWindows } = props;
     const [openNotify, setOpenNotify] = React.useState(false);
     const [dialogOpen, setDialogOpen] = React.useState(false);
     const { actions, setActions } = useContext(ActionsCartContext);
@@ -182,6 +184,21 @@ export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProp
             <strong>Possible conflict with actions already in cart!</strong>
         </Alert>
     );
+    const syncWindowsMessage =
+        syncWindows && syncWindows.length > 0 ? (
+            <Alert variant="outlined" sx={{ m: 1 }} severity="warning">
+                <AlertTitle>ArgoCD sync windows are active for this application!</AlertTitle>
+                <p>Warning: This can delay deployment.</p>
+                <h3>Sync windows:</h3>
+                <ul>
+                    {syncWindows?.map((w, n) => (
+                        <li key={`${n}:${w}`}>
+                            <SyncWindow w={w} />
+                        </li>
+                    ))}
+                </ul>
+            </Alert>
+        ) : null;
 
     return (
         <>
@@ -211,6 +228,7 @@ export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProp
                 {deployLocks}
                 {undeployedUpstreamMessage}
                 {conflictMessage}
+                {syncWindowsMessage}
                 <span style={{ alignSelf: 'end' }}>
                     <Button onClick={closeDialog}>Cancel</Button>
                     <Button onClick={addAction}>Add anyway</Button>
@@ -252,7 +270,7 @@ export const GetActionDetails = (action: BatchAction): ActionDetails => {
             return {
                 type: ActionTypes.Deploy,
                 name: 'Deploy',
-                dialogTitle: 'Are you sure you want to deploy this version?',
+                dialogTitle: 'Please be aware:',
                 notMessageSuccess:
                     'Version ' +
                     action.action?.deploy.version +

pkg/api/api.proto
@@ -133,12 +133,22 @@ message Environment {
   }
 
   message Application {
+    message ArgoCD {
+      message SyncWindow {
+        string kind = 1; // "allow" or "deny"
+        string schedule = 2; // crontab format
+        string duration = 3; // duration the window is open (or closed)
+      }
+      repeated SyncWindow syncWindows = 1;
+    }
+
     string name = 1;
     uint64 version = 2;
     map<string, Lock> locks = 3;
     uint64 queuedVersion = 4;
     Commit versionCommit = 5;
     bool undeployVersion = 6;
+    ArgoCD argoCD = 7;
   }
 
   string name = 1;

services/cd-service/pkg/argocd/render.go
@@ -60,8 +60,12 @@ func RenderV1Alpha1(gitUrl string, gitBranch string, config config.EnvironmentCo
 	buf := []string{}
 	syncWindows := v1alpha1.SyncWindows{}
 	for _, w := range config.ArgoCd.SyncWindows {
+		apps := []string{"*"}
+		if len(w.Apps) > 0 {
+			apps = w.Apps
+		}
 		syncWindows = append(syncWindows, &v1alpha1.SyncWindow{
-			Applications: []string{"*"},
+			Applications: apps,
 			Clusters:     []string{"*"},
 			Namespaces:   []string{"*"},
 			Schedule:     w.Schedule,

services/cd-service/pkg/argocd/render_test.go
@@ -20,6 +20,8 @@ import (
 	"testing"
 
 	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/argocd/v1alpha1"
+	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/config"
+	"github.com/google/go-cmp/cmp"
 	godebug "github.com/kylelemons/godebug/diff"
 )
 
@@ -101,6 +103,128 @@ spec:
 			}
 			if actualResult != tc.ExpectedResult {
 				t.Fatalf("unexpected argocd manifest:\ndiff:\n%s\n\n", godebug.Diff(tc.ExpectedResult, actualResult))
+			}
+		})
+	}
+}
+
+func TestRenderV1Alpha1(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  config.EnvironmentConfig
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "without sync window",
+			config: config.EnvironmentConfig{
+				ArgoCd: &config.EnvironmentConfigArgoCd{
+					SyncWindows: nil,
+				},
+			},
+			want: `apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: test-env
+spec:
+  description: test-env
+  destinations:
+  - {}
+  sourceRepos:
+  - '*'
+`,
+		},
+		{
+			name: "with sync window without apps",
+			config: config.EnvironmentConfig{
+				ArgoCd: &config.EnvironmentConfigArgoCd{
+					SyncWindows: []config.ArgoCdSyncWindow{
+						{
+							Schedule: "not a valid crontab entry",
+							Duration: "invalid duration",
+							Kind:     "neither deny nor allow",
+							Apps:     nil,
+						},
+					},
+				},
+			},
+			want: `apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: test-env
+spec:
+  description: test-env
+  destinations:
+  - {}
+  sourceRepos:
+  - '*'
+  syncWindows:
+  - applications:
+    - '*'
+    clusters:
+    - '*'
+    duration: invalid duration
+    kind: neither deny nor allow
+    manualSync: true
+    namespaces:
+    - '*'
+    schedule: not a valid crontab entry
+`,
+		},
+		{
+			name: "with sync window with apps",
+			config: config.EnvironmentConfig{
+				ArgoCd: &config.EnvironmentConfigArgoCd{
+					SyncWindows: []config.ArgoCdSyncWindow{
+						{
+							Schedule: "not a valid crontab entry",
+							Duration: "invalid duration",
+							Kind:     "neither deny nor allow",
+							Apps: []string{
+								"app*",
+							},
+						},
+					},
+				},
+			},
+			want: `apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: test-env
+spec:
+  description: test-env
+  destinations:
+  - {}
+  sourceRepos:
+  - '*'
+  syncWindows:
+  - applications:
+    - app*
+    clusters:
+    - '*'
+    duration: invalid duration
+    kind: neither deny nor allow
+    manualSync: true
+    namespaces:
+    - '*'
+    schedule: not a valid crontab entry
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			const (
+				gitUrl    = "https://git.example.com/"
+				gitBranch = "branch-name"
+				env       = "test-env"
+			)
+			got, err := RenderV1Alpha1(gitUrl, gitBranch, tt.config, env, nil)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if d := cmp.Diff(tt.want, string(got)); d != "" {
+				t.Errorf("mismatch: %s", d)
 			}
 		})
 	}

services/cd-service/pkg/config/config.go
@@ -47,9 +47,10 @@ type ArgoCdDestination struct {
 }
 
 type ArgoCdSyncWindow struct {
-	Schedule string `json:"schedule,omitempty"`
-	Duration string `json:"duration,omitempty"`
-	Kind     string `json:"kind,omitempty"`
+	Schedule string   `json:"schedule,omitempty"`
+	Duration string   `json:"duration,omitempty"`
+	Kind     string   `json:"kind,omitempty"`
+	Apps     []string `json:"applications,omitempty"`
 }
 
 type ArgoCdIgnoreDifference struct {

services/cd-service/pkg/service/overview.go
@@ -19,7 +19,9 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
 	"sync/atomic"
 
@@ -139,6 +141,15 @@ func (o *OverviewServiceServer) getOverview(
 								Message: lock.Message,
 								Commit:  lockCommit,
 								LockId:  lockId,
+							}
+						}
+					}
+					if config.ArgoCd != nil {
+						if syncWindows, err := transformSyncWindows(config.ArgoCd.SyncWindows, appName); err != nil {
+							return nil, err
+						} else {
+							app.ArgoCD = &api.Environment_Application_ArgoCD{
+								SyncWindows: syncWindows,
 							}
 						}
 					}
@@ -271,4 +282,22 @@ func transformCommit(commit *git.Commit) *api.Commit {
 		AuthorEmail: author.Email,
 		AuthorTime:  timestamppb.New(author.When),
 	}
+}
+
+func transformSyncWindows(syncWindows []config.ArgoCdSyncWindow, appName string) ([]*api.Environment_Application_ArgoCD_SyncWindow, error) {
+	var envAppSyncWindows []*api.Environment_Application_ArgoCD_SyncWindow
+	for _, syncWindow := range syncWindows {
+		for _, pattern := range syncWindow.Apps {
+			if match, err := filepath.Match(pattern, appName); err != nil {
+				return nil, fmt.Errorf("failed to match app pattern %s of sync window to %s at %s with duration %s: %w", pattern, syncWindow.Kind, syncWindow.Schedule, syncWindow.Duration, err)
+			} else if match {
+				envAppSyncWindows = append(envAppSyncWindows, &api.Environment_Application_ArgoCD_SyncWindow{
+					Kind:     syncWindow.Kind,
+					Schedule: syncWindow.Schedule,
+					Duration: syncWindow.Duration,
+				})
+			}
+		}
+	}
+	return envAppSyncWindows, nil
 }

services/frontend-service/src/ui/ActionsCart/ActionsCart.test.tsx
@@ -20,6 +20,7 @@ import { ActionsCart } from './ActionsCart';
 import { Spy } from 'spy4js';
 import { BatchAction, LockBehavior } from '../../api/api';
 import { ActionsCartContext } from '../App';
+import { mockGetOverviewResponseForActions } from './apiMock';
 
 Spy.mockReactComponents('./CheckoutDialog', 'CheckoutCart');
 const mock_setActions = Spy('setActions');
@@ -29,7 +30,7 @@ describe('Actions Cart', () => {
         const value = { actions: actions, setActions: mock_setActions };
         return (
             <ActionsCartContext.Provider value={value}>
-                <ActionsCart />
+                <ActionsCart overview={mockGetOverviewResponseForActions(actions)} />
             </ActionsCartContext.Provider>
         );
     };

services/frontend-service/src/ui/ActionsCart/ActionsCart.tsx
@@ -15,7 +15,7 @@ along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright 2021 freiheit.com*/
 import * as React from 'react';
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, VFC } from 'react';
 import { useBeforeunload } from 'react-beforeunload';
 import {
     AppBar,
@@ -31,7 +31,7 @@ import {
 
 import { ClearRounded } from '@material-ui/icons';
 import { ActionsCartContext } from '../App';
-import { BatchAction } from '../../api/api';
+import { BatchAction, GetOverviewResponse } from '../../api/api';
 import { GetActionDetails } from '../ConfirmationDialog';
 import { theme } from '../App/styles';
 import { CheckoutCart } from './CheckoutDialog';
@@ -56,7 +56,7 @@ const ActionListItem = (props: { act: BatchAction; index: number }) => {
     );
 };
 
-const ActionsList = () => {
+const ActionsList: VFC<{ overview: GetOverviewResponse }> = ({ overview }) => {
     const { actions } = useContext(ActionsCartContext);
 
     useBeforeunload((e) => {
@@ -84,12 +84,12 @@ const ActionsList = () => {
                     {'Cart Is Currently Empty,\nPlease Add Actions!'}
                 </Typography>
             ) : null}
-            <CheckoutCart />
+            <CheckoutCart overview={overview} />
         </div>
     );
 };
 
-export const ActionsCart = () => (
+export const ActionsCart: VFC<{ overview: GetOverviewResponse }> = ({ overview }) => (
     <Drawer
         className="cart-drawer"
         anchor={'right'}
@@ -107,6 +107,6 @@ export const ActionsCart = () => (
                 <strong>{'Planned Actions'}</strong>
             </Typography>
         </AppBar>
-        <ActionsList />
+        <ActionsList overview={overview} />
     </Drawer>
 );

commit e8c79eb3273436fed405b9734577b08823788589

Fix: Show sync window warning
Before, if the sync window warning would have been the only warning,
the confirmation dialog would not show.  This commit fixes it.

Fixes: e4cfa8348dcb97a917daf1d97400a80d1620ed3c

services/frontend-service/src/ui/ConfirmationDialog.tsx
@@ -131,6 +131,7 @@ export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProp
     }, [fin, closeDialog, openNotification, action, actions, setActions]);
 
     const conflicts = getCartConflicts(actions, action);
+    const hasSyncWindows = syncWindows && syncWindows.length > 0;
 
     const replaceAction = useCallback(() => {
         openNotification();
@@ -140,7 +141,7 @@ export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProp
     }, [fin, closeDialog, openNotification, action, actions, setActions, conflicts]);
 
     const handleAddToCart = useCallback(() => {
-        if (conflicts.size || locks?.length || undeployedUpstream) {
+        if (conflicts.size || locks?.length || undeployedUpstream || hasSyncWindows) {
             setDialogOpen(true);
         } else {
             addAction();
@@ -184,21 +185,20 @@ export const ConfirmationDialogProvider = (props: ConfirmationDialogProviderProp
             <strong>Possible conflict with actions already in cart!</strong>
         </Alert>
     );
-    const syncWindowsMessage =
-        syncWindows && syncWindows.length > 0 ? (
-            <Alert variant="outlined" sx={{ m: 1 }} severity="warning">
-                <AlertTitle>ArgoCD sync windows are active for this application!</AlertTitle>
-                <p>Warning: This can delay deployment.</p>
-                <h3>Sync windows:</h3>
-                <ul>
-                    {syncWindows?.map((w, n) => (
-                        <li key={`${n}:${w}`}>
-                            <SyncWindow w={w} />
-                        </li>
-                    ))}
-                </ul>
-            </Alert>
-        ) : null;
+    const syncWindowsMessage = hasSyncWindows ? (
+        <Alert variant="outlined" sx={{ m: 1 }} severity="warning">
+            <AlertTitle>ArgoCD sync windows are active for this application!</AlertTitle>
+            <p>Warning: This can delay deployment.</p>
+            <h3>Sync windows:</h3>
+            <ul>
+                {syncWindows?.map((w, n) => (
+                    <li key={`${n}:${w}`}>
+                        <SyncWindow w={w} />
+                    </li>
+                ))}
+            </ul>
+        </Alert>
+    ) : null;
 
     return (
         <>