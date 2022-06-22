d15758440d81ca0df7e5fbebdab742097273394c
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-06-22 17:02:29 +0200

    uncomment

--- charts/kuberpult/ci/create-test-values.sh
+++ charts/kuberpult/ci/create-test-values.sh
@@ -2,11 +2,11 @@
 set -ueo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 # yaml file override can't be used because as of now, yq can't remove comments, or preserve whitespace. So lint fails.
-# yq e -i '.git.url = "/repository"' "$script_dir"/test-values.yaml
-# yq e -i '.ingress.domainName = "example.kuberpult.com"' "$script_dir"/test-values.yaml
-# yq e -i '.cd.resources.requests.cpu = "500m"' "$script_dir"/test-values.yaml
-# yq e -i '.cd.resources.limits.cpu = "500m"' "$script_dir"/test-values.yaml
-# yq e -i '.cd.resources.requests.memory = "1Gi"' "$script_dir"/test-values.yaml
-# yq e -i '.cd.resources.limits.memory = "1Gi"' "$script_dir"/test-values.yaml
-# yq e -i '.fronted.resources.limits.cpu = "250m"' "$script_dir"/test-values.yaml
-# yq e -i '.fronted.resources.requests.cpu = "250m"' "$script_dir"/test-values.yaml
+yq e -i '.git.url = "/repository"' "$script_dir"/test-values.yaml
+yq e -i '.ingress.domainName = "example.kuberpult.com"' "$script_dir"/test-values.yaml
+yq e -i '.cd.resources.requests.cpu = "500m"' "$script_dir"/test-values.yaml
+yq e -i '.cd.resources.limits.cpu = "500m"' "$script_dir"/test-values.yaml
+yq e -i '.cd.resources.requests.memory = "1Gi"' "$script_dir"/test-values.yaml
+yq e -i '.cd.resources.limits.memory = "1Gi"' "$script_dir"/test-values.yaml
+yq e -i '.fronted.resources.limits.cpu = "250m"' "$script_dir"/test-values.yaml
+yq e -i '.fronted.resources.requests.cpu = "250m"' "$script_dir"/test-values.yaml
