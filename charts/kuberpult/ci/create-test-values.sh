9e69ae797895b5401c8d7cd0bae09931018a05cb
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-06-22 17:27:01 +0200

    make charts in pipeline:

--- charts/kuberpult/ci/create-test-values.sh
+++ charts/kuberpult/ci/create-test-values.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ueo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-# yaml file override can't be used because as of now, yq can't remove comments, or preserve whitespace. So lint fails.
-yq e -i '.git.url = "/repository"' "$script_dir"/test-values.yaml
-yq e -i '.ingress.domainName = "example.kuberpult.com"' "$script_dir"/test-values.yaml
-yq e -i '.cd.resources.requests.cpu = "500m"' "$script_dir"/test-values.yaml
-yq e -i '.cd.resources.limits.cpu = "500m"' "$script_dir"/test-values.yaml
-yq e -i '.cd.resources.requests.memory = "1Gi"' "$script_dir"/test-values.yaml
-yq e -i '.cd.resources.limits.memory = "1Gi"' "$script_dir"/test-values.yaml
-yq e -i '.fronted.resources.limits.cpu = "250m"' "$script_dir"/test-values.yaml
-yq e -i '.fronted.resources.requests.cpu = "250m"' "$script_dir"/test-values.yaml
+# yaml file override keeps comments and removes whitelines, so lint fails. Removing comments for lint
+grep -o '^[^#]*' "$script_dir"/../values.yaml > "$script_dir"/test-values.yaml
+yq eval-all -i 'select(fileIndex == 0) * select(fileIndex == 1)' "$script_dir"/test-values.yaml "$script_dir"/test-values-override.yaml 
