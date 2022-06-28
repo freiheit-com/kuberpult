02a1f2cde4a6e5f026328165b321240c9a992139
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-06-28 14:12:11 +0200

    remove extra line

--- infrastructure/scripts/execution-plan/plan-pr.sh
+++ infrastructure/scripts/execution-plan/plan-pr.sh
@@ -8,5 +8,5 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 # When called locally with Makefile, the default parameters are used
 base="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
 head="${2:-$(git rev-parse HEAD)}"
-git diff --diff-filter=ACMRT --name-only "$base" "$head"
+
 git diff --diff-filter=ACMRT --name-only "$base" "$head" | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" build-pr
