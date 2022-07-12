ac64128e79bbdb1dfd20c7c68b19800fc31d04ee
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-07-12 16:48:02 +0200

    change from fs to use full state

--- services/cd-service/pkg/repository/repository_test.go
+++ services/cd-service/pkg/repository/repository_test.go
@@ -28,7 +28,6 @@ import (
 	"strings"
 	"testing"
 
-	billy "github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/util"
 	"github.com/google/go-cmp/cmp"
 	git "github.com/libgit2/git2go/v33"
@@ -689,7 +688,7 @@ type SlowTransformer struct {
 	started  chan struct{}
 }
 
-func (s *SlowTransformer) Transform(ctx context.Context, fs billy.Filesystem) (string, error) {
+func (s *SlowTransformer) Transform(ctx context.Context, state State) (string, error) {
 	s.started <- struct{}{}
 	<-s.finished
 	return "ok", nil
@@ -697,7 +696,7 @@ func (s *SlowTransformer) Transform(ctx context.Context, fs billy.Filesystem) (s
 
 type PanicTransformer struct{}
 
-func (p *PanicTransformer) Transform(ctx context.Context, fs billy.Filesystem) (string, error) {
+func (p *PanicTransformer) Transform(ctx context.Context, state State) (string, error) {
 	panic("panic tranformer")
 }
 
@@ -705,13 +704,13 @@ var TransformerError = errors.New("error transformer")
 
 type ErrorTransformer struct{}
 
-func (p *ErrorTransformer) Transform(ctx context.Context, fs billy.Filesystem) (string, error) {
+func (p *ErrorTransformer) Transform(ctx context.Context, state State) (string, error) {
 	return "error", TransformerError
 }
 
 type InvalidJsonTransformer struct{}
 
-func (p *InvalidJsonTransformer) Transform(ctx context.Context, fs billy.Filesystem) (string, error) {
+func (p *InvalidJsonTransformer) Transform(ctx context.Context, state State) (string, error) {
 	return "error", invalidJson
 }
 
