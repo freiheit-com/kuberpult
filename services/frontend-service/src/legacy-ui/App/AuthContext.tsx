87dbd21ac242ef2a5c493bcc635c1e7d3bb137a3
Mohammed Nisham Kolakkat <mohammed.nisham.kolakkat@freiheit.com>
2022-08-09 14:43:24 +0200

    Add login

--- services/frontend-service/src/legacy-ui/App/AuthContext.tsx
+++ services/frontend-service/src/legacy-ui/App/AuthContext.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { ConfigsContext } from './index';
 import { Configuration, PublicClientApplication } from '@azure/msal-browser';
-import { MsalProvider, AuthenticatedTemplate, useIsAuthenticated, UnauthenticatedTemplate } from '@azure/msal-react';
+import {
+    MsalProvider,
+    AuthenticatedTemplate,
+    useIsAuthenticated,
+    UnauthenticatedTemplate,
+    useMsal,
+} from '@azure/msal-react';
 
 type AuthContextType = {};
 
@@ -12,7 +18,7 @@ const getMsalConfig = (configs: any): Configuration => ({
         clientId: configs?.authConfig?.azureAuth?.clientId,
         authority: `https://login.microsoftonline.com/${configs?.authConfig?.azureAuth?.tenantId}`,
         // TODO: change to live url
-        redirectUri: 'http://localhost:3000/account',
+        redirectUri: 'http://localhost:3000',
     },
     cache: {
         cacheLocation: 'sessionStorage',
@@ -20,34 +26,34 @@ const getMsalConfig = (configs: any): Configuration => ({
     },
 });
 
-// const getLoginRequest = (configs: any) => ({
-// scopes: ['User.Read'],
-// });
+const getLoginRequest = () => ({
+    scopes: ['User.Read'],
+});
+
+const AutoSignIn = (props: {}) => {
+    const isAuthenticated = useIsAuthenticated();
+    const loginRequest = React.useMemo(() => getLoginRequest(), []);
+    const { instance } = useMsal();
+    if (!isAuthenticated) {
+        instance.loginRedirect(loginRequest);
+    }
+    return <></>;
+};
 
 export function AuthProvider({ children }: { children: React.ReactNode }): JSX.Element {
     const { configs } = React.useContext(ConfigsContext);
-    const isAuthenticated = useIsAuthenticated();
     const useAzureAuth = configs?.authConfig?.azureAuth?.enabled || false;
     const msalConfig = React.useMemo(() => getMsalConfig(configs), [configs]);
-    // const loginRequest = React.useMemo(() => getLoginRequest(configs), [configs]);
     const msalInstance = new PublicClientApplication(msalConfig);
-    if (useAzureAuth && !isAuthenticated) {
-        // msalInstance.loginRedirect(loginRequest).catch(() => {
-        // console.error(e);
-        // });
-        // eslint-disable-next-line no-console
-        console.log(useAzureAuth, isAuthenticated, 'not authed');
-    } else {
-        // eslint-disable-next-line no-console
-        console.log(useAzureAuth, isAuthenticated, 'authed');
-    }
 
     return (
         <AuthContext.Provider value={{}}>
             {useAzureAuth ? (
                 <MsalProvider instance={msalInstance}>
                     <AuthenticatedTemplate>{children}</AuthenticatedTemplate>
-                    <UnauthenticatedTemplate></UnauthenticatedTemplate>
+                    <UnauthenticatedTemplate>
+                        <AutoSignIn />
+                    </UnauthenticatedTemplate>
                 </MsalProvider>
             ) : (
                 children
