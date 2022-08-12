1d235bd148e2d06f826bbeacb6ab2a90f7207c8d
Tamer Emad <tamer.emad@freiheit.com>
2022-08-12 18:54:56 +0200

    Add Top App Bar

---
+++ services/frontend-service/src/ui/TopAppBar/TopAppBar.tsx
@@ -0,0 +1,49 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2021 freiheit.com*/
+import { MDCTopAppBar } from '@material/top-app-bar';
+
+import { Button } from '../components/button';
+import { Textfield } from '../components/textfield';
+import { useEffect, useRef } from 'react';
+
+export const TopAppBar: React.FC = () => {
+    const control = useRef<HTMLDivElement>(null);
+    const MDComponent = useRef<MDCTopAppBar>();
+
+    useEffect(() => {
+        if (control.current) {
+            MDComponent.current = new MDCTopAppBar(control.current);
+        }
+        return () => MDComponent.current?.destroy();
+    }, []);
+
+    return (
+        <div className="mdc-top-app-bar--fixed" ref={control}>
+            <div className="mdc-top-app-bar__row">
+                <div className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                    <span className="mdc-top-app-bar__title">Kuberpult</span>
+                </div>
+                <div className="mdc-top-app-bar__section">
+                    <Textfield className={'top-app-bar-search-field'} floatingLabel={'Search'} leadingIcon={'search'} />
+                </div>
+                <div className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+                    <Button label={'Planned Actions'} />
+                </div>
+            </div>
+        </div>
+    );
+};
