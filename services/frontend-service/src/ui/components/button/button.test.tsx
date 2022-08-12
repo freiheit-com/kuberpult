c981b8ca16279e2e8e0e9f2c30171d91731e9823
Tamer Emad <tamer.emad@freiheit.com>
2022-08-12 18:39:07 +0200

    Create wrapper for button component

---
+++ services/frontend-service/src/ui/components/button/button.test.tsx
@@ -0,0 +1,41 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2021 freiheit.com*/
+import { Button } from './button';
+import { render } from '@testing-library/react';
+
+describe('Button', () => {
+    const getNode = () => <Button className={'button--test'} label={'Test Me'} />;
+    const getWrapper = () => render(getNode());
+    it('renders a button', () => {
+        const { container } = getWrapper();
+        expect(container.firstChild).toMatchInlineSnapshot(`
+    <button
+      aria-label="Test Me"
+      class="mdc-button button--test"
+    >
+      <div
+        class="mdc-button__ripple"
+      />
+      <span
+        class="mdc-button__label"
+      >
+        Test Me
+      </span>
+    </button>
+  `);
+    });
+});
---
+++ services/frontend-service/src/ui/components/button/button.tsx
@@ -0,0 +1,43 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2021 freiheit.com*/
+import { useRef, useEffect } from 'react';
+import classNames from 'classnames';
+import { MDCRipple } from '@material/ripple';
+
+export const Button = (props: { className?: string; label?: string }) => {
+    const MDComponent = useRef<MDCRipple>();
+    const control = useRef<HTMLButtonElement>(null);
+    const { className, label } = props;
+
+    useEffect(() => {
+        if (control.current) {
+            MDComponent.current = new MDCRipple(control.current);
+        }
+        return () => MDComponent.current?.destroy();
+    }, []);
+
+    return (
+        <button className={classNames('mdc-button', className)} ref={control} aria-label={label || ''}>
+            <div className="mdc-button__ripple" />
+            {!!label && (
+                <span key="label" className="mdc-button__label">
+                    {label}
+                </span>
+            )}
+        </button>
+    );
+};
