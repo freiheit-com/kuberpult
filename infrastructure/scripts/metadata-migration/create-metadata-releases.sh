0e8ed1bc4ee32b4f33ba098869845b63b0b3629d
Tamer Emad <tamer.emad@freiheit.com>
2022-08-23 11:55:23 +0200

    use find to loop over releases

--- infrastructure/scripts/metadata-migration/create-metadata-releases.sh
+++ infrastructure/scripts/metadata-migration/create-metadata-releases.sh
@@ -7,7 +7,7 @@ echo --------------------Releases------------------------------
 for app in applications/* # $(find applications  -maxdepth 1 -mindepth 1 -type d)
 do
   echo Adding metadata to "$(ls "$app"/releases | wc -l)" releases in "$app"
-  for release in "$app"/releases/*
+  find "$app"/releases  -maxdepth 1 -mindepth 1 -type d -print0 | while IFS= read -r -d '' release
   do
     echo Release: "$(basename "$release")"
     git log -1 --date=iso-strict --format="%ad" -- "$release" > "$release"/release_date
