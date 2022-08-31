11d4d87a6a69a6f364ca9ed9e0b42c9f0dae9bb5
Tamer Emad <tamer.emad@freiheit.com>
2022-08-31 18:38:06 +0200

    Add chip component

---
+++ services/frontend-service/src/ui/components/chip/chip.test.tsx
@@ -0,0 +1,45 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2021 freiheit.com*/
+import { Chip } from './chip';
+import { render } from '@testing-library/react';
+
+describe('Chip', () => {
+    const getNode = () => <Chip className={'chip--test'} label={'Test Me'} />;
+    const getWrapper = () => render(getNode());
+    it('renders a chip', () => {
+        const { container } = getWrapper();
+        expect(container.firstChild).toMatchInlineSnapshot(`
+<div
+  class="mdc-evolution-chip chip--test"
+>
+  <div
+    class="mdc-evolution-chip__cell mdc-evolution-chip__cell--primary"
+  >
+    <div
+      class="mdc-evolution-chip__action mdc-evolution-chip__action--primary"
+    >
+      <span
+        class="mdc-evolution-chip__text-label"
+      >
+        Test Me
+      </span>
+    </div>
+  </div>
+</div>
+`);
+    });
+});
