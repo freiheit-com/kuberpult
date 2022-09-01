828df069e976fab9e5c7a9d015831c8edbc91cf3
Mohammed Nisham Kolakkat <104761739+mnishamk-freiheit@users.noreply.github.com>
2022-09-01 14:16:53 +0200

    Update to use new execution plan structure with stages (#324) SRX-ZHDJUE

--- infrastructure/scripts/execution-plan/plan-main.sh
+++ infrastructure/scripts/execution-plan/plan-main.sh
@@ -2,4 +2,4 @@
 set -ueo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 source "${script_dir}"/container.inc.sh
-find -name Buildfile | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" build-main
+find -name Buildfile | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" main
