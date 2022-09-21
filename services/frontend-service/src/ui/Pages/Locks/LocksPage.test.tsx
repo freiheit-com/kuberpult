3c60ba865d766b11c18df8cb244a62104f2e901f
leandro-freiheit <leandro.salgado@freiheit.com>
2022-09-21 09:54:53 +0100

    Updates test for locks page

--- services/frontend-service/src/ui/Pages/Locks/LocksPage.test.tsx
+++ services/frontend-service/src/ui/Pages/Locks/LocksPage.test.tsx
@@ -17,12 +17,12 @@ Copyright 2021 freiheit.com*/
 import { render } from '@testing-library/react';
 import { LocksPage } from './LocksPage';
 
-describe('EnvironmentsPage', () => {
+describe('LocksPage', () => {
     const getNode = (): JSX.Element | any => <LocksPage />;
     const getWrapper = () => render(getNode());
 
     it('Renders full app', () => {
         const { container } = getWrapper();
-        expect(container.textContent).toBe('List All Locks Here');
+        expect(container.getElementsByClassName('mdc-env-data-table')[0]).toBeTruthy();
     });
 });
