9e4ddc224f36e9dbce85cf620dc05225591c7526
leandro-freiheit <leandro.salgado@freiheit.com>
2022-10-06 15:28:59 +0100

    Fixed infinite loop

--- services/cd-service/pkg/cmd/server.go
+++ services/cd-service/pkg/cmd/server.go
@@ -1,19 +1,3 @@
-/*This file is part of kuberpult.
-
-Kuberpult is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Kuberpult is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
-
-Copyright 2021 freiheit.com*/
 package cmd
 
 import (
@@ -146,8 +130,8 @@ func RunServer() {
 
 		span.Finish()
 
-		wg.Add(1)
 		go repository.SendRegularlyDatadogMetrics(repo, 300, repository.GetRepositoryStateAndUpdateMetrics)
+		wg.Wait()
 
 		// Shutdown channel is used to terminate server side streams.
 		shutdownCh := make(chan struct{})
@@ -196,8 +180,6 @@ func RunServer() {
 				return nil
 			},
 		})
-
-		wg.Wait()
 
 		return nil
 	})
