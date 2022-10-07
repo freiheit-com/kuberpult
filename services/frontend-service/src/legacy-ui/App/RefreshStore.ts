7f24dd4b8ba3cf72c716565895ae45f9ba0dfa0d
Sven Urbanski <sven.urbanski@freiheit.com>
2022-10-07 13:13:31 +0200

    Fixed an issue where the legacy ui did not reload the data on connection errors
    Implemented by doing polling in legacy ui and reloading the data after every basket submit.
    
    This is just a temporary quick fix to help make it stable quickly now.
    
    For the new UI (v2) we will implement a different reloading mechanism.
    
    Co-authored-by: Tamer Emad <tamer.emad@freiheit.com>

---
+++ services/frontend-service/src/legacy-ui/App/RefreshStore.ts
@@ -0,0 +1,37 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2021 freiheit.com*/
+/**
+ * The RefreshStore is solution to refresh the state (GetOverview).
+ * This is currently needed as an addition to polling.
+ * This will be re-worked in v2.
+ * See story SRX-0ELJF9.
+ */
+
+class RefreshStore {
+    doRefresh: boolean = false;
+
+    setRefresh(newState: boolean) {
+        this.doRefresh = newState;
+    }
+
+    shouldRefresh() {
+        return this.doRefresh;
+    }
+}
+
+const refreshStore = new RefreshStore();
+export default refreshStore;
