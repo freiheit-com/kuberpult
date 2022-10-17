0b3539ef05b92556f34c1cfe8992860f539e518a
leandro-freiheit <leandro.salgado@freiheit.com>
2022-10-17 17:34:00 +0100

    Refactored test

--- services/frontend-service/src/ui/Pages/Environments/EnvironmentsPage.test.tsx
+++ services/frontend-service/src/ui/Pages/Environments/EnvironmentsPage.test.tsx
@@ -15,19 +15,75 @@ along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright 2021 freiheit.com*/
 import { render } from '@testing-library/react';
-import { EnvironmentsPage } from './EnvironmentsPage';
-import { MemoryRouter } from 'react-router-dom';
+import { UpdateOverview } from '../../utils/store';
+import { EnvironmentLane } from '../../components/EnvironmentLane/EnvironmentLane';
 
-describe('EnvironmentsPage', () => {
-    const getNode = (): JSX.Element | any => (
-        <MemoryRouter>
-            <EnvironmentsPage />
-        </MemoryRouter>
-    );
-    const getWrapper = () => render(getNode());
+const sampleEnvs = {
+    foo: {
+        name: 'foo',
+        locks: {
+            testId: {
+                message: 'test message',
+                lockId: 'testId',
+                createdBy: {
+                    name: 'TestUser',
+                    email: 'testuser@test.com',
+                },
+            },
+            anotherTestId: {
+                message: 'more test messages',
+                lockId: 'anotherTestId',
+                createdBy: {
+                    name: 'TestUser',
+                    email: 'testuser@test.com',
+                },
+            },
+        },
+    },
+    moreTest: {
+        name: 'moreTest',
+        locks: {},
+    },
+} as any;
 
-    it('Renders full app', () => {
-        const { container } = getWrapper();
-        expect(container.textContent).toBe('List All Environments Here');
+interface dataT {
+    name: string;
+    environment: string;
+    expected: number;
+}
+
+const cases: dataT[] = [
+    {
+        name: 'Environment row with two locks',
+        environment: 'foo',
+        expected: 2,
+    },
+    {
+        name: 'Environment row with no locks',
+        environment: 'moreTest',
+        expected: 0,
+    },
+    {
+        name: 'None existant environment',
+        environment: 'nonExistant',
+        expected: 0,
+    },
+];
+
+describe('Environment Lane', () => {
+    const getNode = (overrides: { environment: string }) => <EnvironmentLane {...overrides} />;
+    const getWrapper = (overrides: { environment: string }) => render(getNode(overrides));
+
+    describe.each(cases)('Renders a row of environments', (testcase) => {
+        it(testcase.name, () => {
+            //given
+            UpdateOverview.set({
+                environments: sampleEnvs,
+            });
+            // when
+            const { container } = getWrapper({ environment: testcase.environment });
+            // then
+            expect(container.getElementsByClassName('environment-lock-display')).toHaveLength(testcase.expected);
+        });
     });
 });
