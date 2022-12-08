034cc66bd2b60b4c5f26b2a0eb32f9ee77010155
Mouhsen Ibrahim <78358035+mouhsen-ibrahim@users.noreply.github.com>
2022-12-08 14:24:01 +0100

    Add hidden releases indicator (#410)
    * Add a diff between release cards according to how many versions are between them
    
    * Fix style for diff display
    
    * Add tests for diff between services

--- services/frontend-service/src/ui/components/ServiceLane/ServiceLane.tsx
+++ services/frontend-service/src/ui/components/ServiceLane/ServiceLane.tsx
@@ -23,6 +23,23 @@ import { Application } from '../../../api/api';
 export const ServiceLane: React.FC<{ application: Application }> = (props) => {
     const { application } = props;
     const releases = useDeployedReleases(application.name);
+    const releases_lane =
+        !!releases &&
+        releases.map((rel, index) => {
+            if (index > 0) {
+                const diff = releases[index - 1] - rel - 1;
+                if (diff !== 0) {
+                    return (
+                        <div key={application + '-' + rel} className="service-lane__diff">
+                            <div className="service-lane__diff_number">{diff}</div>
+                            <ReleaseCard app={application.name} version={rel} />
+                        </div>
+                    );
+                }
+            }
+            return <ReleaseCard app={application.name} version={rel} key={application + '-' + rel} />;
+        });
+
     return (
         <div className="service-lane">
             <div className="service-lane__header">
@@ -42,11 +59,7 @@ export const ServiceLane: React.FC<{ application: Application }> = (props) => {
                     />
                 </div>
             </div>
-            <div className="service__releases">
-                {releases.map((rel) => (
-                    <ReleaseCard app={application.name} version={rel} key={application + '-' + rel} />
-                ))}
-            </div>
+            <div className="service__releases">{releases_lane}</div>
         </div>
     );
 };
