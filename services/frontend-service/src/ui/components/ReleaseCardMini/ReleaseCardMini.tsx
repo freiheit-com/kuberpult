8d45a9bedc08a12b9549db1defdadbebbd744536
Sven Urbanski <sven.urbanski@freiheit.com>
2022-12-22 15:34:14 +0100

    Refactored List of Environment Chips into own component

--- services/frontend-service/src/ui/components/ReleaseCardMini/ReleaseCardMini.tsx
+++ services/frontend-service/src/ui/components/ReleaseCardMini/ReleaseCardMini.tsx
@@ -16,10 +16,8 @@ along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
 Copyright 2021 freiheit.com*/
 import classNames from 'classnames';
 import React from 'react';
-import { updateReleaseDialog, useCurrentlyDeployedAt, useOverview, useRelease } from '../../utils/store';
-import { Chip } from '../chip';
-import { calculateEnvironmentPriorities, EnvPrioMap, sortEnvironmentsByUpstream } from '../ReleaseDialog/ReleaseDialog';
-import { Environment } from '../../../api/api';
+import { updateReleaseDialog, useRelease } from '../../utils/store';
+import { EnvironmentChipList } from '../ReleaseCard/ReleaseCard';
 
 export type ReleaseCardMiniProps = {
     className?: string;
@@ -37,7 +35,6 @@ const getDays = (date: Date) => {
 export const ReleaseCardMini: React.FC<ReleaseCardMiniProps> = (props) => {
     const { className, app, version } = props;
     const { createdAt, sourceMessage, sourceAuthor } = useRelease(app, version);
-    const environmentsForApp = useCurrentlyDeployedAt(app, version);
     const clickHanlder = React.useCallback(() => {
         updateReleaseDialog(app, version);
     }, [app, version]);
@@ -53,10 +50,6 @@ export const ReleaseCardMini: React.FC<ReleaseCardMiniProps> = (props) => {
         }
         msg += `${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}`;
     }
-    const allEnvs: Environment[] = useOverview((x) => Object.values(x.environments));
-    const sortedEnvs: Environment[] = sortEnvironmentsByUpstream(allEnvs);
-    const envPrioMap: EnvPrioMap = calculateEnvironmentPriorities(allEnvs);
-    const envsForAppSorted = sortedEnvs.filter((env: Environment) => environmentsForApp.includes(env));
 
     return (
         <div className={classNames('release-card-mini', className)} onClick={clickHanlder}>
@@ -65,14 +58,7 @@ export const ReleaseCardMini: React.FC<ReleaseCardMiniProps> = (props) => {
                 <div className="release__details-msg">{msg}</div>
             </div>
             <div className="release__environments-mini">
-                {envsForAppSorted.map((env) => (
-                    <Chip
-                        className={classNames('release-environment', className)}
-                        label={env.name}
-                        key={env.name}
-                        priority={envPrioMap[env.name]}
-                    />
-                ))}
+                <EnvironmentChipList app={props.app} version={props.version} />
             </div>
         </div>
     );
