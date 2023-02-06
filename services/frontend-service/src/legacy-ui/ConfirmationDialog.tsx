58a6c5eb9ba6f5c33c19e7c4377828dcba66b642
Sven Urbanski <sven.urbanski@freiheit.com>
2023-02-06 17:39:11 +0100

    remove licence header everywhere

--- services/frontend-service/src/legacy-ui/ConfirmationDialog.tsx
+++ services/frontend-service/src/legacy-ui/ConfirmationDialog.tsx
@@ -1,20 +1,3 @@
-/*This file is part of kuberpult.
-
-Kuberpult is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Kuberpult is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with kuberpult.  If not, see <http://www.gnu.org/licenses/>.
-
-Copyright 2021 freiheit.com*/
-
 import { Environment_Application_ArgoCD_SyncWindow, Lock } from '../api/api';
 import * as React from 'react';
 import { useCallback, useContext } from 'react';
