19dbca25021c4f7bcbf8e82096ef46819424824a
Tamer Emad <tamer.emad@freiheit.com>
2023-02-17 21:15:57 +0100

    Add mdc-tooltip component with JSX content

---
+++ services/frontend-service/src/ui/components/tooltip/tooltip.tsx
@@ -0,0 +1,50 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright 2023 freiheit.com*/
+import { useEffect, useRef } from 'react';
+import { MDCTooltip } from '@material/tooltip';
+
+export const Tooltip = (props: { children: JSX.Element; content: JSX.Element; id: string }) => {
+    const MDComponent = useRef<MDCTooltip>();
+    const control = useRef<HTMLDivElement>(null);
+    const { children, content, id } = props;
+
+    useEffect(() => {
+        if (control.current) {
+            MDComponent.current = new MDCTooltip(control.current);
+        }
+        return () => MDComponent.current?.destroy();
+    }, []);
+
+    return (
+        <div className="mdc-tooltip-wrapper--rich">
+            <div data-tooltip-id={'tt-' + id} aria-haspopup="dialog" aria-expanded="false">
+                {children}
+            </div>
+            <div
+                id={'tt-' + id}
+                ref={control}
+                className="mdc-tooltip mdc-tooltip--rich"
+                aria-hidden="true"
+                tabIndex={-1}
+                role="dialog">
+                <div className="mdc-tooltip__surface mdc-tooltip__surface-animation">
+                    <div className="mdc-tooltip__caret-surface-top" />
+                    <div className="mdc-tooltip__content">{content}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
