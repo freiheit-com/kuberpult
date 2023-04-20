3fb7511c43231062e5f09055a7733d0a1317a738
Tamer Emad <88655074+tameremad@users.noreply.github.com>
2023-04-20 11:18:53 +0200

    Fix UI scaling - HomePage (#653)
    * fix top bar title font
    
    * fix icon sizes navigation + logo
    
    * fix the scaling of Navigation Bar
    
    * fix scaling of top app bar, clean up unused variables
    
    * fix service lane icon sizes
    
    * remove two dots from service lane diff, fix lock icon size
    
    * fix scaling of font sizes
    
    * fix scaling for service lane component
    
    * fix lock icon size env chip
    
    * fix ReleaseCard and ServiceLane scale, refactor css
    
    * fix test snapshot
    
    * fix scale for release card tooltip
    
    * make commitmessage font smaller and release card smaller

--- services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.tsx
+++ services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.tsx
@@ -41,7 +41,7 @@ export const EnvLock: React.FC<{
             title={'Lock Message: "' + lock.message + '" | ID: "' + lock.lockId + '"  | Click to unlock. '}>
             <div>
                 <Button
-                    icon={<LocksWhite className="env-card-env-lock-icon" width="24px" height="24px" />}
+                    icon={<LocksWhite className="env-card-env-lock-icon" width="16px" height="16px" />}
                     className={'button-lock'}
                     onClick={deleteEnvLock}
                 />
@@ -62,7 +62,7 @@ export const AppLockSummary: React.FC<{
             title={'"' + app + '" has ' + numLocks + ' application ' + plural + '. Click on a tile to see details.'}>
             <div>
                 &nbsp;
-                <LocksWhite className="env-card-env-lock-icon" width="24px" height="24px" />
+                <LocksWhite className="env-card-env-lock-icon" width="16px" height="16px" />
             </div>
         </Tooltip>
     );
@@ -97,7 +97,7 @@ export const EnvironmentChip = (props: EnvironmentChipProps): JSX.Element => {
     ) : (
         !!Object.entries(env.locks).length && (
             <div className={classNames(className, 'env-locks')}>
-                <LocksWhite className="env-card-env-lock-icon" width="18px" height="18px" />
+                <LocksWhite className="env-card-env-lock-icon" width="12px" height="12px" />
             </div>
         )
     );
