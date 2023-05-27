0a52661083360b52159cbbfc61d27efbae4649ae
Hannes Voigt-Georg <hannes.georg@gmail.com>
2023-05-28 00:12:21 +0200

    Updated nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
   description = "kuberpult";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.systems.url = "github:nix-systems/default";
 
   inputs.grpc-gateway = {
     url = "github:grpc-ecosystem/grpc-gateway";
@@ -15,63 +14,63 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, grpc-gateway, gomod2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # General setup
-        pkgs_ = nixpkgs.legacyPackages.${system};
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ gomod2nix.overlays.default ];
-        };
-        version = pkgs.readFile ./version;
-        protoc-gen-grpc-gateway = pkgs.callPackage ./nix/grpc-gateway/default.nix { inherit pkgs; };
-        protoc-gen-ts-proto = (pkgs.callPackage ./nix/ts-proto/default.nix { pkgs = pkgs_; nodejs = pkgs_.nodejs; }).ts-proto;
+  outputs = { self, nixpkgs, systems, grpc-gateway, gomod2nix }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShell = eachSystem
+        (system:
+          let
+            # General setup
+            pkgs_ = nixpkgs.legacyPackages.${system};
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ gomod2nix.overlays.default ];
+            };
+            version = pkgs.readFile ./version;
+            protoc-gen-grpc-gateway = pkgs.callPackage ./nix/grpc-gateway/default.nix { inherit pkgs; };
+            protoc-gen-ts-proto = (pkgs.callPackage ./nix/ts-proto/default.nix { pkgs = pkgs_; nodejs = pkgs_.nodejs; }).ts-proto;
 
-        # Native Build inputs
-        nativeBuildInputs = [
-          # go build
-          pkgs.pkgconfig
-          pkgs.gnumake
-          pkgs.gomod2nix
-          pkgs.go_1_19
+            # Native Build inputs
+            nativeBuildInputs = [
+              # go build
+              pkgs.pkgconfig
+              pkgs.gnumake
+              pkgs.gomod2nix
+              pkgs.go_1_19
 
-          # nodejs build
-          pkgs.nodePackages.pnpm
-          pkgs.nodejs-slim
-          pkgs.nodePackages.node2nix
+              # nodejs build
+              pkgs.nodePackages.pnpm
+              pkgs.nodejs-slim
+              pkgs.nodePackages.node2nix
 
-          # chart build
-          pkgs.kubernetes-helm
-          pkgs.envsubst
+              # chart build
+              pkgs.kubernetes-helm
+              pkgs.envsubst
 
-          # build tools
-          pkgs.jq
+              # build tools
+              pkgs.jq
+              pkgs.docker-client
 
-          # protobuf generation
-          pkgs.buf
-          pkgs.protoc-gen-go
-          pkgs.protoc-gen-go-grpc
-          protoc-gen-grpc-gateway
-          protoc-gen-ts-proto
-        ];
+              # protobuf generation
+              pkgs.buf
+              pkgs.protoc-gen-go
+              pkgs.protoc-gen-go-grpc
+              protoc-gen-grpc-gateway
+              protoc-gen-ts-proto
+            ];
 
-        # Target Build inputs
-        buildInputs = [
-          pkgs.libgit2_1_5
-          pkgs.sqlite
-        ];
-      in
-      rec {
-
-        packages = {
-          "protoc-gen-grpc-gateway" = protoc-gen-grpc-gateway;
-        };
-
-        # Creates a dev shell that has all dependencies preloaded
-        devShell = pkgs.mkShell {
-          inherit nativeBuildInputs buildInputs;
-        };
-      }
-    );
+            # Target Build inputs
+            buildInputs = [
+              pkgs.libgit2_1_5
+              pkgs.sqlite
+              pkgs.glibc
+            ];
+          in
+          # Creates a dev shell that has all dependencies preloaded
+          pkgs.mkShell {
+            inherit nativeBuildInputs buildInputs;
+          });
+    };
 }
