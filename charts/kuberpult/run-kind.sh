bc26789a3d1c7c5aadf1f0908f955c6a01c1bb8f
Sven Urbanski <sven.urbanski@freiheit.com>
2023-06-07 16:04:47 +0200

    Run kind in CI with kuberpult & git server & environments as test data (#708)
    This installs `kind` as integration test setup.
    Including: setting up kuberpult, the manifest repo with 3 env groups and 5 envs total, setting up argoCd and allowing both tools access.
    Excluding: There is no app release yet, so no kuberpult-managed microservice is automatically deployed into the kind cluster.
    
    Co-authored-by: Sven Urbanski <sven.urbanski+git@freiheit.com>
    Co-authored-by: Initial Kuberpult Commiter <team.sre.permanent+kuberpult-initial-commiter@freiheit.com>

---
+++ charts/kuberpult/run-kind.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+set -x
+
+# This script assumes that the docker images have already been built.
+# To run/debug/develop this locally, you probably want to run like this:
+# rm -rf ./manifests/; make clean; LOCAL_EXECUTION=true ./run-kind.sh
+
+cd "$(dirname $0)"
+
+
+# prefix every call to "echo" with the name of the script:
+function print() {
+  /bin/echo "$0:" "$@"
+}
+
+cleanup() {
+    print "Cleaning stuff up..."
+    helm uninstall kuberpult-local || print kuberpult was not installed
+    kind delete cluster || print kind cluster was not deleted
+}
+trap cleanup INT TERM
+cleanup
+
+print 'creating kind cluster with a hostpath to share testdata...'
+kind create cluster --config=- <<EOF || print cluster exists
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+EOF
+
+export GIT_NAMESPACE=git
+
+LOCAL_EXECUTION=${LOCAL_EXECUTION:-false}
+print "LOCAL_EXECUTION: $LOCAL_EXECUTION"
+
+print 'ensuring that the helm chart is build...'
+# it was already build, but we are in another workflow now, so we have to rebuild it
+make all
+
+print installing ssh...
+./setup-cluster-ssh.sh
+
+function waitForDeployment() {
+  ns="$1"
+  label="$2"
+  print "waiting for $ns/$label"
+  until kubectl wait --for=condition=ready pod -n "$ns" -l "$label" --timeout=30s
+  do
+    sleep 3s
+    print ...
+  done
+}
+
+function portForwardAndWait() {
+  ns="$1"
+  deployment="$2"
+  portHere="$3"
+  portThere="$4"
+  ports="$portHere:$portThere"
+  print "waiting for $ns/$deployment $ports"
+  kubectl -n "$ns" port-forward deployment/"$deployment" "$ports" &
+  print "waiting until the port forward works..."
+  until nc -vz localhost "$portHere"
+  do
+    sleep 1s
+    print ...
+  done
+}
+
+print "setting up manifest repo"
+waitForDeployment "git" "app.kubernetes.io/name=server"
+portForwardAndWait "git" "server" "2222" "22"
+
+rm emptyfile -f
+print "cloning..."
+git config --global user.email 'team.sre.permanent+kuberpult-initial-commiter@freiheit.com'; git config --global user.name 'Initial Kuberpult Commiter';
+GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=emptyfile -o StrictHostKeyChecking=no -i ../../services/cd-service/client' git clone ssh://git@localhost:2222/git/repos/manifests
+
+cd manifests
+pwd
+cp ../../../infrastructure/scripts/create-testdata/testdata_template/environments -r .
+git add environments
+git commit -m "add initial environments from template"
+print "pushing environments to manifest repo..."
+GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=emptyfile -o StrictHostKeyChecking=no -i ../../../services/cd-service/client' git push origin master
+cd -
+
+export IMAGE_REGISTRY=europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult
+
+if "$LOCAL_EXECUTION"
+then
+  print 'building cd service...'
+  WITH_DOCKER=true make -C ../../services/cd-service/ docker
+
+  print 'building frontend service...'
+  make -C ../../services/frontend-service/ docker
+else
+  print 'not building cd or frontend service...'
+fi
+
+
+VERSION=$(make --no-print-directory -C ../../services/cd-service/ version)
+IMAGE_TAG_FRONTEND=${IMAGE_TAG_FRONTEND:-$VERSION}
+IMAGE_TAG_CD=${IMAGE_TAG_CD:-$VERSION}
+
+cd_imagename="${IMAGE_REGISTRY}/kuberpult-cd-service:${IMAGE_TAG_CD}"
+frontend_imagename="${IMAGE_REGISTRY}/kuberpult-frontend-service:${IMAGE_TAG_FRONTEND}"
+
+print "cd image: $cd_imagename"
+print "cd image tag: $IMAGE_TAG_CD"
+print "frontend image: $frontend_imagename"
+print "frontend image tag: $IMAGE_TAG_FRONTEND"
+
+if ! "$LOCAL_EXECUTION"
+then
+  print 'pulling cd service...'
+  docker pull "$cd_imagename"
+  print 'pulling frontend service...'
+  docker pull "$frontend_imagename"
+else
+  print 'not pulling cd or frontend service...'
+fi
+
+print 'loading docker images into kind...'
+print "$cd_imagename"
+print "$frontend_imagename"
+kind load docker-image "$cd_imagename"
+kind load docker-image "$frontend_imagename"
+
+print 'installing kuberpult helm chart...'
+
+set_options='ingress.domainName=kuberpult.example.com,name=kuberpult-local,VERSION='"$VERSION"',cd.tag='"$IMAGE_TAG_CD",frontend.tag="$IMAGE_TAG_FRONTEND"',git.url=ssh://git@server.'"${GIT_NAMESPACE}"'.svc.cluster.local/git/repos/manifests'
+ssh_options=",ssh.identity=$(cat ../../services/cd-service/client),ssh.known_hosts=$(cat ../../services/cd-service/known_hosts),"
+
+helm template ./ --set "$set_options""$ssh_options" > tmp.tmpl
+helm install --set "$set_options""$ssh_options" kuberpult-local ./
+
+print 'checking for pods and waiting for portforwarding to be ready...'
+
+kubectl get deployment
+kubectl get pods
+
+waitForDeployment "default" "app=kuberpult-frontend-service"
+portForwardAndWait "default" "kuberpult-frontend-service" "8081" "8081"
+print "connection to frontend service successful"
+
+kubectl get deployment
+kubectl get pods
+
+if "$LOCAL_EXECUTION"
+then
+  echo "sleeping for 1h to allow debugging"
+  sleep 1h
+else
+  echo "done. Kind cluster is up and kuberpults frontend service is running."
+fi
---
+++ charts/kuberpult/setup-cluster-ssh.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+
+cd "$(dirname "$0")"
+
+ARGO_NAMESPACE=argocd
+#GIT_NAMESPACE=git # required to be set outside
+
+scratch=$(mktemp -d)
+function finish {
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
+ssh-keygen -t ed25519 -N "" -C host -f "${scratch}/host" 1>&2
+ssh-keygen -t ed25519 -N "" -C client -f "${scratch}/client" 1>&2
+
+host_pub="$(cat "${scratch}/host.pub")"
+
+cp "${scratch}/client" ../../services/cd-service/client
+cat <<EOF > ../../services/cd-service/known_hosts
+server.${GIT_NAMESPACE}.svc.cluster.local ${host_pub}
+localhost ${host_pub}
+EOF
+
+echo printing known_hosts
+cat ../../services/cd-service/known_hosts
+
+kubectl create namespace "git" || echo "already exists"
+kubectl create namespace "argocd" || echo "already exists"
+
+
+docker pull europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult/git-ssh:1.1.1
+
+kind load docker-image europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult/git-ssh:1.1.1
+kubectl apply -f - <<EOF
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  labels:
+    app.kubernetes.io/name: argocd-ssh-known-hosts-cm
+    app.kubernetes.io/part-of: argocd
+  name: argocd-ssh-known-hosts-cm
+  namespace: ${ARGO_NAMESPACE}
+data:
+  ssh_known_hosts: |
+    server.${GIT_NAMESPACE}.svc.cluster.local ${host_pub}
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: my-private-ssh-repo
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+  namespace: ${ARGO_NAMESPACE}
+stringData:
+  url: ssh://git@server.${GIT_NAMESPACE}.svc.cluster.local/git/repos/manifests
+  sshPrivateKey: |
+$(sed -e "s/^/    /" <"$scratch"/client)
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: ssh-host
+  namespace: ${GIT_NAMESPACE}
+data:
+  ssh_host_ed25519_key: |
+$(sed -e "s/^/    /" <"$scratch"/host)
+  ssh_host_ed25519_key.pub: |
+$(sed -e "s/^/    /" <"$scratch"/host.pub)
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: ssh-client
+  namespace: ${GIT_NAMESPACE}
+data:
+  client.pub: |
+$(sed -e "s/^/    /" <"$scratch"/client.pub)
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: server
+  namespace: ${GIT_NAMESPACE}
+spec:
+  ports:
+  - name: ssh
+    port: 22
+    protocol: TCP
+    targetPort: 22
+  selector:
+    app.kubernetes.io/name: server
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: server
+  namespace: ${GIT_NAMESPACE}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: server
+  strategy:
+    type: RollingUpdate
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: server
+    spec:
+      initContainers:
+      - image: "europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult/git-ssh:1.1.1"
+        imagePullPolicy: Never
+        name: "git-init"
+        command: ["/bin/sh","-c"]
+        args: ["ls -l /template/; git init --bare /git/repos/manifests"]
+        volumeMounts:
+        - mountPath: /git/repos
+          name: repos
+        - name: template
+          mountPath: /template
+      containers:
+      - image: "europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult/git-ssh:1.1.1"
+        imagePullPolicy: Never
+        name: git
+        ports:
+        - containerPort: 22
+          protocol: TCP
+        env:
+        - name: PUID
+          value: "1000"
+        - name: PGID
+          value: "1000"
+        volumeMounts:
+        - mountPath: /git/keys-host
+          name: ssh-host
+          readOnly: true
+        - mountPath: /git/keys
+          name: ssh-client
+          readOnly: true
+        - mountPath: /git/repos
+          name: repos
+      volumes:
+      - name: template # for initial test data
+        hostPath:
+          path: /create-testdata
+      - name: ssh-host
+        configMap:
+          name: ssh-host
+          defaultMode: 0600
+      - name: ssh-client
+        configMap:
+          name: ssh-client
+      - name: repos
+        emptyDir:
+          sizeLimit: 50Mi
+      restartPolicy: Always
+EOF
+echo "done setting up ssh"
