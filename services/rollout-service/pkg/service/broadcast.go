4f88632fe4e0a82145bb1eff76c6a6847f8a52c6
Hannes Voigt-Georg <hannes.georg@freiheit.com>
2023-06-22 14:26:34 +0200

    Add broadcast implementation and tests (#746)
    Co-authored-by: Initial Kuberpult Commiter <team.sre.permanent+kuberpult-initial-commiter@freiheit.com>

--- services/rollout-service/pkg/service/broadcast.go
+++ services/rollout-service/pkg/service/broadcast.go
@@ -18,10 +18,12 @@ package service
 
 import (
 	"context"
+	"errors"
 	"sync"
 
 	"github.com/freiheit-com/kuberpult/pkg/api"
 
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/gitops-engine/pkg/health"
 )
 
@@ -31,13 +33,15 @@ type key struct {
 }
 
 type Broadcast struct {
-	state map[key]Event
-	mx    sync.Mutex
+	state    map[key]Event
+	mx       sync.Mutex
+	listener map[chan *api.StreamStatusResponse]struct{}
 }
 
 func New() *Broadcast {
 	return &Broadcast{
-		state: map[key]Event{},
+		state:    map[key]Event{},
+		listener: map[chan *api.StreamStatusResponse]struct{}{},
 	}
 }
 
@@ -52,10 +56,56 @@ func (b *Broadcast) Process(ctx context.Context, ev Event) {
 	if b.state[k] == ev {
 		return
 	}
+	msg := &api.StreamStatusResponse{
+		Environment:   ev.Environment,
+		Application:   ev.Application,
+		Version:       ev.Version,
+		RolloutStatus: rolloutStatus(&ev),
+	}
 	b.state[k] = ev
+	desub := []chan *api.StreamStatusResponse{}
+	for l := range b.listener {
+		select {
+		case l <- msg:
+		default:
+			close(l)
+			desub = append(desub, l)
+		}
+	}
+	for _, l := range desub {
+		delete(b.listener, l)
+	}
 }
 
 func (b *Broadcast) StreamStatus(req *api.StreamStatusRequest, svc api.RolloutService_StreamStatusServer) error {
+	resp, ch, unsubscribe := b.start()
+	defer unsubscribe()
+	for _, r := range resp {
+		svc.Send(r)
+	}
+	for {
+		select {
+		case r := <-ch:
+			err := svc.Send(r)
+			if err != nil {
+				return err
+			}
+		case <-svc.Context().Done():
+			err := svc.Context().Err()
+			if errors.Is(err, context.Canceled) {
+				return nil
+			}
+			return err
+		}
+	}
+}
+
+type unsubscribe func()
+
+func (b *Broadcast) start() ([]*api.StreamStatusResponse, <-chan *api.StreamStatusResponse, unsubscribe) {
+	b.mx.Lock()
+	defer b.mx.Unlock()
+	result := make([]*api.StreamStatusResponse, 0, len(b.state))
 	for _, ev := range b.state {
 		msg := &api.StreamStatusResponse{
 			Environment:   ev.Environment,
@@ -63,16 +113,15 @@ func (b *Broadcast) StreamStatus(req *api.StreamStatusRequest, svc api.RolloutSe
 			Version:       ev.Version,
 			RolloutStatus: rolloutStatus(&ev),
 		}
-		svc.Send(msg)
+		result = append(result, msg)
 	}
-	return nil
-}
-
-type unsubscribe interface {
-}
-
-func (b *Broadcast) start() ([]api.StreamStatusResponse, <-chan api.StreamStatusResponse, unsubscribe) {
-	panic("unimplemented")
+	ch := make(chan *api.StreamStatusResponse, 100)
+	b.listener[ch] = struct{}{}
+	return result, ch, func() {
+		b.mx.Lock()
+		defer b.mx.Unlock()
+		delete(b.listener, ch)
+	}
 }
 
 func rolloutStatus(ev *Event) api.RolloutStatus {
@@ -82,6 +131,10 @@ func rolloutStatus(ev *Event) api.RolloutStatus {
 	case health.HealthStatusProgressing, health.HealthStatusSuspended:
 		return api.RolloutStatus_RolloutStatusProgressing
 	case health.HealthStatusHealthy:
+		switch ev.SyncStatusCode {
+		case v1alpha1.SyncStatusCodeOutOfSync:
+			return api.RolloutStatus_RolloutStatusProgressing
+		}
 		return api.RolloutStatus_RolloutStatusSuccesful
 	}
 	return api.RolloutStatus_RolloutStatusUnknown
