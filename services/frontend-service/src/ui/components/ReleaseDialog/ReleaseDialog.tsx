11ed56bd6177b9d38400602655b5ee28e4c276a2
Gracia Sandok <47796934+gsandok@users.noreply.github.com>
2023-07-04 18:41:25 +0200

    services/frontend-service: Add deployedBy and deployedAt to UI
    - Fixing snapshot
    - adding FormattedDate

--- services/frontend-service/src/ui/components/ReleaseDialog/ReleaseDialog.tsx
+++ services/frontend-service/src/ui/components/ReleaseDialog/ReleaseDialog.tsx
@@ -138,24 +138,28 @@ export const EnvironmentListItem: React.FC<EnvironmentListItemProps> = ({
         }
         return otherRelease?.sourceCommitId + ': ' + otherRelease?.sourceMessage;
     };
-    const getDeploymentMetadata = (): string => {
+    const getDeploymentMetadata = (): [String, JSX.Element] => {
         if (!application) {
-            return ``;
+            return ['', <></>];
         }
         if (!(application && application.version === release.version)) {
-            return '';
+            return ['', <></>];
         }
         if (application.deploymentMetaData === null) {
-            return '';
+            return ['', <></>];
         }
         const deployedBy = application.deploymentMetaData?.deployAuthor ?? 'unknown';
         const deployedUNIX = application.deploymentMetaData?.deployTime ?? '';
         if (deployedUNIX === '') {
-            return 'Deployed by ' + deployedBy;
+            return ['Deployed by ' + deployedBy, <></>];
         }
         const deployedDate = new Date(+deployedUNIX * 1000);
-        const returnString = 'Deployed by ' + deployedBy + ' on ' + deployedDate;
-        return returnString;
+        const returnString = 'Deployed by ' + deployedBy + ' ';
+        const time = (
+            <FormattedDate createdAt={deployedDate} className={classNames('release-dialog-createdAt', className)} />
+        );
+
+        return [returnString, time];
     };
     return (
         <li key={env.name} className={classNames('env-card', className)}>
@@ -192,7 +196,11 @@ export const EnvironmentListItem: React.FC<EnvironmentListItemProps> = ({
                         {getCommitString()}
                     </div>
                     {queueInfo}
-                    <div className={classNames('env-card-data', className)}>{getDeploymentMetadata()}</div>
+                    <div className={classNames('env-card-data', className)}>
+                        {getDeploymentMetadata().flatMap((metadata, i) => (
+                            <div key={i}>{metadata}</div>
+                        ))}
+                    </div>
                 </div>
                 <div className="content-right">
                     <div className="env-card-buttons">
