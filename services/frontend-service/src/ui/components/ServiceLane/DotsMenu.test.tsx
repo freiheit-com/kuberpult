dbb57685c282d153fe0c7e2e53d84cbf95bb42c6
Sven Urbanski <sven.urbanski+git@freiheit.com>
2023-07-05 17:58:35 +0200

    Hide Buttons behind "..." menu in service lane
    The buttons "show history", "Prepare Undeploy Release" and "Delete Forever" are now hidden in a menu, since they are rather uncommon.

---
+++ services/frontend-service/src/ui/components/ServiceLane/DotsMenu.test.tsx
@@ -0,0 +1,96 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright 2023 freiheit.com*/
+import { act, render } from '@testing-library/react';
+import { DotsMenu, DotsMenuProps } from './DotsMenu';
+import { elementQuerySelectorSafe } from '../../../setupTests';
+
+type TestData = {
+    name: string;
+    input: DotsMenuProps;
+    expectedNumItems: number;
+};
+
+const mySpy = jest.fn();
+
+const data: TestData[] = [
+    {
+        name: 'renders empty list',
+        input: { buttons: [] },
+        expectedNumItems: 0,
+    },
+    {
+        name: 'renders one button',
+        input: {
+            buttons: [
+                {
+                    label: 'test label',
+                    onClick: mySpy,
+                },
+            ],
+        },
+        expectedNumItems: 1,
+    },
+    {
+        name: 'renders three button',
+        input: {
+            buttons: [
+                {
+                    label: 'test label A',
+                    onClick: mySpy,
+                },
+                {
+                    label: 'test label B',
+                    onClick: mySpy,
+                },
+                {
+                    label: 'test label C',
+                    onClick: mySpy,
+                },
+            ],
+        },
+        expectedNumItems: 3,
+    },
+];
+
+describe('DotsMenu Rendering', () => {
+    const getNode = (overrides: DotsMenuProps) => <DotsMenu {...overrides} />;
+    const getWrapper = (overrides: DotsMenuProps) => render(getNode(overrides));
+    describe.each(data)('DotsMenu Test', (testcase) => {
+        it(testcase.name, () => {
+            mySpy.mockReset();
+            expect(mySpy).toHaveBeenCalledTimes(0);
+
+            const { container } = getWrapper(testcase.input);
+
+            expect(document.querySelectorAll('.dots-menu-hidden .mdc-button--unelevated').length).toEqual(1);
+            const result = elementQuerySelectorSafe(container, '.dots-menu-hidden .mdc-button--unelevated');
+            act(() => {
+                result.click();
+            });
+            expect(document.querySelectorAll('.dots-menu-hidden .mdc-button--unelevated').length).toEqual(0);
+            expect(document.querySelectorAll('li.item').length).toEqual(testcase.expectedNumItems);
+
+            if (testcase.expectedNumItems > 0) {
+                expect(mySpy).toHaveBeenCalledTimes(0);
+                const result = elementQuerySelectorSafe(container, 'li .mdc-button--unelevated');
+                act(() => {
+                    result.click();
+                });
+                expect(mySpy).toHaveBeenCalledTimes(1);
+            }
+        });
+    });
+});
