8d5697e11fb8da4c2cc453f7ea5e189235a86ed6
Bernardo Bastos <bernardo.bastos@freiheit.com>
2023-07-13 15:37:57 +0100

    Add Environment Check for RBAC permissions

--- pkg/auth/rbac.go
+++ pkg/auth/rbac.go
@@ -3,13 +3,21 @@ package auth
 import (
 	"fmt"
 	"strings"
+
+	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/config"
 )
 
 // Inits the RBAC Config struct
-func initRbacConfig() RBACconfig {
+func initRbacConfig(configs map[string]config.EnvironmentConfig) RBACconfig {
+	allowedEnvs := []string{}
+	for env, config := range configs {
+		group := *config.EnvironmentGroup
+		allowedEnvs = append(allowedEnvs, group+":"+env)
+	}
 	return RBACconfig{
-		allowedApp:     []string{"EnvironmentLock", "EnvironmentApplicationLock", "Deploy", "Undeploy", "EnvironmentFromApp"},
+		allowedApp:     []string{"EnvironmentLock", "EnvironmentApplicationLock", "Deploy", "Undeploy", "EnvironmentFromApplication"},
 		allowedActions: []string{"Create", "Delete"},
+		allowedEnvs:    allowedEnvs,
 	}
 }
 
@@ -17,6 +25,7 @@ func initRbacConfig() RBACconfig {
 type RBACconfig struct {
 	allowedApp     []string
 	allowedActions []string
+	allowedEnvs    []string
 }
 
 func (c *RBACconfig) validateApp(app string) error {
@@ -43,6 +52,15 @@ func (c *RBACconfig) validateAction(action string) error {
 	return fmt.Errorf("invalid action %s", action)
 }
 
+func (c *RBACconfig) validateEnvs(env string) error {
+	for _, e := range c.allowedEnvs {
+		if e == env {
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid environment %s", env)
+}
+
 // Struct to store an RBAC permission.
 type permission struct {
 	Role        string
@@ -64,6 +82,11 @@ func validateRbacPermission(line string, cfg RBACconfig) (p *permission, err err
 	}
 	// Validate the permission action
 	err = cfg.validateAction(c[3])
+	if err != nil {
+		return nil, err
+	}
+	// Validate the environments
+	err = cfg.validateEnvs(c[4])
 	if err != nil {
 		return nil, err
 	}
--- pkg/auth/rbac_test.go
+++ pkg/auth/rbac_test.go
@@ -3,19 +3,27 @@ package auth
 import (
 	"testing"
 
+	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/config"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestValidateRbacPermission(t *testing.T) {
+	devEnvironmentGroup := "dev"
 	tcs := []struct {
 		Name           string
 		Permission     string
+		configs        map[string]config.EnvironmentConfig
 		WantError      string
 		WantPermission *permission
 	}{
 		{
 			Name:       "Validating RBAC works as expected",
 			Permission: "p,Developer,Deploy,*,dev:development-d2,allow",
+			configs: map[string]config.EnvironmentConfig{
+				"development-d2": {
+					EnvironmentGroup: &devEnvironmentGroup,
+				},
+			},
 			WantPermission: &permission{
 				Role:        "Developer",
 				Application: "Deploy",
@@ -26,19 +34,31 @@ func TestValidateRbacPermission(t *testing.T) {
 		{
 			Name:       "Invalid permission Application",
 			Permission: "p,Developer,WRONG_APP,*,dev:development-d2,allow",
+			configs:    map[string]config.EnvironmentConfig{},
 			WantError:  "invalid application WRONG_APP",
 		},
 		{
 			Name:       "Invalid permission Action",
 			Permission: "p,Developer,EnvironmentLock,WRONG_ACTION,dev:development-d2,allow",
+			configs:    map[string]config.EnvironmentConfig{},
 			WantError:  "invalid action WRONG_ACTION",
+		},
+		{
+			Name:       "Invalid permission Environment",
+			Permission: "p,Developer,Deploy,*,dev:development-WRONG,allow",
+			configs: map[string]config.EnvironmentConfig{
+				"development-d2": {
+					EnvironmentGroup: &devEnvironmentGroup,
+				},
+			},
+			WantError: "invalid environment dev:development-WRONG",
 		},
 	}
 
 	for _, tc := range tcs {
 		tc := tc
 		t.Run(tc.Name, func(t *testing.T) {
-			cfg := initRbacConfig()
+			cfg := initRbacConfig(tc.configs)
 			permission, err := validateRbacPermission(tc.Permission, cfg)
 			if err != nil {
 				if diff := cmp.Diff(err.Error(), tc.WantError); diff != "" {
