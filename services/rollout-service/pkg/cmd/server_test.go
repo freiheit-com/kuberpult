bc46ee83d0d609f6f3ac2d19fa38e0e6a59d12b9
Hannes Voigt-Georg <hannes.georg@freiheit.com>
2023-08-28 13:37:02 +0200

    Fix typo

--- services/rollout-service/pkg/cmd/server_test.go
+++ services/rollout-service/pkg/cmd/server_test.go
@@ -38,7 +38,7 @@ func TestService(t *testing.T) {
 				ArgocdServer: "not a http address",
 			},
 		},
-		
+		{
 			Name:          "valid http argocd url",
 			ExpectedError: "connecting to argocd version: dial tcp 127.0.0.1:32761: connect: connection refused",
 			Config: Config{
