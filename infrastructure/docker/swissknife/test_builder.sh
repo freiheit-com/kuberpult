891a8a9424c3a08073291f0a45e77cd1e7ca1289
Sven Urbanski <sven.urbanski+git@freiheit.com>
2023-10-17 18:34:25 +0200

    rename builder to swissknife, discard golang

---
+++ infrastructure/docker/swissknife/test_builder.sh
@@ -0,0 +1,8 @@
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+docker build -t kuberpult-swissknife .
+docker run -d --privileged -v $SCRIPT_DIR/../../..:/repo kuberpult-swissknife
+id=$(docker ps | grep "kuberpult-builder" | head -n 1 | cut -f1 -d" ")
+docker exec $id sh -c 'sleep 5; cd /repo/infrastructure/docker/swissknife; make build; cd /repo/services/frontend-service; make docker; cd /repo/services/cd-service; make docker'
+docker kill $id
+docker rm $id
+
