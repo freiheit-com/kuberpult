4b9b427fe31b1bab16c88f543bd9de81c6c46b28
Bernardo Bastos <bernardo.bastos@freiheit.com>
2023-10-27 16:26:43 +0100

    Do helm dependency build before template

--- charts/kuberpult/run-kind.sh
+++ charts/kuberpult/run-kind.sh
@@ -310,12 +310,11 @@ pgp:
 $(sed -e "s/^/    /" <./kuberpult-keyring.gpg)
 VALUES
 
-helm template ./ --values vals.yaml > tmp.tmpl
-
 # Get helm dependency chartas and unzip them
 helm dependency build
 (rm -rf charts && helm dep update && cd charts && for filename in *.tgz; do tar -xf "$filename" && rm -f "$filename"; done;)
 
+helm template ./ --values vals.yaml > tmp.tmpl
 helm install --values vals.yaml kuberpult-local ./
 print 'checking for pods and waiting for portforwarding to be ready...'
 
