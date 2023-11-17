983c2017c0fdebc655a7c09f0bfead87dbd1ac72
Gracia Sandok <47796934+gsandok@users.noreply.github.com>
2023-11-17 10:36:33 +0000

    Add endpoint that returns the version of all services in an environment at a given commit (#1146)
    Create endpoint that calls overview service and filters response
    
    REV: DSN-S9VVRW

--- services/cd-service/pkg/service/tags.go
+++ services/cd-service/pkg/service/tags.go
@@ -19,20 +19,66 @@ package service
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/freiheit-com/kuberpult/pkg/api"
 	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/repository"
 )
 
 type TagsServer struct {
-	Cfg repository.RepositoryConfig
+	Config          repository.RepositoryConfig
+	OverviewService *OverviewServiceServer
 }
 
 func (s *TagsServer) GetGitTags(ctx context.Context, in *api.GetGitTagsRequest) (*api.GetGitTagsResponse, error) {
-	tags, err := repository.GetTags(s.Cfg, "./repository_tags", ctx)
+	tags, err := repository.GetTags(s.Config, "./repository_tags", ctx)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get tags from repository: %v", err)
 	}
 
 	return &api.GetGitTagsResponse{TagData: tags}, nil
+}
+
+func (s *TagsServer) GetProductSummary(ctx context.Context, in *api.GetProductSummaryRequest) (*api.GetProductSummaryResponse, error) {
+	if in.Environment == "" {
+		return nil, fmt.Errorf("Must have an environment to get the product summary for")
+	}
+	if in.CommitHash == "" {
+		return nil, fmt.Errorf("Must have a commit to get the product summary for")
+	}
+	response, err := s.OverviewService.GetOverview(ctx, &api.GetOverviewRequest{GitRevision: in.CommitHash})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get overview for %s: %v", in.CommitHash, err)
+	}
+	var summaryFromEnv []api.ProductSummary
+	for _, group := range response.EnvironmentGroups {
+		for _, env := range group.Environments {
+			if env.Name == in.Environment {
+				for _, app := range env.Applications {
+					summaryFromEnv = append(summaryFromEnv, api.ProductSummary{App: app.Name, Version: strconv.Itoa(int(app.Version))})
+				}
+			}
+		}
+	}
+	if len(summaryFromEnv) == 0 {
+		return nil, fmt.Errorf("environment %s not found", in.Environment)
+	}
+
+	var productVersion []*api.ProductSummary
+	for _, row := range summaryFromEnv {
+		for _, app := range response.Applications {
+			if row.App == app.Name {
+				length := len(app.Releases) - 1
+				if app.Releases[length].DisplayVersion != "" {
+					productVersion = append(productVersion, &api.ProductSummary{App: row.App, Version: app.Releases[length].DisplayVersion})
+				} else if app.Releases[length].SourceCommitId != "" {
+					productVersion = append(productVersion, &api.ProductSummary{App: row.App, Version: app.Releases[length].SourceCommitId})
+				} else {
+					productVersion = append(productVersion, &api.ProductSummary{App: row.App, Version: row.Version})
+				}
+				break
+			}
+		}
+	}
+	return &api.GetProductSummaryResponse{ProductSummary: productVersion}, nil
 }
