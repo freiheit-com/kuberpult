21384e91bb3a2bcf15f90a15e0ab7998afa4f284
Sven Urbanski <sven.urbanski+git@freiheit.com>
2023-12-19 17:57:23 +0100

    fix TestAllErrorsHandledDeleteEnvironmentLock

--- services/cd-service/pkg/repository/transformer_test.go
+++ services/cd-service/pkg/repository/transformer_test.go
@@ -3872,7 +3872,7 @@ func TestAllErrorsHandledDeleteEnvironmentLock(t *testing.T) {
 			createLockBefore: false,
 			operation:        testfs.REMOVE,
 			filename:         "environments/dev/locks",
-			expectedError:    "rpc error: code = FailedPrecondition desc = file does not exist",
+			expectedError:    "rpc error: code = FailedPrecondition desc = directory environments/dev/locks/foo for env lock does not exist",
 		},
 	}
 	for _, tc := range tcs {
