f1a17257c1596a3ae140be138e210b01f0ea57d9
Sven Urbanski <sven.urbanski@freiheit.com>
2024-01-22 18:34:05 +0100

    fix: refactor go packages "grcp" and "valid" into "pkg"
    All go services now only depend on "pgk", but not other services
    
    The whitespace changes is a quickfix for a flaky test.

--- services/cd-service/pkg/service/batch_test.go
+++ services/cd-service/pkg/service/batch_test.go
@@ -383,7 +383,7 @@ func TestBatchServiceErrors(t *testing.T) {
 						},
 					},
 				}},
-			ExpectedResponse: `results:{create_release_response:{too_long:{appName:"myappIsWayTooLongDontYouThink"  regExp:"\\A[a-z0-9]+(?:-[a-z0-9]+)*\\z"  maxLen:39}}}`,
+			ExpectedResponse: `results:{create_release_response:{too_long:{appName:"myappIsWayTooLongDontYouThink" regExp:"\\A[a-z0-9]+(?:-[a-z0-9]+)*\\z" maxLen:39}}}`,
 		},
 	}
 	for _, tc := range tcs {
@@ -408,10 +408,10 @@ func TestBatchServiceErrors(t *testing.T) {
 				},
 			)
 			if tc.ExpectedResponse != "" && response.String() != tc.ExpectedResponse {
-				t.Fatalf("expected: %s, got: %s, %s", tc.ExpectedResponse, response.String(), processErr)
+				t.Fatalf("expected:\n%s\ngot:\n%s\n%s", tc.ExpectedResponse, response.String(), processErr)
 			}
 			if tc.ExpectedResponse == "" && tc.ExpectedError != processErr.Error() {
-				t.Fatalf("expected error: %s, got: %s, %s", tc.ExpectedError, response.String(), processErr)
+				t.Fatalf("expected error\n%s\ngot:\n%s\n%s", tc.ExpectedError, response.String(), processErr)
 			}
 		})
 	}
@@ -899,4 +899,3 @@ func TestCreateEnvironmentTrain(t *testing.T) {
 		})
 	}
 }
-
