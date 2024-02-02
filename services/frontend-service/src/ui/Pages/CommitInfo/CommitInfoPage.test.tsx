2e0c41d06180b5ebe91037c047326921068d8e2f
Khaled Ismaeel <khaled.ismaeel@freiheit.com>
2024-02-02 12:44:45 +0100

    add commit info page unit tests

--- services/frontend-service/src/ui/Pages/CommitInfo/CommitInfoPage.test.tsx
+++ services/frontend-service/src/ui/Pages/CommitInfo/CommitInfoPage.test.tsx
@@ -13,11 +13,18 @@ You should have received a copy of the MIT License
 along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
 
 Copyright 2023 freiheit.com*/
+
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { CommitInfoPage } from './CommitInfoPage';
 import { fakeLoadEverything } from '../../../setupTests';
-// import { updateCommitInfo, useCommitInfo } from '../../utils/store';
+import { updateCommitInfo, CommitInfoState } from '../../utils/store';
+
+// const mockCommitInfo = jest.fn();
+// jest.mock('../../components/CommitInfo/CommitInfo', () => (props) => {
+//     mockCommitInfo(props);
+//     return <h1>xyzxyz</h1>;
+// });
 
 describe('CommitPage', () => {
     const getNode = (): JSX.Element | any => (
@@ -55,4 +62,70 @@ describe('CommitPage', () => {
             expect(container.getElementsByClassName('spinner')).toHaveLength(testcase.expectedNumSpinner);
         });
     });
+});
+
+test('Commit info page shows an error when the commit ID is not provided in the URL', () => {
+    fakeLoadEverything(true);
+    const { container } = render(
+        <MemoryRouter initialEntries={['/ui/commits/']}>
+            <Routes>
+                <Route path="/ui/commits/" element={<CommitInfoPage />} />
+            </Routes>
+        </MemoryRouter>
+    );
+    expect(container.textContent).toContain('commit ID not provided');
+});
+
+test('Commit info page shows a spinner when waiting for the server to respond', () => {
+    fakeLoadEverything(true);
+    updateCommitInfo.set({ response: undefined, commitInfoReady: CommitInfoState.LOADING });
+    const { container } = render(
+        <MemoryRouter initialEntries={['/ui/commits/helloooo']}>
+            <Routes>
+                <Route path="/ui/commits/:commit" element={<CommitInfoPage />} />
+            </Routes>
+        </MemoryRouter>
+    );
+
+    expect(container.getElementsByClassName('spinner')).not.toHaveLength(0);
+    expect(container.textContent).toContain('Loading commit info');
+});
+
+test('Commit info page shows an error message when the backend returns an error', () => {
+    fakeLoadEverything(true);
+    updateCommitInfo.set({ response: undefined, commitInfoReady: CommitInfoState.ERROR });
+
+    const { container } = render(
+        <MemoryRouter initialEntries={['/ui/commits/helloooo']}>
+            <Routes>
+                <Route path="/ui/commits/:commit" element={<CommitInfoPage />} />
+            </Routes>
+        </MemoryRouter>
+    );
+
+    expect(container.textContent).toContain('Backend error');
+});
+
+test('Commit info page shows displays commit info when everything is okay', () => {
+    fakeLoadEverything(true);
+    updateCommitInfo.set({
+        response: {
+            touchedApps: ['google', 'windows'],
+            commitMessage: `Add google to windows
+
+Commit message body line 1
+Commit message body line 2`,
+        },
+        commitInfoReady: CommitInfoState.READY,
+    });
+
+    const { container } = render(
+        <MemoryRouter initialEntries={['/ui/commits/helloooo']}>
+            <Routes>
+                <Route path="/ui/commits/:commit" element={<CommitInfoPage />} />
+            </Routes>
+        </MemoryRouter>
+    );
+
+    expect(container.textContent).toContain('Commit Add google to windows');
 });
