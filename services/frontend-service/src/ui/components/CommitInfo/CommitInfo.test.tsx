e7fe7793e78934fa44650f6bfe4e451ba898f29d
Khaled Ismaeel <khaled.ismaeel@freiheit.com>
2024-02-02 18:16:11 +0100

    add license to file

--- services/frontend-service/src/ui/components/CommitInfo/CommitInfo.test.tsx
+++ services/frontend-service/src/ui/components/CommitInfo/CommitInfo.test.tsx
@@ -1,5 +1,18 @@
-// import { updateCommitInfo } from "../../utils/store";
+/*This file is part of kuberpult.
 
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright 2023 freiheit.com*/
 import { render, screen } from '@testing-library/react';
 import { CommitInfo } from './CommitInfo';
 import { MemoryRouter } from 'react-router-dom';
