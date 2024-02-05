aeb32a8d86b85ec27ab512aef71dfaedf85c8323
Sven Urbanski <sven.urbanski@freiheit.com>
2024-02-05 15:50:27 +0100

    added missing pkg

---
+++ pkg/uuid/uuid.go
@@ -0,0 +1,34 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright 2023 freiheit.com*/
+
+package uuid
+
+import "github.com/google/uuid"
+
+type GenerateUUIDs interface {
+	Generate() (string, error)
+}
+
+type RealUUIDGenerator struct {
+}
+
+func (t RealUUIDGenerator) Generate() (string, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
+}
