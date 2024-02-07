a283e3491127962d5766feb03e3c97527cf44274
Gracia Sandok <47796934+gsandok@users.noreply.github.com>
2024-02-07 15:02:01 +0100

    feat: Add filtering based on team to Product Version Page (#1319)
    - Adding a column to table that says team value
    - Add filtering for the Product Version table based on selected teams

--- services/frontend-service/src/ui/components/ProductVersion/ProductVersion.test.tsx
+++ services/frontend-service/src/ui/components/ProductVersion/ProductVersion.test.tsx
@@ -13,19 +13,42 @@ You should have received a copy of the MIT License
 along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
 
 Copyright 2023 freiheit.com*/
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import {
-    Environment,
-    EnvironmentGroup,
-    GetGitTagsResponse,
-    GetProductSummaryResponse,
-    Priority,
-    ProductSummary,
-    TagData,
-} from '../../../api/api';
-import { UpdateOverview, updateSummary, updateTag } from '../../utils/store';
-import { ProductVersion } from './ProductVersion';
+import { Environment, EnvironmentGroup, Priority, ProductSummary, TagData } from '../../../api/api';
+import { ProductVersion, TableFiltered } from './ProductVersion';
+import { Spy } from 'spy4js';
+
+const mock_UseEnvGroups = Spy('envGroup');
+const mock_UseTags = Spy('Overview');
+const mock_UseSummaryDisplay = Spy('Status');
+
+jest.mock('../../utils/store', () => ({
+    getSummary() {
+        return {};
+    },
+    refreshTags() {
+        return {};
+    },
+    useEnvironmentGroups() {
+        return mock_UseEnvGroups();
+    },
+    useTags() {
+        return mock_UseTags();
+    },
+    useSummaryDisplay() {
+        return mock_UseSummaryDisplay();
+    },
+}));
+
+jest.mock('../../utils/Links', () => ({
+    DisplayManifestLink() {
+        return <></>;
+    },
+    DisplaySourceLink() {
+        return <></>;
+    },
+}));
 
 const sampleEnvsA: Environment[] = [
     {
@@ -73,7 +96,7 @@ describe('Product Version Data', () => {
                     commitId: '123',
                     displayVersion: 'v1.2.3',
                     environment: 'dev',
-                    team: '',
+                    team: 'sre-team',
                 },
             ],
             environmentGroups: [
@@ -99,7 +122,7 @@ describe('Product Version Data', () => {
                     commitId: '123',
                     displayVersion: 'v1.2.3',
                     environment: 'dev',
-                    team: '',
+                    team: 'sre-team',
                 },
                 { app: 'tester', version: '10', commitId: '4565', displayVersion: '', environment: 'dev', team: '' },
             ],
@@ -117,14 +140,12 @@ describe('Product Version Data', () => {
         // given
         it(testCase.name, () => {
             // replicate api calls
-            UpdateOverview.set({
-                environmentGroups: testCase.environmentGroups,
+            mock_UseEnvGroups.returns(testCase.environmentGroups);
+            mock_UseTags.returns({ response: { tagData: testCase.tags }, tagsReady: true });
+            mock_UseSummaryDisplay.returns({
+                response: { productSummary: testCase.productSummary },
+                summaryReady: true,
             });
-            const tagsResponse: GetGitTagsResponse = { tagData: testCase.tags };
-            updateTag.set({ response: tagsResponse, tagsReady: true });
-            const summaryResponse: GetProductSummaryResponse = { productSummary: testCase.productSummary };
-            updateSummary.set({ response: summaryResponse, summaryReady: true });
-
             render(
                 <MemoryRouter>
                     <ProductVersion />
@@ -142,6 +163,157 @@ describe('Product Version Data', () => {
                 expect(document.querySelector('.page_description')?.textContent).toContain(
                     'This page shows the version'
                 );
+            }
+        });
+    });
+});
+
+describe('Test table filtering', () => {
+    type TestData = {
+        name: string;
+        productSummary: ProductSummary[];
+        teams: string[];
+        expectedApps: string[];
+        filteredApps: string[];
+    };
+    const data: TestData[] = [
+        {
+            name: 'no rows to display',
+            productSummary: [],
+            teams: [],
+            expectedApps: [],
+            filteredApps: [],
+        },
+        {
+            name: 'no teams to filter out',
+            productSummary: [
+                {
+                    app: 'testing-app',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'sre-team',
+                },
+            ],
+            teams: [],
+            expectedApps: ['testing-app'],
+            filteredApps: [],
+        },
+        {
+            name: 'a team to filter out',
+            productSummary: [
+                {
+                    app: 'testing-app',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'sre-team',
+                },
+                {
+                    app: 'testing-app2',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'others',
+                },
+            ],
+            teams: ['sre-team'],
+            expectedApps: ['testing-app'],
+            filteredApps: ['testing-app2'],
+        },
+        {
+            name: 'no teams to filter out',
+            productSummary: [
+                {
+                    app: 'testing-app',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'sre-team',
+                },
+                {
+                    app: 'testing-app 2',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'others',
+                },
+                {
+                    app: 'testing-app 3',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'others',
+                },
+            ],
+            teams: ['sre-team', 'others'],
+            expectedApps: ['testing-app', 'testing-app 2', 'testing-app 3'],
+            filteredApps: [],
+        },
+        {
+            name: 'bigger example',
+            productSummary: [
+                {
+                    app: 'testing-app',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'sre-team',
+                },
+                {
+                    app: 'testing-app 2',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'others',
+                },
+                {
+                    app: 'testing-app 3',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'others',
+                },
+                {
+                    app: 'testing-app 4',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'another',
+                },
+                {
+                    app: 'testing-app 5',
+                    version: '4',
+                    commitId: '123',
+                    displayVersion: 'v1.2.3',
+                    environment: 'dev',
+                    team: 'last team',
+                },
+            ],
+            teams: ['sre-team', 'others'],
+            expectedApps: ['testing-app', 'testing-app 2', 'testing-app 3'],
+            filteredApps: ['testing-app 4', 'testing-app 5'],
+        },
+    ];
+    describe.each(data)(`Displays Product Version Table`, (testCase) => {
+        it(testCase.name, () => {
+            render(<TableFiltered productSummary={testCase.productSummary} teams={testCase.teams} />);
+            expect(document.querySelector('.table')?.textContent).toContain('App Name');
+            for (let i = 0; i < testCase.expectedApps.length; i++) {
+                expect(document.querySelector('.table')?.textContent).toContain(testCase.expectedApps[i]);
+            }
+            for (let i = 0; i < testCase.filteredApps.length; i++) {
+                expect(screen.queryByText(testCase.filteredApps[i])).not.toBeInTheDocument();
             }
         });
     });
