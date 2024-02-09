17b7b59321147612327c8bcdf6ecd1e4058b67a0
Henning Goes <henning.goes@freiheit.com>
2024-02-09 13:35:45 +0100

    fix: Remove MDCList dependency

--- services/frontend-service/src/ui/components/navigation/navList.tsx
+++ services/frontend-service/src/ui/components/navigation/navList.tsx
@@ -14,25 +14,10 @@ along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>
 
 Copyright 2023 freiheit.com*/
 import classNames from 'classnames';
-import { ReactNode, useEffect, useRef } from 'react';
-import { MDCList } from '@material/list';
+import { ReactNode } from 'react';
 
 export const NavList: React.FC<{ children?: ReactNode; className?: string }> = (props) => {
-    const MDComponent = useRef<MDCList>();
-    const control = useRef<HTMLElement>(null);
     const { className, children } = props;
 
-    useEffect(() => {
-        if (control.current) {
-            MDComponent.current = new MDCList(control.current);
-            MDComponent.current.wrapFocus = true;
-        }
-        return (): void => MDComponent.current?.destroy();
-    }, []);
-
-    return (
-        <nav className={classNames('mdc-list', className)} ref={control}>
-            {children}
-        </nav>
-    );
+    return <nav className={classNames('mdc-list', className)}>{children}</nav>;
 };
