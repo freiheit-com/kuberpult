ee92fe87819ef529548915eb6ef0e5ba450324c9
Ahmed Nour <125875466+ahmed-nour-fdc@users.noreply.github.com>
2024-03-08 10:21:25 +0100

    fix(CI): Include deleted files in the plan (#1416)

--- infrastructure/scripts/execution-plan/plan-pr.sh
+++ infrastructure/scripts/execution-plan/plan-pr.sh
@@ -9,4 +9,4 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 base="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
 head="${2:-$(git rev-parse HEAD)}"
 
-git diff --diff-filter=ACMRT --name-only "$base" "$head" | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" pr
+git diff --diff-filter=ACMRDT --name-only "$base" "$head" | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" pr
