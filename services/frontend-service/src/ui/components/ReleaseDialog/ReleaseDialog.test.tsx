freiheit-com/kuberpult

commit 1232cd1afa03e4a5cddf5999c707a875bb99e78e

fix: environment cards colors (#1321)
In this PR we change the rules for environment cards colors in the
environments page.

Environments have a
[`priority`](https://github.com/freiheit-com/kuberpult/blob/19d0f430f3948526694b099e4f9be295afc89754/pkg/api/v1/api.proto#L337)
property that indicates where they lie in a chain of upstream relations.
Currently the color of an environment card is
[determined](https://github.com/freiheit-com/kuberpult/blob/4a3eaa34cd55732df7b16da61503fe54cbb0de9f/services/frontend-service/src/ui/components/EnvironmentCard/EnvironmentCard.tsx#L40)
based on this property.

The color of environment groups is determined by the priority of the
first environment in that group, which is erroneous: environments in one
group have the same _distance to upstream_, but not necessarily the same
_priority_. This leads to situations of an environment group its
environments having different colors which looks awkward in the UI.

| ![](https://i.imgur.com/LGvQj6t.png) |
| :---: |
| _Figure 1: awkward UI colors, notice that both `testing-1` and
`testing-2` have different colors. This is because `testing-2` has not
downstream, so it is assigned the `PRODUCTION` priority level._|

In this PR we:
1. Introduce a new field to the environment group struct, called
`priority` of type `Priority`. Despite the similarity, this field for
environment groups is calculated in a different way from that of
environments such that the priority is determined based on the distance
to upstream and not the priorities of environments. The reason for
reusing the same name and the same type is to keep changes to the
frontend to a minimum.
2. Change the frontend logic for determining the colors of environment
and environment group cards to be based on the newly introduced
`EnvironmentGroup.priority` field.

Now it looks like this:

| ![](https://imgur.com/LUcbmqH.png) |
| :---: |
| _Figure 2: fixed colors in the UI_|

---------

services/frontend-service/src/ui/components/ReleaseDialog/ReleaseDialog.test.tsx
@@ -307,6 +307,7 @@ describe('Release Dialog', () => {
                     environmentGroupName: 'dev',
                     environments: testcase.envs,
                     distanceToUpstream: 2,
+                    priority: Priority.UNRECOGNIZED,
                 },
             ],
         });

services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.test.tsx
@@ -41,7 +41,14 @@ describe('EnvironmentChip', () => {
     it('renders a chip', () => {
         // given
         UpdateOverview.set({
-            environmentGroups: [{ environments: [env], environmentGroupName: 'dontcare', distanceToUpstream: 0 }],
+            environmentGroups: [
+                {
+                    environments: [env],
+                    environmentGroupName: 'dontcare',
+                    distanceToUpstream: 0,
+                    priority: Priority.UNRECOGNIZED,
+                },
+            ],
         });
         // then
         const { container } = getWrapper();
@@ -101,6 +108,7 @@ describe('EnvironmentChip', () => {
                             },
                         },
                     ],
+                    priority: Priority.UNRECOGNIZED,
                     environmentGroupName: 'dontcare',
                     distanceToUpstream: 0,
                 },
@@ -168,7 +176,12 @@ describe.each(envChipData)(`EnvironmentChip with envPrio Classname`, (testcase) 
         // given
         UpdateOverview.set({
             environmentGroups: [
-                { environments: [testcase.env], environmentGroupName: 'dontcare', distanceToUpstream: 0 },
+                {
+                    environments: [testcase.env],
+                    environmentGroupName: 'dontcare',
+                    distanceToUpstream: 0,
+                    priority: Priority.UNRECOGNIZED,
+                },
             ],
         });
         // then
@@ -186,6 +199,7 @@ const envGroupFromPrio = (prio: Priority, numEnvsInGroup: number, envs: Environm
     environmentGroupName: 'i am the group',
     environments: envs,
     distanceToUpstream: 0,
+    priority: Priority.UNRECOGNIZED,
 });
 
 type TestDataGroups = {

pkg/api/v1/api.proto
@@ -323,6 +323,15 @@ message EnvironmentGroup {
   repeated Environment environments = 2;
   // note that the distance_to_upstream should usually be configured to be the same for all envs in this group, but this is not enforced.
   uint32 distance_to_upstream = 3;
+  /*
+    Even though this field has the same name and type as Environment.priority, it does have difference semantics.
+
+    Environment priority is calculated based on the location of an environment in a chain. Environment group priority is calculated based on the distance to upstream **and** the maximum global distance to upstream.
+    This field therefore characterizes the "layer" of an environment group. The reason it is reusing the name and type of Environment.priority is to keep the calculation of environment colors in the frontend untouched.
+  
+    Note: proper calculation of this field assumes there is not more than one environment group hierarchy; that is, there is only one group with distance_to_upstream = 0.
+  */
+  Priority priority = 4;
 }
 
 enum Priority {

services/cd-service/pkg/mapper/environments_config.go
@@ -140,7 +140,45 @@ func MapEnvironmentsToGroups(envs map[string]config.EnvironmentConfig) []*api.En
 		}
 	}
 	calculateEnvironmentPriorities(tmpEnvs) // note that `tmpEnvs` were copied by reference - otherwise this function would have no effect on `result`
+
+	{
+		var downstreamDepth uint32 = 0
+		for _, group := range result {
+			downstreamDepth = max(downstreamDepth, group.DistanceToUpstream)
+		}
+
+		for _, group := range result {
+			group.Priority = calculateGroupPriority(group.DistanceToUpstream, downstreamDepth)
+		}
+	}
+
 	return result
+}
+
+func calculateGroupPriority(distanceToUpstream, downstreamDepth uint32) api.Priority {
+	lookup := [][]api.Priority {
+		[]api.Priority {api.Priority_YOLO},
+		[]api.Priority {api.Priority_UPSTREAM, api.Priority_PROD},
+		[]api.Priority {api.Priority_UPSTREAM, api.Priority_PRE_PROD, api.Priority_PROD},
+		[]api.Priority {api.Priority_UPSTREAM, api.Priority_PRE_PROD, api.Priority_CANARY, api.Priority_PROD},
+		[]api.Priority {api.Priority_UPSTREAM, api.Priority_OTHER, api.Priority_PRE_PROD, api.Priority_CANARY, api.Priority_PROD},
+	}
+	if downstreamDepth > uint32(len(lookup) - 1) {
+		if distanceToUpstream == 0 {
+			return api.Priority_UPSTREAM
+		}
+		if distanceToUpstream == downstreamDepth {
+			return api.Priority_PROD
+		}
+		if distanceToUpstream == downstreamDepth - 1 {
+			return api.Priority_CANARY
+		}
+		if distanceToUpstream == downstreamDepth - 2 {
+			return api.Priority_PRE_PROD
+		}
+		return api.Priority_OTHER
+	}
+	return lookup[downstreamDepth][distanceToUpstream]
 }
 
 // either the groupName is set in the config, or we use the envName as a default

commit dae7abc589b1f15d8869475ef3901b975cee2d62

fix: make colors between `EnvironmentCard` and `EnvironmentChip` consistent (#1432)
Currently environment colors are inconsistent between the environments
page and the release dialogue, as shown below.

| ![](https://i.imgur.com/LchXL1G.png) |
| :---: |
| _Figure 1: the environments page (left) and the release dialogue for
an arbitrary app (right). Note that `development` has different colors
in the two pages_|

These colors are determined by the `priority` field of `Environment` and
`EnvironmentGroup` types; the environments page refers to the
`EnvironmentGroup` type, while the release dialogue refers to the
`Environment` type. This inconsistency was introduced in #1321.

This PR fixes this by modifying the logic of calculating the CSS style
in the `EnvironmentChip` component (which consequently determines the
CSS style for the color) to be similar to that of the `EnvironmentCard`
component by referring to the `priority` field of the group of the
environment rather than the environment itself.

Now it looks like this:

| ![](https://i.imgur.com/lj59pGu.png)|
| :---: |
| _Figure 2: the colors now_ |

services/frontend-service/src/ui/components/ReleaseDialog/ReleaseDialog.tsx
@@ -71,6 +71,7 @@ export const AppLock: React.FC<{
 
 export type EnvironmentListItemProps = {
     env: Environment;
+    envGroup: EnvironmentGroup;
     app: string;
     release: Release;
     queuedVersion: number;
@@ -103,6 +104,7 @@ const DeployedVersion: React.FC<CommitIdProps> = ({ application, app, env, other
 
 export const EnvironmentListItem: React.FC<EnvironmentListItemProps> = ({
     env,
+    envGroup,
     app,
     release,
     queuedVersion,
@@ -185,6 +187,7 @@ export const EnvironmentListItem: React.FC<EnvironmentListItemProps> = ({
                 <EnvironmentChip
                     env={env}
                     app={app}
+                    envGroup={envGroup}
                     className={'release-environment'}
                     key={env.name}
                     groupNameOverride={undefined}
@@ -258,6 +261,7 @@ export const EnvironmentList: React.FC<{
                         <EnvironmentListItem
                             key={env.name}
                             env={env}
+                            envGroup={envGroup}
                             app={app}
                             release={release}
                             className={className}

services/frontend-service/src/ui/components/ReleaseDialog/__snapshots__/ReleaseDialog.test.tsx.snap
@@ -84,7 +84,7 @@ exports[`Release Dialog Renders the environment locks normal release 1`] = `
                   class="env-card-header"
                 >
                   <div
-                    class="mdc-evolution-chip release-environment environment-priority-upstream"
+                    class="mdc-evolution-chip release-environment environment-priority-unrecognized"
                     role="row"
                   >
                     <span
@@ -352,7 +352,7 @@ exports[`Release Dialog Renders the environment locks normal release with deploy
                   class="env-card-header"
                 >
                   <div
-                    class="mdc-evolution-chip release-environment environment-priority-upstream"
+                    class="mdc-evolution-chip release-environment environment-priority-unrecognized"
                     role="row"
                   >
                     <span
@@ -626,7 +626,7 @@ exports[`Release Dialog Renders the environment locks two envs release 1`] = `
                   class="env-card-header"
                 >
                   <div
-                    class="mdc-evolution-chip release-environment environment-priority-upstream"
+                    class="mdc-evolution-chip release-environment environment-priority-unrecognized"
                     role="row"
                   >
                     <span
@@ -808,7 +808,7 @@ exports[`Release Dialog Renders the environment locks two envs release 1`] = `
                   class="env-card-header"
                 >
                   <div
-                    class="mdc-evolution-chip release-environment environment-priority-upstream"
+                    class="mdc-evolution-chip release-environment environment-priority-unrecognized"
                     role="row"
                   >
                     <span

services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.tsx
@@ -14,7 +14,7 @@ along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>
 
 Copyright 2023 freiheit.com*/
 import classNames from 'classnames';
-import { Environment } from '../../../api/api';
+import { Environment, EnvironmentGroup } from '../../../api/api';
 import React from 'react';
 import { EnvironmentGroupExtended, getPriorityClassName, useCurrentlyDeployedAtGroup } from '../../utils/store';
 import { LocksWhite } from '../../../images';
@@ -41,6 +41,7 @@ export const AppLockSummary: React.FC<{
 export type EnvironmentChipProps = {
     className: string;
     env: Environment;
+    envGroup: EnvironmentGroup;
     app: string;
     groupNameOverride?: string;
     numberEnvsDeployed?: number;
@@ -49,8 +50,8 @@ export type EnvironmentChipProps = {
 };
 
 export const EnvironmentChip = (props: EnvironmentChipProps): JSX.Element => {
-    const { className, env, smallEnvChip, app } = props;
-    const priorityClassName = getPriorityClassName(env);
+    const { className, env, envGroup, smallEnvChip, app } = props;
+    const priorityClassName = getPriorityClassName(envGroup);
     const name = props.groupNameOverride ? props.groupNameOverride : env.name;
     const numberString =
         props.numberEnvsDeployed && props.numberEnvsInGroup
@@ -107,6 +108,7 @@ export const EnvironmentGroupChip = (props: {
                 <EnvironmentChip
                     className={className}
                     env={envGroup.environments[0]}
+                    envGroup={envGroup}
                     app={app}
                     groupNameOverride={envGroup.environmentGroupName}
                     numberEnvsDeployed={envGroup.environments.length}
@@ -121,6 +123,7 @@ export const EnvironmentGroupChip = (props: {
         <EnvironmentChip
             className={className}
             env={envGroup.environments[0]}
+            envGroup={envGroup}
             app={app}
             groupNameOverride={undefined}
             numberEnvsDeployed={1}

services/frontend-service/src/ui/components/ReleaseDialog/ReleaseDialog.test.tsx
@@ -16,7 +16,7 @@ Copyright 2023 freiheit.com*/
 import { EnvironmentListItem, ReleaseDialog, ReleaseDialogProps } from './ReleaseDialog';
 import { fireEvent, render } from '@testing-library/react';
 import { UpdateAction, UpdateOverview, UpdateRolloutStatus, UpdateSidebar } from '../../utils/store';
-import { Environment, Priority, Release, RolloutStatus, UndeploySummary } from '../../../api/api';
+import { Environment, EnvironmentGroup, Priority, Release, RolloutStatus, UndeploySummary } from '../../../api/api';
 import { Spy } from 'spy4js';
 import { SideBar } from '../SideBar/SideBar';
 import { MemoryRouter } from 'react-router-dom';
@@ -36,6 +36,7 @@ describe('Release Dialog', () => {
         props: ReleaseDialogProps;
         rels: Release[];
         envs: Environment[];
+        envGroups: EnvironmentGroup[];
         expect_message: boolean;
         expect_queues: number;
         data_length: number;
@@ -52,6 +53,7 @@ describe('Release Dialog', () => {
         props: ReleaseDialogProps;
         rels: Release[];
         envs: Environment[];
+        envGroups: EnvironmentGroup[];
         expect_message: boolean;
         expect_queues: number;
         data_length: number;
@@ -90,6 +92,15 @@ describe('Release Dialog', () => {
                         },
                     },
                     distanceToUpstream: 0,
+                    priority: Priority.UPSTREAM,
+                },
+            ],
+            envGroups: [
+                {
+                    // this data should never appear (group with no envs with a well-defined priority), but we'll make it for the sake of the test.
+                    distanceToUpstream: 0,
+                    environmentGroupName: 'prod',
+                    environments: [],
                     priority: Priority.UPSTREAM,
                 },
             ],
@@ -135,6 +146,15 @@ describe('Release Dialog', () => {
                     priority: Priority.UPSTREAM,
                 },
             ],
+            envGroups: [
+                {
+                    // this data should never appear (group with no envs with a well-defined priority), but we'll make it for the sake of the test.
+                    distanceToUpstream: 0,
+                    environmentGroupName: 'prod',
+                    environments: [],
+                    priority: Priority.UPSTREAM,
+                },
+            ],
             expect_message: true,
             expect_queues: 0,
             data_length: 2,
@@ -173,6 +193,15 @@ describe('Release Dialog', () => {
                         },
                     },
                     distanceToUpstream: 0,
+                    priority: Priority.UPSTREAM,
+                },
+            ],
+            envGroups: [
+                {
+                    // this data should never appear (group with no envs with a well-defined priority), but we'll make it for the sake of the test.
+                    distanceToUpstream: 0,
+                    environmentGroupName: 'prod',
+                    environments: [],
                     priority: Priority.UPSTREAM,
                 },
             ],
@@ -216,6 +245,15 @@ describe('Release Dialog', () => {
                         },
                     },
                     distanceToUpstream: 0,
+                    priority: Priority.UPSTREAM,
+                },
+            ],
+            envGroups: [
+                {
+                    // this data should never appear (group with no envs with a well-defined priority), but we'll make it for the sake of the test.
+                    distanceToUpstream: 0,
+                    environmentGroupName: 'prod',
+                    environments: [],
                     priority: Priority.UPSTREAM,
                 },
             ],
@@ -279,6 +317,7 @@ describe('Release Dialog', () => {
                 },
             ],
             envs: [],
+            envGroups: [],
             expect_message: false,
             expect_queues: 0,
             data_length: 0,
@@ -418,6 +457,7 @@ describe('Release Dialog', () => {
                 render(
                     <EnvironmentListItem
                         env={testcase.envs[0]}
+                        envGroup={testcase.envGroups[0]}
                         app={testcase.props.app}
                         queuedVersion={0}
                         release={{ ...testcase.rels[0], version: 3 }}
@@ -463,6 +503,7 @@ describe('Release Dialog', () => {
             render(
                 <EnvironmentListItem
                     env={testcase.envs[0]}
+                    envGroup={testcase.envGroups[0]}
                     app={testcase.props.app}
                     queuedVersion={0}
                     release={testcase.rels[0]}

services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.test.tsx
@@ -15,7 +15,7 @@ along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>
 Copyright 2023 freiheit.com*/
 import { EnvironmentChip, EnvironmentChipProps, EnvironmentGroupChip } from './EnvironmentGroupChip';
 import { fireEvent, render } from '@testing-library/react';
-import { Environment, Lock, Priority } from '../../../api/api';
+import { Environment, EnvironmentGroup, Lock, Priority } from '../../../api/api';
 import { EnvironmentGroupExtended, UpdateOverview } from '../../utils/store';
 import { Spy } from 'spy4js';
 
@@ -34,8 +34,14 @@ describe('EnvironmentChip', () => {
         locks: {},
         applications: {},
     };
+    const envGroup: EnvironmentGroup = {
+        distanceToUpstream: 0,
+        environments: [env],
+        environmentGroupName: 'Test Me Group',
+        priority: Priority.PROD,
+    };
     const getNode = (overloads?: Partial<EnvironmentChipProps>) => (
-        <EnvironmentChip app="app2" className={'chip--test'} env={env} {...overloads} />
+        <EnvironmentChip app="app2" className={'chip--test'} env={env} envGroup={envGroup} {...overloads} />
     );
     const getWrapper = (overloads?: Partial<EnvironmentChipProps>) => render(getNode(overloads));
     it('renders a chip', () => {
@@ -139,53 +145,69 @@ describe('EnvironmentChip', () => {
     });
 });
 
-const envFromPrio = (prio: Priority): Environment => ({
-    name: 'Test Me',
-    distanceToUpstream: 0,
-    priority: prio,
-    locks: {},
-    applications: {},
-});
+const envGroupPairFromPrios = (
+    envPrio: Priority,
+    envGroupPrio: Priority
+): { env: Environment; envGroup: EnvironmentGroup } => {
+    const env: Environment = {
+        applications: {},
+        distanceToUpstream: -1, // shouldn't matter, if this value is used an error will be thrown
+        locks: {},
+        name: 'Test me',
+        priority: envPrio,
+    };
+    const envGroup: EnvironmentGroup = {
+        distanceToUpstream: -1, // shouldn't matter, if this value is used an error will be thrown
+        environmentGroupName: 'Test me group',
+        environments: [env],
+        priority: envGroupPrio,
+    };
+
+    return { env, envGroup };
+};
 
 type TestDataEnvs = {
-    env: Environment;
+    envGroupPair: {
+        env: Environment;
+        envGroup: EnvironmentGroup;
+    };
     expectedClass: string;
 };
 
 const envChipData: Array<TestDataEnvs> = [
     {
-        env: envFromPrio(Priority.PROD),
+        envGroupPair: envGroupPairFromPrios(Priority.PROD, Priority.PROD),
         expectedClass: 'prod',
     },
     {
-        env: envFromPrio(Priority.PRE_PROD),
+        envGroupPair: envGroupPairFromPrios(Priority.PRE_PROD, Priority.PRE_PROD),
         expectedClass: 'pre_prod',
     },
     {
-        env: envFromPrio(Priority.UPSTREAM),
+        envGroupPair: envGroupPairFromPrios(Priority.UPSTREAM, Priority.UPSTREAM),
         expectedClass: 'upstream',
     },
     {
-        env: envFromPrio(Priority.OTHER),
+        envGroupPair: envGroupPairFromPrios(Priority.OTHER, Priority.OTHER),
         expectedClass: 'other',
+    },
+    {
+        // important case: env and group have different priorities, the priority of the group should take precedence
+        envGroupPair: envGroupPairFromPrios(Priority.UPSTREAM, Priority.PROD),
+        expectedClass: 'prod',
+    },
+    {
+        // important case: env and group have different priorities, the priority of the group should take precedence
+        envGroupPair: envGroupPairFromPrios(Priority.PRE_PROD, Priority.CANARY),
+        expectedClass: 'canary',
     },
 ];
 
 describe.each(envChipData)(`EnvironmentChip with envPrio Classname`, (testcase) => {
-    it(`with envPrio=${testcase.env.priority}`, () => {
-        // given
-        UpdateOverview.set({
-            environmentGroups: [
-                {
-                    environments: [testcase.env],
-                    environmentGroupName: 'dontcare',
-                    distanceToUpstream: 0,
-                    priority: Priority.UNRECOGNIZED,
-                },
-            ],
-        });
-        // then
-        const getNode = () => <EnvironmentChip app="app1" className={'chip--hello'} env={testcase.env} />;
+    it(`with envPrio=${testcase.envGroupPair.env.priority} and groupPrio=${testcase.envGroupPair.envGroup.priority}`, () => {
+        const env = testcase.envGroupPair.env;
+        const group = testcase.envGroupPair.envGroup;
+        const getNode = () => <EnvironmentChip app="app1" className={'chip--hello'} env={env} envGroup={group} />;
         const getWrapper = () => render(getNode());
         const { container } = getWrapper();
         expect(container.firstChild).toHaveClass(
@@ -208,6 +230,14 @@ type TestDataGroups = {
     expectedNumbers: string;
     expectedDisplayName: string;
 };
+
+const envFromPrio = (prio: Priority): Environment => ({
+    name: 'Test Me',
+    distanceToUpstream: 0,
+    priority: prio,
+    locks: {},
+    applications: {},
+});
 
 const envGroupChipData: Array<TestDataGroups> = [
     {