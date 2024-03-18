03b862d63b1ee21e0f0d38a96c3165d35475f60c
Miguel Crespo <miguel.crespo@freiheit.com>
2024-03-18 17:23:56 +0000

    Local now uses the rest endpoint to create test data

--- infrastructure/scripts/create-testdata/create-environments.sh
+++ infrastructure/scripts/create-testdata/create-environments.sh
@@ -7,99 +7,20 @@ set -o pipefail
 # ./create-release.sh my-service-name [my-team-name]
 # Note that this just creates files, it doesn't push in git
 
-configFile=$1
-applicationOwnerTeam=${2:-sreteam}
-name=${2:-sreteam}
-commit_id=$(LC_CTYPE=C tr -dc a-f0-9 </dev/urandom | head -c 40 ; echo '')
-authors[0]="urbansky"
-authors[1]="Medo"
-authors[2]="Hannes"
-authors[3]="Mouhsen"
-authors[4]="Tamer"
-authors[5]="Ahmed"
-authors[6]="João"
-authors[7]="Leandro"
-sizeAuthors=${#authors[@]}
-index=$(($RANDOM % $sizeAuthors))
-echo ${authors[$index]}
-author="${authors[$index]}"
-commit_message_file="$(mktemp "${TMPDIR:-/tmp}/publish.XXXXXX")"
-trap "rm -f ""$commit_message_file" INT TERM HUP EXIT
-displayVersion=
-if ((RANDOM % 2)); then
-  displayVersion=$(( $RANDOM % 100)).$(( $RANDOM % 100)).$(( $RANDOM % 100))
-fi
 
-msgs[0]="Added new eslint rule"
-msgs[1]="Fix annotations in helm templates"
-msgs[2]="Improve performance with gitLib2"
-msgs[3]="Add colors to new UI"
-msgs[4]="Release trains for env groups"
-msgs[5]="Fix whitespace in ReleaseDialog"
-msgs[6]="Add Snackbar Notifications"
-msgs[7]="Rephrase ReleaseDialog text"
-msgs[8]="Change renovate schedule"
-msgs[9]="Fix bug in distanceToUpstream calculation"
-msgs[10]="Allow deleting locks on locks page"
-sizeMsgs=${#msgs[@]}
-index=$(($RANDOM % $sizeMsgs))
-echo $index
-echo "${msgs[$index]}" > "${commit_message_file}"
-
-ls "${commit_message_file}"
-
-release_version=''
-case "${RELEASE_VERSION:-}" in
-	'') echo "No RELEASE_VERSION set. Using non-idempotent versioning scheme";;
-	*[!0-9]*) echo "Please set the env variable RELEASE_VERSION to a number"; exit 1;;
-	*) release_version='--form-string '"version=${RELEASE_VERSION:-}";;
-esac
-
-echo "release version:" "${release_version}"
-
-configuration=()
-configuration+=("--form" "team=${applicationOwnerTeam}")
-
-manifests=()
-for env in development_1
-do
-  file=$(mktemp "${TMPDIR:-/tmp}/$env.XXXXXX")
-  signatureFile=$(mktemp "${TMPDIR:-/tmp}/$env.XXXXXX")
-  randomValue=$(LC_CTYPE=C tr -dc a-f0-9 </dev/urandom | head -c 12 ; echo '')
-cat <<EOF > "${file}"
----
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: $name-dummy-config-map
-  namespace: "$env"
-data:
-  key: value
-  random: "${randomValue}"
----
-EOF
-  echo "wrote file ${file}"
-  manifests+=("--form" "manifests[${env}]=@${file}")
-  #gpg  --keyring trustedkeys-kuberpult.gpg --local-user kuberpult-kind@example.com --detach --sign --armor < "${file}" > "${signatureFile}"
-  manifests+=("--form" "signatures[${env}]=@${signatureFile}")
-done
-echo commit id: "${commit_id}"
 
 FRONTEND_PORT=8081 # see docker-compose.yml
 
-if [[ $(uname -o) == Darwin ]];
-then
-  EMAIL=$(echo -n "script-user@example.com" | base64 -b0)
-  AUTHOR=$(echo -n "script-user" | base64 -b0)
-else
-  EMAIL=$(echo -n "script-user@example.com" | base64 -w 0)
-  AUTHOR=$(echo -n "script-user" | base64 -w 0)
-fi
-
-DATA=$(cat $1)
-curl  -f -X POST -H "multipart/form-data" \
-      --form-string "config=${DATA}" \
-       http://localhost:${FRONTEND_PORT}/environments/development_1
+testData=${1:-"./testdata_template/environments"}
+for filename in "$testData"/*; do
+  configFile="$filename"/config.json
+  env=$(basename -- "$filename")
+  echo Writing $env...
+  DATA=$(cat $configFile)
+  curl  -f -X POST -H "multipart/form-data" \
+        --form-string "config=${DATA}" \
+         http://localhost:${FRONTEND_PORT}/environments/${env}
+done
 
 
 echo # curl sometimes does not print a trailing \n
