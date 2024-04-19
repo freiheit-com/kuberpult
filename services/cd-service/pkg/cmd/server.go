21bb810225da39f06c345a1cc4769fda8007c164
Miguel Crespo <miguel.crespo@freiheit.com>
2024-04-19 18:51:07 +0200

    logging

--- services/cd-service/pkg/cmd/server.go
+++ services/cd-service/pkg/cmd/server.go
@@ -207,6 +207,7 @@ func RunServer() {
 					logger.FromContext(ctx).Fatal("Error actually pinging DB: ", zap.Error(pErr))
 				}
 				logger.FromContext(ctx).Info("Pong!")
+				db.Close()
 			} else {
 				migErr := repository.RunDBMigrations(c.DbLocation)
 				if migErr != nil {
@@ -226,6 +227,8 @@ func RunServer() {
 					}
 				}
 			}
+		} else {
+			logger.FromContext(ctx).Info("DB not enabled!")
 		}
 		cfg := repository.RepositoryConfig{
 			WebhookResolver: nil,
