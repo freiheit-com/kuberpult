c1ae6681a247c6af68347b9c70268ec7830e39e2
Miguel Crespo <miguel.crespo@freiheit.com>
2024-04-23 15:22:34 +0200

    Revert "ssl disable"
    This reverts commit 04a4c28b013b971c1d8602f936b86c57952071da.

--- services/cd-service/pkg/repository/db.go
+++ services/cd-service/pkg/repository/db.go
@@ -42,7 +42,7 @@ func GetRemoteConnection() (*sql.DB, error) {
 		dbName    = "test-service" // e.g. 'my-database'
 	)
 
-	dbURI := fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s sslmode=disable",
+	dbURI := fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s",
 		dbTCPHost, dbUser, "", dbPort, dbName)
 
 	dbPool, err := sql.Open("postgres", dbURI)
