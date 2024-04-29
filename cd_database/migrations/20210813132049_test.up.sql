c6a031cc4b8b879243d53cae2a6b5680ec4585d1
Sven Urbanski <sven.urbanski@freiheit.com>
2024-04-29 18:19:52 +0200

    chore: write all apps to database (#1557)
    This replaces the dummy sql table with the "all apps" table.
    During startup, we write to the all apps table by reading the manifest
    repo.
    This PR does not use the all apps table yet for reading.
    Ref: SRX-T8YG6D

--- cd_database/migrations/20210813132049_test.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS dummy_table
-(   id BIGINT,
-    created TIMESTAMP,
-    data VARCHAR(255),
-    PRIMARY KEY(id)
-);
-
-INSERT INTO dummy_table (id , created , data)  VALUES (0, 	"1709627400", "First Message");
-INSERT INTO dummy_table (id , created , data)  VALUES (1, 	"1709627400", "Second Message");
