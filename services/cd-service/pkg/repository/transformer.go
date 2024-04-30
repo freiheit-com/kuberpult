84aebb3c6954689989546454aa6b9410e363c3d1
Sven Urbanski <sven.urbanski@freiheit.com>
2024-04-30 17:44:48 +0200

    chore: rename dbhandler

--- services/cd-service/pkg/repository/transformer.go
+++ services/cd-service/pkg/repository/transformer.go
@@ -401,15 +401,15 @@ func (c *CreateApplicationVersion) Transform(
 	if !valid.ApplicationName(c.Application) {
 		return "", GetCreateReleaseAppNameTooLong(c.Application, valid.AppNameRegExp, uint32(valid.MaxAppNameLen))
 	}
-	if state.DB != nil {
-		err = state.DB.WithTransaction(ctx, func(ctx context.Context) error {
-			allApps, err := state.DB.DBSelectAllApplications(ctx)
+	if state.DBHandler != nil {
+		err = state.DBHandler.WithTransaction(ctx, func(ctx context.Context) error {
+			allApps, err := state.DBHandler.DBSelectAllApplications(ctx)
 			if err != nil {
 				return GetCreateReleaseGeneralFailure(err)
 			}
 			if !slices.Contains(allApps.Apps, c.Application) {
 				allApps.Apps = append(allApps.Apps, c.Application)
-				err := state.DB.DBWriteAllApplications(ctx, allApps.Version, allApps.Apps)
+				err := state.DBHandler.DBWriteAllApplications(ctx, allApps.Version, allApps.Apps)
 				if err != nil {
 					return GetCreateReleaseGeneralFailure(err)
 				}
@@ -1356,7 +1356,7 @@ func (c *DeleteEnvironmentLock) Transform(
 		BootstrapMode:          false,
 		EnvironmentConfigsPath: "",
 		Filesystem:             fs,
-		DB:                     state.DB,
+		DBHandler:              state.DBHandler,
 	}
 	lockDir := s.GetEnvLockDir(c.Environment, c.LockId)
 	_, err = fs.Stat(lockDir)
@@ -1534,7 +1534,7 @@ func (c *DeleteEnvironmentApplicationLock) Transform(
 		BootstrapMode:          false,
 		EnvironmentConfigsPath: "",
 		Filesystem:             fs,
-		DB:                     state.DB,
+		DBHandler:              state.DBHandler,
 	}
 	if err := s.DeleteAppLockIfEmpty(ctx, c.Environment, c.Application); err != nil {
 		return "", err
@@ -1660,7 +1660,7 @@ func (c *DeleteEnvironmentTeamLock) Transform(
 		BootstrapMode:          false,
 		EnvironmentConfigsPath: "",
 		Filesystem:             fs,
-		DB:                     state.DB,
+		DBHandler:              state.DBHandler,
 	}
 	if err := s.DeleteTeamLockIfEmpty(ctx, c.Environment, c.Team); err != nil {
 		return "", err
@@ -1928,7 +1928,7 @@ func (c *DeployApplicationVersion) Transform(
 		BootstrapMode:          false,
 		EnvironmentConfigsPath: "",
 		Filesystem:             fs,
-		DB:                     state.DB,
+		DBHandler:              state.DBHandler,
 	}
 	err = s.DeleteQueuedVersionIfExists(c.Environment, c.Application)
 	if err != nil {
