521e19c9f34420d7d3ad47daa245896f2224f879
Miguel Crespo <miguel.crespo@freiheit.com>
2024-05-02 17:13:17 +0100

    Fixed dialog

--- services/frontend-service/src/ui/components/EnvironmentLockDisplay/EnvironmentLockDisplay.tsx
+++ services/frontend-service/src/ui/components/EnvironmentLockDisplay/EnvironmentLockDisplay.tsx
@@ -23,16 +23,27 @@ export const DisplayLockInlineRenderer: React.FC<{ lock: DisplayLock }> = (props
     const { lock } = props;
     const hasAuthor = lock.authorEmail || lock.authorName;
     const author = hasAuthor ? lock.authorName + '<' + lock.authorEmail + '>' : 'unknown';
+    let description: JSX.Element;
+    if (lock.application) {
+        description = (
+            <span>
+                Application <b>{lock.application}</b> locked by <b>{author}</b> on environment <b>{lock.environment}</b>
+            </span>
+        );
+    } else if (lock.team) {
+        description = (
+            <span>
+                Team <b>{lock.team}</b> locked by <b>{author}</b> on environment <b>{lock.environment}</b>
+            </span>
+        );
+    } else {
+        description = (
+            <span>
+                Application <b>{lock.application}</b> locked by <b>{author}</b> on environment <b>{lock.environment}</b>
+            </span>
+        );
+    }
 
-    const description = lock.application ? (
-        <span>
-            Application <b>{lock.application}</b> locked by <b>{author}</b> on environment <b>{lock.environment}</b>
-        </span>
-    ) : (
-        <span>
-            Environment <b>{lock.environment}</b> locked by <b>{author}</b>
-        </span>
-    );
     return (
         <span title={lock.lockId}>
             {description}
