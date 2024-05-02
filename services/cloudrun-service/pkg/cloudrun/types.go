1467368de8f6734b4d4802809e0492b0059d0b31
Ahmed Nour <125875466+ahmed-nour-fdc@users.noreply.github.com>
2024-05-02 10:52:34 +0200

    feat: Create cloudrun-service (#1555)
    * The service currently deploys a hard-coded service.
    * The service waits for the deployment to complete and reports back the
    status of the deployed service.

---
+++ services/cloudrun-service/pkg/cloudrun/types.go
@@ -0,0 +1,63 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright 2023 freiheit.com*/
+
+package cloudrun
+
+import "fmt"
+
+type ServiceReadyCondition struct {
+	Name     string
+	Revision int64
+	Status   string
+	Reason   string
+	Message  string
+}
+
+func (c ServiceReadyCondition) String() string {
+	return fmt.Sprintf("Service:%s, ObservedGeneration:%d, Ready:%s, Reason:%s, Message:%s", c.Name, c.Revision, c.Status, c.Reason, c.Message)
+}
+
+type serviceConfigError struct {
+	name             string
+	namespaceMissing bool
+	locationMissing  bool
+}
+
+func (p serviceConfigError) Error() string {
+	if p.namespaceMissing {
+		return fmt.Sprintf("Namespace is missing for service: %s", p.name)
+	}
+	if p.locationMissing {
+		return fmt.Sprintf("location is missing for service: %s", p.name)
+	}
+	return "Configuration error"
+}
+
+type operationIdMissingError struct {
+	name string
+}
+
+func (p operationIdMissingError) Error() string {
+	return fmt.Sprintf("failed to get operation-id for service %s", p.name)
+}
+
+type serviceReadyConditionError struct {
+	name string
+}
+
+func (p serviceReadyConditionError) Error() string {
+	return fmt.Sprintf("failed to get Ready status for service %s", p.name)
+}
