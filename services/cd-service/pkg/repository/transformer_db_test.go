039d906934e14a451fb2facb684d41b4531ad62f
Sven Urbanski <sven.urbanski@freiheit.com>
2024-05-17 16:47:58 +0200

    fix: introduce generated sequence for esl table (#1615)
    This is necessary because otherwise there is not total order on the rows
    of the esl table.
    
    Unfortunately, it is note possible to write an SQL statement that
    creates a sequence and works for both sqlite and postgres.
    
    Therefore, we split the migrations into sqlite and postgres and work
    with symlinks for cases where the SQL statements are the same - which is
    most of the time.

--- services/cd-service/pkg/repository/transformer_db_test.go
+++ services/cd-service/pkg/repository/transformer_db_test.go
@@ -26,29 +26,10 @@ import (
 	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/config"
 	"github.com/freiheit-com/kuberpult/services/cd-service/pkg/repository/testutil"
 	"google.golang.org/protobuf/testing/protocmp"
-	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
-
-// CreateMigrationsPath detects if it's running withing earthly/CI or locally and adapts the path to the migrations accordingly
-func CreateMigrationsPath() (string, error) {
-	const subDir = "/database/migrations"
-	_, err := os.Stat("/kp")
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			wd, err := os.Getwd()
-			if err != nil {
-				return "", err
-			}
-			// this ".." sequence is necessary, because Getwd() returns the path of this go file (when running in an idea like goland):
-			return wd + "/../../../.." + subDir, nil
-		}
-		return "", err
-	}
-	return "/kp" + subDir, nil
-}
 
 func TestTransformerWritesEslDataRoundTrip(t *testing.T) {
 	setupTransformers := []Transformer{
@@ -296,7 +277,7 @@ func TestTransformerWritesEslDataRoundTrip(t *testing.T) {
 		},
 	}
 
-	dir, err := CreateMigrationsPath()
+	dir, err := testutil.CreateMigrationsPath()
 	if err != nil {
 		t.Fatalf("setup error could not detect dir \n%v", err)
 		return
