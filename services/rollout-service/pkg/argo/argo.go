147584f698a17bc7b0311e94082ab35d58949cd0
Leandro Salgado <leandro.salgado_ext@mail.schwarz>
2024-05-17 11:26:43 +0100

    added exclude for the unexported field

--- services/rollout-service/pkg/argo/argo.go
+++ services/rollout-service/pkg/argo/argo.go
@@ -175,8 +175,11 @@ func (a ArgoAppProcessor) CreateOrUpdateApp(ctx context.Context, overview *api.G
 
 			//exhaustruct:ignore
 			emptyAppSpec := v1alpha1.ApplicationSpec{}
+			emptyAppSyncPolicy := v1alpha1.SyncPolicy{
+				Automated: &v1alpha1.SyncPolicyAutomated{},
+			}
 			//We have to exclude the unexported type destination and the syncPolicy
-			diff := cmp.Diff(appUpdateRequest.Application.Spec, existingApp.Spec, cmp.AllowUnexported(emptyAppSpec.Destination), cmp.AllowUnexported(v1alpha1.SyncPolicy{}))
+			diff := cmp.Diff(appUpdateRequest.Application.Spec, existingApp.Spec, cmp.AllowUnexported(emptyAppSpec.Destination), cmp.AllowUnexported(emptyAppSyncPolicy))
 			if diff != "" {
 				updateSpan, ctx := tracer.StartSpanFromContext(ctx, "UpdateApplications")
 				updateSpan.SetTag("application", app.Name)
