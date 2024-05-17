f1d73153c066d3a5be451b13c521ced7287ab8ef
Sven Urbanski <sven.urbanski@freiheit.com>
2024-05-17 14:23:47 +0200

    fix: introduce generated sequence for esl table
    This is necessary because otherwise there is not total order on the rows of the esl table.
    Unfortunately, it is note possible to write an SQL statement that works for both sqlite and postgres.
    Therefore, we split the migrations into sqlite and postgres and work with symlinks for cases where the SQL statements are the same - which is most of the time

--- database/migrations/sqlite/1714995407000000_event_sourcing_light.up.sql
+++ database/migrations/sqlite/1714995407000000_event_sourcing_light.up.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS event_sourcing_light -- aka ESL
 (
-    eslId UUID,
+    eslId INTEGER PRIMARY KEY,
     created TIMESTAMP,
     event_type VARCHAR(255),
-    json VARCHAR,
-    PRIMARY KEY(eslId)
+    json VARCHAR
 );
-
