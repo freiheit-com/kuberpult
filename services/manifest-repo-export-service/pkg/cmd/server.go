e39c3b3d7933d6d44d9ed13b5e743c18386bb616
Sven Urbanski <sven.urbanski@freiheit.com>
2024-05-22 17:27:50 +0200

    chore: read esl table in export-service (#1619)

--- services/manifest-repo-export-service/pkg/cmd/server.go
+++ services/manifest-repo-export-service/pkg/cmd/server.go
@@ -18,16 +18,96 @@ package cmd
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"github.com/freiheit-com/kuberpult/pkg/db"
 	"github.com/freiheit-com/kuberpult/pkg/logger"
+	"os"
 )
 
 func RunServer() {
 	err := logger.Wrap(context.Background(), func(ctx context.Context) error {
 		logger.FromContext(ctx).Sugar().Warnf("hello world from the manifest-repo-export-service!")
+
+		dbLocation, err := readEnvVar("KUBERPULT_DB_LOCATION")
+		if err != nil {
+			return err
+		}
+		dbName, err := readEnvVar("KUBERPULT_DB_NAME")
+		if err != nil {
+			return err
+		}
+		dbOption, err := readEnvVar("KUBERPULT_DB_OPTION")
+		if err != nil {
+			return err
+		}
+		dbUserName, err := readEnvVar("KUBERPULT_DB_USER_NAME")
+		if err != nil {
+			return err
+		}
+		dbPassword, err := readEnvVar("KUBERPULT_DB_USER_PASSWORD")
+		if err != nil {
+			return err
+		}
+		dbAuthProxyPort, err := readEnvVar("KUBERPULT_DB_AUTH_PROXY_PORT")
+		if err != nil {
+			return err
+		}
+
+		var dbCfg db.DBConfig
+		if dbOption == "cloudsql" {
+			dbCfg = db.DBConfig{
+				DbHost:         dbLocation,
+				DbPort:         dbAuthProxyPort,
+				DriverName:     "postgres",
+				DbName:         dbName,
+				DbPassword:     dbPassword,
+				DbUser:         dbUserName,
+				MigrationsPath: "",
+				WriteEslOnly:   false,
+			}
+		} else if dbOption == "sqlite" {
+			dbCfg = db.DBConfig{
+				DbHost:         dbLocation,
+				DbPort:         dbAuthProxyPort,
+				DriverName:     "sqlite3",
+				DbName:         dbName,
+				DbPassword:     dbPassword,
+				DbUser:         dbUserName,
+				MigrationsPath: "",
+				WriteEslOnly:   false,
+			}
+		} else {
+			logger.FromContext(ctx).Fatal("Database was enabled but no valid DB option was provided.")
+		}
+		dbHandler, err := db.Connect(dbCfg)
+		if err != nil {
+			return err
+		}
+
+		err = dbHandler.WithTransaction(ctx, func(ctx context.Context, transaction *sql.Tx) error {
+			esl, err := dbHandler.DBReadEslEventInternal(ctx, transaction)
+			if err != nil {
+				return err
+			}
+			logger.FromContext(ctx).Sugar().Warnf("esl event: %v", esl)
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 	if err != nil {
-		fmt.Printf("error in logger.wrap: %v %#v", err, err)
+		fmt.Printf("error in startup: %v %#v", err, err)
 	}
+}
+
+func readEnvVar(envName string) (string, error) {
+	envValue, ok := os.LookupEnv(envName)
+	if !ok {
+		return "", fmt.Errorf("could not read environment variable '%s'", envName)
+	}
+	return envValue, nil
 }
