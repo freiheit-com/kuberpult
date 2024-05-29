24678366580a00f019f061c1179b7fc635246974
Khaled Ismaeel <khaled.ismaeel@freiheit.com>
2024-05-29 13:41:59 +0200

    add page with dummy content

---
+++ services/frontend-service/src/ui/Pages/ReleaseTrain/ReleaseTrainPage.tsx
@@ -0,0 +1,8 @@
+import { TopAppBar } from '../../components/TopAppBar/TopAppBar';
+
+export const ReleaseTrainPage: React.FC = () => (
+    <div>
+        <TopAppBar showAppFilter={false} showTeamFilter={false} showWarningFilter={false} />
+        <main className="main-content">HELLO</main>
+    </div>
+);
