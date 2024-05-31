129d72ea3dd47eb83e9b0050c944b6fdcaecb1b8
Sven Urbanski <sven.urbanski@freiheit.com>
2024-05-31 10:11:43 +0200

    fix(db): add missing env vars for export-service (#1649)
    this fixes missing env vars,
    adds chart tests
    and handles the logging initialization better.
    The logging with `fmt.Printf` did not work on kubernetes, so we now wrap
    the whole "Run" function into the logger.

--- services/manifest-repo-export-service/pkg/cmd/server.go
+++ services/manifest-repo-export-service/pkg/cmd/server.go
@@ -40,168 +40,175 @@ func storageBackend(enableSqlite bool) repository.StorageBackend {
 }
 
 func RunServer() {
-	err := logger.Wrap(context.Background(), func(ctx context.Context) error {
-		dbLocation, err := readEnvVar("KUBERPULT_DB_LOCATION")
+	_ = logger.Wrap(context.Background(), func(ctx context.Context) error {
+		err := Run(ctx)
 		if err != nil {
-			return err
+			logger.FromContext(ctx).Sugar().Errorf("error in startup: %v %#v", err, err)
 		}
-		dbName, err := readEnvVar("KUBERPULT_DB_NAME")
-		if err != nil {
-			return err
-		}
-		dbOption, err := readEnvVar("KUBERPULT_DB_OPTION")
-		if err != nil {
-			return err
-		}
-		dbUserName, err := readEnvVar("KUBERPULT_DB_USER_NAME")
-		if err != nil {
-			return err
-		}
-		dbPassword, err := readEnvVar("KUBERPULT_DB_USER_PASSWORD")
-		if err != nil {
-			return err
-		}
-		dbAuthProxyPort, err := readEnvVar("KUBERPULT_DB_AUTH_PROXY_PORT")
-		if err != nil {
-			return err
-		}
-		gitUrl, err := readEnvVar("KUBERPULT_GIT_URL")
-		if err != nil {
-			return err
-		}
-		gitBranch, err := readEnvVar("KUBERPULT_GIT_BRANCH")
-		if err != nil {
-			return err
-		}
-		gitSshKey, err := readEnvVar("KUBERPULT_GIT_SSH_KEY")
-		if err != nil {
-			return err
-		}
-		gitSshKnownHosts, err := readEnvVar("KUBERPULT_GIT_SSH_KNOWN_HOSTS")
-		if err != nil {
-			return err
-		}
-		// not that this is for the git storage backand, not our database:
-		enableSqliteStorageBackendString, err := readEnvVar("KUBERPULT_ENABLE_SQLITE")
-		if err != nil {
-			return err
-		}
-		enableSqliteStorageBackend := enableSqliteStorageBackendString == "true"
-
-		argoCdGenerateFilesString, err := readEnvVar("KUBERPULT_ARGO_CD_GENERATE_FILES")
-		if err != nil {
-			return err
-		}
-		argoCdGenerateFiles := argoCdGenerateFilesString == "true"
-
-		var dbCfg db.DBConfig
-		if dbOption == "cloudsql" {
-			dbCfg = db.DBConfig{
-				DbHost:         dbLocation,
-				DbPort:         dbAuthProxyPort,
-				DriverName:     "postgres",
-				DbName:         dbName,
-				DbPassword:     dbPassword,
-				DbUser:         dbUserName,
-				MigrationsPath: "",
-				WriteEslOnly:   false,
-			}
-		} else if dbOption == "sqlite" {
-			dbCfg = db.DBConfig{
-				DbHost:         dbLocation,
-				DbPort:         dbAuthProxyPort,
-				DriverName:     "sqlite3",
-				DbName:         dbName,
-				DbPassword:     dbPassword,
-				DbUser:         dbUserName,
-				MigrationsPath: "",
-				WriteEslOnly:   false,
-			}
-		} else {
-			logger.FromContext(ctx).Fatal("Cannot start without DB configuration was provided.")
-		}
-		dbHandler, err := db.Connect(dbCfg)
-		if err != nil {
-			return err
-		}
-
-		err = dbHandler.WithTransaction(ctx, func(ctx context.Context, transaction *sql.Tx) error {
-			cfg := repository.RepositoryConfig{
-				URL:            gitUrl,
-				Path:           "./repository",
-				CommitterEmail: "noemail@example.com", // TODO will be handled in Ref SRX-PA568W
-				CommitterName:  "noname",
-				Credentials: repository.Credentials{
-					SshKey: gitSshKey,
-				},
-				Certificates: repository.Certificates{
-					KnownHostsFile: gitSshKnownHosts,
-				},
-				Branch:                 gitBranch,
-				NetworkTimeout:         120 * time.Second,
-				GcFrequency:            20,
-				BootstrapMode:          false,
-				EnvironmentConfigsPath: "./environment_configs.json",
-				StorageBackend:         storageBackend(enableSqliteStorageBackend),
-
-				ArgoCdGenerateFiles: argoCdGenerateFiles,
-				DBHandler:           dbHandler,
-			}
-
-			repo, err := repository.New(ctx, cfg)
-			if err != nil {
-				return fmt.Errorf("repository.new failed %v", err)
-			}
-
-			log := logger.FromContext(ctx).Sugar()
-			for {
-				err = dbHandler.WithTransaction(ctx, func(ctx context.Context, transaction *sql.Tx) error {
-					eslId, err := cutoff.DBReadCutoff(dbHandler, ctx, transaction)
-					if err != nil {
-						return fmt.Errorf("error in DBReadCutoff %v", err)
-					}
-					if eslId == nil {
-						log.Infof("did not find cutoff")
-					} else {
-						log.Infof("found cutoff: %d", *eslId)
-					}
-					esl, err := readEslEvent(ctx, transaction, eslId, log, dbHandler)
-					if err != nil {
-						return fmt.Errorf("error in readEslEvent %v", err)
-					}
-					if esl == nil {
-						log.Warn("event processing skipped: no esl event found")
-						return nil
-					}
-					transformer, err := processEslEvent(ctx, repo, esl, transaction)
-					if err != nil {
-						return fmt.Errorf("error in processEslEvent %v", err)
-					}
-					if transformer == nil {
-						log.Warn("event processing skipped")
-						return nil
-					}
-					log.Infof("event processed successfully, now writing to cutoff...")
-					err = cutoff.DBWriteCutoff(dbHandler, ctx, transaction, esl.EslId)
-					if err != nil {
-						return fmt.Errorf("error in DBWriteCutoff %v", err)
-					}
-
-					return nil
-				})
-				if err != nil {
-					return fmt.Errorf("error in transaction %v", err)
-				}
-				d := 10 * time.Second
-				log.Infof("sleeping for %v before processing the next event", d)
-				time.Sleep(d)
-			}
-		})
-		return err
+		return nil
 	})
+}
+
+func Run(ctx context.Context) error {
+	logger.FromContext(ctx).Info("Startup")
+
+	dbLocation, err := readEnvVar("KUBERPULT_DB_LOCATION")
 	if err != nil {
-		fmt.Printf("error in startup: %v %#v", err, err)
+		return err
 	}
+	dbName, err := readEnvVar("KUBERPULT_DB_NAME")
+	if err != nil {
+		return err
+	}
+	dbOption, err := readEnvVar("KUBERPULT_DB_OPTION")
+	if err != nil {
+		return err
+	}
+	dbUserName, err := readEnvVar("KUBERPULT_DB_USER_NAME")
+	if err != nil {
+		return err
+	}
+	dbPassword, err := readEnvVar("KUBERPULT_DB_USER_PASSWORD")
+	if err != nil {
+		return err
+	}
+	dbAuthProxyPort, err := readEnvVar("KUBERPULT_DB_AUTH_PROXY_PORT")
+	if err != nil {
+		return err
+	}
+	gitUrl, err := readEnvVar("KUBERPULT_GIT_URL")
+	if err != nil {
+		return err
+	}
+	gitBranch, err := readEnvVar("KUBERPULT_GIT_BRANCH")
+	if err != nil {
+		return err
+	}
+	gitSshKey, err := readEnvVar("KUBERPULT_GIT_SSH_KEY")
+	if err != nil {
+		return err
+	}
+	gitSshKnownHosts, err := readEnvVar("KUBERPULT_GIT_SSH_KNOWN_HOSTS")
+	if err != nil {
+		return err
+	}
+	// not that this is for the git storage backand, not our database:
+	enableSqliteStorageBackendString, err := readEnvVar("KUBERPULT_ENABLE_SQLITE")
+	if err != nil {
+		return err
+	}
+	enableSqliteStorageBackend := enableSqliteStorageBackendString == "true"
+
+	argoCdGenerateFilesString, err := readEnvVar("KUBERPULT_ARGO_CD_GENERATE_FILES")
+	if err != nil {
+		return err
+	}
+	argoCdGenerateFiles := argoCdGenerateFilesString == "true"
+
+	var dbCfg db.DBConfig
+	if dbOption == "cloudsql" {
+		dbCfg = db.DBConfig{
+			DbHost:         dbLocation,
+			DbPort:         dbAuthProxyPort,
+			DriverName:     "postgres",
+			DbName:         dbName,
+			DbPassword:     dbPassword,
+			DbUser:         dbUserName,
+			MigrationsPath: "",
+			WriteEslOnly:   false,
+		}
+	} else if dbOption == "sqlite" {
+		dbCfg = db.DBConfig{
+			DbHost:         dbLocation,
+			DbPort:         dbAuthProxyPort,
+			DriverName:     "sqlite3",
+			DbName:         dbName,
+			DbPassword:     dbPassword,
+			DbUser:         dbUserName,
+			MigrationsPath: "",
+			WriteEslOnly:   false,
+		}
+	} else {
+		logger.FromContext(ctx).Fatal("Cannot start without DB configuration was provided.")
+	}
+	dbHandler, err := db.Connect(dbCfg)
+	if err != nil {
+		return err
+	}
+
+	err = dbHandler.WithTransaction(ctx, func(ctx context.Context, transaction *sql.Tx) error {
+		cfg := repository.RepositoryConfig{
+			URL:            gitUrl,
+			Path:           "./repository",
+			CommitterEmail: "noemail@example.com", // TODO will be handled in Ref SRX-PA568W
+			CommitterName:  "noname",
+			Credentials: repository.Credentials{
+				SshKey: gitSshKey,
+			},
+			Certificates: repository.Certificates{
+				KnownHostsFile: gitSshKnownHosts,
+			},
+			Branch:                 gitBranch,
+			NetworkTimeout:         120 * time.Second,
+			GcFrequency:            20,
+			BootstrapMode:          false,
+			EnvironmentConfigsPath: "./environment_configs.json",
+			StorageBackend:         storageBackend(enableSqliteStorageBackend),
+
+			ArgoCdGenerateFiles: argoCdGenerateFiles,
+			DBHandler:           dbHandler,
+		}
+
+		repo, err := repository.New(ctx, cfg)
+		if err != nil {
+			return fmt.Errorf("repository.new failed %v", err)
+		}
+
+		log := logger.FromContext(ctx).Sugar()
+		for {
+			err = dbHandler.WithTransaction(ctx, func(ctx context.Context, transaction *sql.Tx) error {
+				eslId, err := cutoff.DBReadCutoff(dbHandler, ctx, transaction)
+				if err != nil {
+					return fmt.Errorf("error in DBReadCutoff %v", err)
+				}
+				if eslId == nil {
+					log.Infof("did not find cutoff")
+				} else {
+					log.Infof("found cutoff: %d", *eslId)
+				}
+				esl, err := readEslEvent(ctx, transaction, eslId, log, dbHandler)
+				if err != nil {
+					return fmt.Errorf("error in readEslEvent %v", err)
+				}
+				if esl == nil {
+					log.Warn("event processing skipped: no esl event found")
+					return nil
+				}
+				transformer, err := processEslEvent(ctx, repo, esl, transaction)
+				if err != nil {
+					return fmt.Errorf("error in processEslEvent %v", err)
+				}
+				if transformer == nil {
+					log.Warn("event processing skipped")
+					return nil
+				}
+				log.Infof("event processed successfully, now writing to cutoff...")
+				err = cutoff.DBWriteCutoff(dbHandler, ctx, transaction, esl.EslId)
+				if err != nil {
+					return fmt.Errorf("error in DBWriteCutoff %v", err)
+				}
+
+				return nil
+			})
+			if err != nil {
+				return fmt.Errorf("error in transaction %v", err)
+			}
+			d := 10 * time.Second
+			log.Infof("sleeping for %v before processing the next event", d)
+			time.Sleep(d)
+		}
+	})
+	return err
 }
 
 func readEslEvent(ctx context.Context, transaction *sql.Tx, eslId *db.EslId, log *zap.SugaredLogger, dbHandler *db.DBHandler) (*db.EslEventRow, error) {
