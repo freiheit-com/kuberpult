10d03c08fb1d03a4447ca9f7df028e982f6aad02
Sven Urbanski <sven.urbanski@freiheit.com>
2024-06-14 14:30:42 +0200

    document sql

--- database/migrations/sqlite/1717749007994908_releases.up.sql
+++ database/migrations/sqlite/1717749007994908_releases.up.sql
@@ -4,17 +4,18 @@ CREATE TABLE IF NOT EXISTS releases
     releaseVersion INTEGER, -- release ID given by the client that calls CreateApplicationVersion
     created TIMESTAMP,
     appName VARCHAR,
-    manifests VARCHAR,
-    metadata VARCHAR,
+    manifests VARCHAR, -- json blob, map where each key is an environment and each value is a manifest
+    metadata VARCHAR,  -- json blob about the release, sourceAuthor, sourceCommitId, etc
     PRIMARY KEY(eslVersion, releaseVersion, appName)
 );
 
 
+-- the all_releases exists to get quick access to all releases of one app
 CREATE TABLE IF NOT EXISTS all_releases
 (
     eslVersion INTEGER, -- internal ID for ESL
     created TIMESTAMP,
     appName VARCHAR,
-    metadata VARCHAR,
+    metadata VARCHAR, -- json blob containing all releaseVersions of the given app
     PRIMARY KEY(eslVersion, appName)
 );
