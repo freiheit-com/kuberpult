5c14f069d20f2f019a4f9ab59ce5b9b250cf5a89
Miguel Crespo <miguel.crespo@freiheit.com>
2024-07-05 17:18:30 +0100

    Fixing eslid

--- services/cd-service/pkg/repository/transformer.go
+++ services/cd-service/pkg/repository/transformer.go
@@ -771,6 +771,8 @@ func writeCommitData(ctx context.Context, h *db.DBHandler, transaction *sql.Tx, 
 	if h.ShouldUseEslTable() {
 		gen := getGenerator(ctx)
 		eventUuid := gen.Generate()
+		state.DBHandler.DBReadEslEventInternal(ctx, transaction, false)
+
 		writeError = state.DBHandler.DBWriteNewReleaseEvent(ctx, transaction, transformerEslID, eventUuid, sourceCommitId, ev)
 		fmt.Printf("Transformer eslID: %d\n", transformerEslID)
 		fmt.Printf("uuid: %s\n", eventUuid)
