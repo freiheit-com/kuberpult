9e82666779277b14e822ed5ca623315cfab332c4
Ahmed Nour <ahmed.nour@freiheit.com>
2024-07-10 11:03:32 +0200

    Don't set size for VARCHAR

--- database/migrations/sqlite/1718277940006100_environments.up.sql
+++ database/migrations/sqlite/1718277940006100_environments.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS environments
     created TIMESTAMP,
     version BIGINT,
     name VARCHAR(255),
-    json VARCHAR(1000000),
+    json VARCHAR,
     PRIMARY KEY(name, version)
 );
