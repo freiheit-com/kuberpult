44f8e867f31167ee13f9d7ea2ce679bd52ce5ca0
Amin Salarkia <amin.salarkia@freiheit.com>
2024-07-16 18:34:21 +0200

    feat(ui): add esl warnings page

---
+++ services/frontend-service/src/ui/Pages/EslWarnings/EslWarningsPage.tsx
@@ -0,0 +1,60 @@
+/*This file is part of kuberpult.
+
+Kuberpult is free software: you can redistribute it and/or modify
+it under the terms of the Expat(MIT) License as published by
+the Free Software Foundation.
+
+Kuberpult is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MIT License for more details.
+
+You should have received a copy of the MIT License
+along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>.
+
+Copyright freiheit.com*/
+
+import { getFailedEsls, useFailedEsls, useGlobalLoadingState, FailedEslsState } from '../../utils/store';
+import { TopAppBar } from '../../components/TopAppBar/TopAppBar';
+import { Spinner } from '../../components/Spinner/Spinner';
+import React from 'react';
+import { EslWarnings } from '../../components/EslWarnings/EslWarnings';
+import { useAzureAuthSub } from '../../utils/AzureAuthProvider';
+
+export const EslWarningsPage: React.FC = () => {
+    const { authHeader } = useAzureAuthSub((auth) => auth);
+
+    React.useEffect(() => {
+        getFailedEsls(authHeader);
+    }, [authHeader]);
+
+    const failedEsls = useFailedEsls((res) => res);
+
+    const element = useGlobalLoadingState();
+    if (element) {
+        return element;
+    }
+
+    switch (failedEsls.failedEslsReady) {
+        case FailedEslsState.LOADING:
+            return <Spinner message="Loading commit info" />;
+        case FailedEslsState.ERROR:
+            return (
+                <div>
+                    <TopAppBar showAppFilter={false} showTeamFilter={false} showWarningFilter={false} />
+                    <main className="main-content commit-page">Backend error</main>
+                </div>
+            );
+        case FailedEslsState.NOTFOUND:
+            return (
+                <div>
+                    {/*<TopAppBar showAppFilter={false} showTeamFilter={false} showWarningFilter={false} />*/}
+                    <main className="main-content commit-page">
+                        <p>No failed ESLs found</p>
+                    </main>
+                </div>
+            );
+        case FailedEslsState.READY:
+            return <EslWarnings failedEsls={failedEsls.response} />;
+    }
+};
