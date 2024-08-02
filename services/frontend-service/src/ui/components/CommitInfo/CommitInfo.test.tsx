aaae3b94641ba3cdbd18c6be764c3198651acbdd
João Correia <joao.correia@freiheit.com>
2024-08-02 16:52:25 +0100

    fix: React hook dependency error

--- services/frontend-service/src/ui/components/CommitInfo/CommitInfo.test.tsx
+++ services/frontend-service/src/ui/components/CommitInfo/CommitInfo.test.tsx
@@ -21,7 +21,7 @@ import { GetCommitInfoResponse, LockPreventedDeploymentEvent_LockType } from '..
 test('CommitInfo component does not render commit info when the response is undefined', () => {
     const { container } = render(
         <MemoryRouter>
-            <CommitInfo commitInfo={undefined} triggerLoadMore={null} pageNumber={0} />
+            <CommitInfo commitInfo={undefined} triggerLoadMore={null} />
         </MemoryRouter>
     );
     expect(container.textContent).toContain('Backend returned empty response');
@@ -260,7 +260,7 @@ test('CommitInfo component renders commit info when the response is valid', () =
         );
         const { container } = render(
             <MemoryRouter>
-                <CommitInfo commitInfo={testCase.commitInfo} triggerLoadMore={null} pageNumber={0} />
+                <CommitInfo commitInfo={testCase.commitInfo} triggerLoadMore={null} />
             </MemoryRouter>
         );
 
@@ -450,7 +450,7 @@ describe('CommitInfo component renders next and previous buttons correctly', () 
         it(testCase.name, () => {
             const { container } = render(
                 <MemoryRouter>
-                    <CommitInfo commitInfo={testCase.commitInfo} triggerLoadMore={null} pageNumber={0} />
+                    <CommitInfo commitInfo={testCase.commitInfo} triggerLoadMore={null} />
                 </MemoryRouter>
             );
 
--- services/frontend-service/src/ui/components/CommitInfo/CommitInfo.tsx
+++ services/frontend-service/src/ui/components/CommitInfo/CommitInfo.tsx
@@ -19,18 +19,16 @@ import { GetCommitInfoResponse, Event, LockPreventedDeploymentEvent_LockType } f
 type CommitInfoProps = {
     commitInfo: GetCommitInfoResponse | undefined;
     triggerLoadMore: { (): void } | null;
-    pageNumber: number;
 };
 
 export const CommitInfo: React.FC<CommitInfoProps> = (props) => {
     const commitInfo = props.commitInfo;
     const triggerLoadMore: () => void = props.triggerLoadMore !== null ? props.triggerLoadMore : (): void => {};
     const canLoadMore = props.commitInfo?.loadMore;
-    const pageNumber = props.pageNumber;
 
     const onClick = useCallback(() => {
         triggerLoadMore();
-    }, [pageNumber, triggerLoadMore]);
+    }, [triggerLoadMore]);
 
     if (commitInfo === undefined) {
         return (
