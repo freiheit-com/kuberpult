fed1d6541cad71c528b60698c51ffc2d0cffcbe2
Miguel Crespo <miguel.crespo@freiheit.com>
2024-08-05 11:03:28 +0100

    all_envs

--- database/migrations/postgres/1722702812877934_all_deployments.up.sql
+++ database/migrations/postgres/1722702812877934_all_deployments.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS all_deployments
     eslVersion INTEGER, -- internal ID for ESL
     created TIMESTAMP,
     appName VARCHAR,
-    json VARCHAR,
+    json VARCHAR, -- Stores map from environment to (deployed) release number
     PRIMARY KEY(eslVersion, appName)
 );
--- database/migrations/sqlite/1718277975909620_all_environments.up.sql
+++ database/migrations/sqlite/1718277975909620_all_environments.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS all_environments
 (
     created TIMESTAMP,
     version BIGINT,
-    json VARCHAR,
+    json VARCHAR(255),
     PRIMARY KEY(version)
 );
