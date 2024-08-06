b054edeb505c674c8e12465dfb5f66df41b4ef65
João Correia <joao.correia@freiheit.com>
2024-08-06 11:32:04 +0100

    feat: More efficient custom migrations Read and insert in-place for Commit-Events

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -358,9 +358,8 @@ func TestCustomMigrationsApps(t *testing.T) {
 }
 
 func TestMigrationCommitEvent(t *testing.T) {
-	var getAllCommitEvents = /*getAllCommitEvents*/ func(ctx context.Context) (AllCommitEvents, error) {
-		result := AllCommitEvents{}
-		return result, nil
+	var writeAllCommitEvents = /*writeAllCommitEvents*/ func(ctx context.Context, transaction *sql.Tx, dbHandler *DBHandler) error {
+		return nil
 	}
 	tcs := []struct {
 		Name           string
@@ -383,7 +382,7 @@ func TestMigrationCommitEvent(t *testing.T) {
 					return fmt.Errorf("error: %v", err2)
 				}
 
-				err2 = dbHandler.RunCustomMigrationsCommitEvents(ctx, getAllCommitEvents)
+				err2 = dbHandler.RunCustomMigrationsCommitEvents(ctx, writeAllCommitEvents)
 				if err2 != nil {
 					return fmt.Errorf("error: %v", err2)
 				}
@@ -538,7 +537,7 @@ func writeEventAux(ctx context.Context, db *DBHandler, tx *sql.Tx, sourceCommitH
 	if err != nil {
 		return err
 	}
-	return db.writeEvent(ctx, tx, 0, ev.EventMetadata.Uuid, event.EventType(ev.EventMetadata.EventType), sourceCommitHash, jsonToInsert)
+	return db.WriteEvent(ctx, tx, 0, ev.EventMetadata.Uuid, event.EventType(ev.EventMetadata.EventType), sourceCommitHash, jsonToInsert)
 }
 
 func TestSqliteToPostgresQuery(t *testing.T) {
