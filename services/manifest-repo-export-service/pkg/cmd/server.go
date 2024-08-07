7a27d5eacaa3a9239e8ee5d37b2af0561e1b77f7
Sven Urbanski <sven.urbanski@freiheit.com>
2024-08-07 16:48:28 +0200

    fix

--- services/manifest-repo-export-service/pkg/cmd/server.go
+++ services/manifest-repo-export-service/pkg/cmd/server.go
@@ -320,10 +320,10 @@ func processEsls(ctx context.Context, repo repository.Repository, dbHandler *db.
 				if err2 != nil {
 					d := sleepDuration.NextBackOff()
 					logger.FromContext(ctx).Sugar().Warnf("error pushing, will try again in %v", d)
-					measurePushFailures(ddMetrics, log, true)
+					measurePushes(ddMetrics, log, true)
 					time.Sleep(d)
 				} else {
-					measurePushFailures(ddMetrics, log, false)
+					measurePushes(ddMetrics, log, false)
 				}
 				return err2
 			})
@@ -339,7 +339,7 @@ func processEsls(ctx context.Context, repo repository.Repository, dbHandler *db.
 	}
 }
 
-func measurePushFailures(ddMetrics statsd.ClientInterface, log *zap.SugaredLogger, failure bool) {
+func measurePushes(ddMetrics statsd.ClientInterface, log *zap.SugaredLogger, failure bool) {
 	if ddMetrics != nil {
 		var value float64 = 0
 		if failure {
