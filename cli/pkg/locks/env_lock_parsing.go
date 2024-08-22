95493127e8dd7383867fb4d1863d2446739d9f5b
Miguel Crespo <162333021+miguel-crespo-fdc@users.noreply.github.com>
2024-08-22 13:07:10 +0100

    Update cli/pkg/locks/env_lock_parsing.go
    Co-authored-by: Ahmed Nour <125875466+ahmed-nour-fdc@users.noreply.github.com>

--- cli/pkg/locks/env_lock_parsing.go
+++ cli/pkg/locks/env_lock_parsing.go
@@ -123,7 +123,7 @@ func readDeleteEnvLockArgs(args []string) (*DeleteEnvLockCommandLineArguments, e
 	fs := flag.NewFlagSet("flag set", flag.ContinueOnError)
 
 	fs.Var(&cmdArgs.environment, "environment", "the environment to delete the lock for")
-	fs.Var(&cmdArgs.lockId, "lockID", "the ID of the lock you are trying to create")
+	fs.Var(&cmdArgs.lockId, "lockID", "the ID of the lock you are trying to delete")
 
 	if err := fs.Parse(args); err != nil {
 		return nil, fmt.Errorf("error while parsing command line arguments, error: %w", err)
