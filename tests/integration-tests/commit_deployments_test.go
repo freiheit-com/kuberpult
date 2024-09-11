4fc2acb9b80c5a74db57f419c5c31ca9ead3885b
Ahmed Nour <125875466+ahmed-nour-fdc@users.noreply.github.com>
2024-09-11 15:39:38 +0200

    fix: Use Repeatable Read isolation level for postgres transactions (#1942)
    Ref: SRX-GWLFAS

--- tests/integration-tests/commit_deployments_test.go
+++ tests/integration-tests/commit_deployments_test.go
@@ -23,7 +23,6 @@ import (
 	"net/http"
 	"strings"
 	"testing"
-	"time"
 
 	json "google.golang.org/protobuf/encoding/protojson"
 
@@ -90,8 +89,6 @@ func TestCommitDeployments(t *testing.T) {
 				if status != tc.expectedStatus[staging] {
 					t.Errorf("Deployment status for %s on %s is not as expected\nexpected: %v, got: %v", app, development, tc.expectedStatus[staging], status)
 				}
-
-				time.Sleep(2 * time.Second)
 
 				// Run release train to staging
 				resp, err := runReleaseTrain(staging)
