28906fee14d9882a2d673e155e40944879909a08
Miguel Crespo <miguel.crespo@freiheit.com>
2024-09-11 17:42:04 +0100

    sym link

--- database/migrations/sqlite/1726064901745858_transaction_timestamps.up.sql
+++ database/migrations/sqlite/1726064901745858_transaction_timestamps.up.sql
@@ -1,7 +1,1 @@
--- Table that stores which timestamp
-CREATE TABLE IF NOT EXISTS commit_transaction_timestamps
-(
-    commitHash VARCHAR NOT NULL,
-    transactionTimestamp TIMESTAMP NOT NULL,
-    PRIMARY KEY(commitHash)
-);
+../postgres/1726064901745858_transaction_timestamps.up.sql
