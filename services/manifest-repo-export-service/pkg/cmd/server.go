7773cf1b132358c3a6cf79f0ab9a25e9f8789950
Miguel Crespo <miguel.crespo@freiheit.com>
2024-09-12 09:17:11 +0100

    reduce noise

--- services/manifest-repo-export-service/pkg/cmd/server.go
+++ services/manifest-repo-export-service/pkg/cmd/server.go
@@ -279,7 +279,6 @@ func processEsls(ctx context.Context, repo repository.Repository, dbHandler *db.
 		var transformer repository.Transformer = nil
 		var esl *db.EslEventRow = nil
 		const readonly = true // we just handle the reading here, there's another transaction for writing the result to the db/git
-
 		err := dbHandler.WithTransactionR(ctx, transactionRetries, readonly, func(ctx context.Context, transaction *sql.Tx) error {
 			var err2 error
 			transformer, esl, err2 = handleOneEvent(ctx, transaction, dbHandler, ddMetrics, repo)
@@ -290,7 +289,6 @@ func processEsls(ctx context.Context, repo repository.Repository, dbHandler *db.
 				log.Errorf("skipping esl event, because we could not construct esl object: %v", err)
 				return err
 			}
-
 			log.Errorf("skipping esl event, because it returned an error: %v", err)
 			// after this many tries, we can just skip it:
 			err2 := dbHandler.WithTransactionR(ctx, transactionRetries, false, func(ctx context.Context, transaction *sql.Tx) error {
@@ -305,7 +303,6 @@ func processEsls(ctx context.Context, repo repository.Repository, dbHandler *db.
 			}
 			sleepDuration.Reset()
 		} else {
-
 			if transformer == nil {
 				sleepDuration.Reset()
 				d := sleepDuration.NextBackOff()
