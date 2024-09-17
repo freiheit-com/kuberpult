3f5174a8d8d1ccd973ec4ecf4a3a7ae5ffdcf015
Sven Urbanski <sven.urbanski@freiheit.com>
2024-09-17 13:28:04 +0200

    Revert "fix: Pass context to OverviewServiceServer (#1945)"
    This reverts commit 34b05851642178643fadc603f9f8ca9ad49f530a.

--- services/cd-service/pkg/cmd/server.go
+++ services/cd-service/pkg/cmd/server.go
@@ -404,7 +404,6 @@ func RunServer() {
 						Repository:       repo,
 						RepositoryConfig: cfg,
 						Shutdown:         shutdownCh,
-						Context:          ctx,
 					}
 					api.RegisterOverviewServiceServer(srv, overviewSrv)
 					api.RegisterGitServiceServer(srv, &service.GitServer{Config: cfg, OverviewService: overviewSrv, PageSize: 10})
