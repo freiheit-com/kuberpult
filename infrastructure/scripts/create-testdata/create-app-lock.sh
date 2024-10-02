b8f1223756ec6745f3cdc513049537898fcb8c62
Miguel Crespo <miguel.crespo@freiheit.com>
2024-10-02 10:32:45 +0100

    updates

--- infrastructure/scripts/create-testdata/create-app-lock.sh
+++ infrastructure/scripts/create-testdata/create-app-lock.sh
@@ -3,10 +3,9 @@ set -eu
 set -o pipefail
 set -x
 
-
+env=staging
 lockId=test${RANDOM}
 app=${1}
-env=${2}
 url="http://localhost:8081/environments/${env}/applications/${app}/locks/${lockId}"
 
 curl -X PUT "$url" -d '{"message": "test app lock"}' -H 'Content-Type: application/json'
