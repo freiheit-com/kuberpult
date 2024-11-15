dea2594d77c7f4746a4d9d887a594ba2a010b889
Amin Salarkia <amin.salarkia@freiheit.com>
2024-11-15 13:20:38 +0100

    fix(migrations): make migration idempotent
    Ref: SRX-43OZB7

--- database/migrations/postgres/1731492516426287_applications_autoincrement.up.sql
+++ database/migrations/postgres/1731492516426287_applications_autoincrement.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE IF EXISTS apps ADD COLUMN version INTEGER;
+ALTER TABLE IF EXISTS apps ADD COLUMN IF NOT EXISTS version INTEGER;
 WITH ordered_rows AS (
     SELECT eslversion, appname, ROW_NUMBER() OVER (ORDER BY eslversion) AS row_num
     FROM apps
@@ -8,15 +8,15 @@ SET version = ordered_rows.row_num
 FROM ordered_rows
 WHERE apps.eslversion = ordered_rows.eslversion AND apps.appname = ordered_rows.appname;
 
-CREATE SEQUENCE apps_version_seq OWNED BY apps.version;
+CREATE SEQUENCE IF NOT EXISTS apps_version_seq OWNED BY apps.version;
 
 SELECT setval('apps_version_seq', coalesce(max(version), 0) + 1, false) FROM apps;
 
-ALTER TABLE apps
+ALTER TABLE IF EXISTS apps
 ALTER COLUMN version SET DEFAULT nextval('apps_version_seq');
 
-ALTER TABLE apps DROP CONSTRAINT apps_pkey;
+ALTER TABLE IF EXISTS apps DROP CONSTRAINT IF EXISTS apps_pkey;
 
-ALTER TABLE apps ADD PRIMARY KEY (version, appname);
+ALTER TABLE IF EXISTS apps ADD PRIMARY KEY (version, appname);
 
-ALTER TABLE apps DROP COLUMN eslversion;
+ALTER TABLE IF EXISTS apps DROP COLUMN IF EXISTS eslversion;
