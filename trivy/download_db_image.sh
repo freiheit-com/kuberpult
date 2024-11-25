461aa1bb32ea3ccccde9ec73be96fa255fbca63c
diogo-nogueira-freiheit <diogo.nogueira@freiheit.com>
2024-11-25 11:13:27 +0000

    Fixed earthfile and download script.

--- trivy/download_db_image.sh
+++ trivy/download_db_image.sh
@@ -1,9 +1,15 @@
-#! /bin/bash
-for i in $(seq $1)
-do
+#!/bin/sh
+
+i=0
+status=0
+while [ "$i" -lt $1 ]; do
 	trivy --cache-dir $2 image --download-db-only
-	if [ $? -eq 0 ]; then
+	status=$?
+	if [ $status -eq 0 ]; then
 		break
 	fi
-	sleep 1
+	sleep 2
+    i=$(( i + 1 ))
 done
+
+exit $status
