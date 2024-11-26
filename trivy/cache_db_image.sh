77b5ebc8a04579ec2340c11e5e79b72c821a1b2e
Diogo Nogueira <diogo.nogueira@freiheit.com>
2024-11-26 15:24:35 +0000

    fix(ci): cache trivy (#2130)
    When performing a trivy scan it pulls a db image from GHCR. Because of
    rate limiting the often fails.
    This PR adds a github actions workflow that triggers daily to cache this
    db image in our gcp artifact registry so it can be pulled by other
    workflows to avoid the rate limiting issue.
    
    Ref: SRX-R4I0PO

---
+++ trivy/cache_db_image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+i=0
+status=1
+while [ "$i" -lt $1 ]; do
+	oras cp ghcr.io/aquasecurity/trivy-db:2 europe-west3-docker.pkg.dev/fdc-public-docker-registry/kuberpult/aquasecurity/trivy-db:2
+	status=$?
+	if [ $status -eq 0 ]; then
+		break
+	fi
+	sleep 2
+	i=$(( i + 1 ))
+done
+exit $status
+
