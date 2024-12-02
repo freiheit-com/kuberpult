4b04f5207782764191375c8f04c8783c46ef1062
Amin Salarkia <amin.salarkia@freiheit.com>
2024-12-02 12:48:07 +0100

    fix(cd-service): fix server.go linting
    Ref: SRX-0F9ZN3

--- services/cd-service/pkg/cmd/server.go
+++ services/cd-service/pkg/cmd/server.go
@@ -460,7 +460,7 @@ func RunServer() {
 					reposerver.Register(srv, repo, cfg)
 					if dbHandler != nil {
 						api.RegisterCommitDeploymentServiceServer(srv, &service.CommitDeploymentServer{DBHandler: dbHandler})
-						overviewSrv.GetOverview(ctx, &api.GetOverviewRequest{})
+						_, _ = overviewSrv.GetOverview(ctx, &api.GetOverviewRequest{GitRevision: ""})
 					}
 				},
 			},
