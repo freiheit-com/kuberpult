3f941d17ec1d3bec58bb3f06bed2802b9ba216b0
Amin Salarkia <amin.salarkia@freiheit.com>
2024-12-03 14:50:21 +0100

    fix(cd-service): fix flaky datadog gauge test (#2159)
    Ref: SRX-PXQRS5
    lastDeployed is a float time difference. It can be anywhere between 0
    and 1. For us if it's less than 1 minute is enough.

--- services/cd-service/pkg/repository/transformer_db_test.go
+++ services/cd-service/pkg/repository/transformer_db_test.go
@@ -4147,6 +4147,11 @@ func TestUpdateDatadogMetricsInternal(t *testing.T) {
 				sort.Strings(actualGauge.Tags)
 				t.Logf("actualGauges:[%v] %v:%v", i, actualGauge.Name, actualGauge.Tags)
 				t.Logf("expectedGauges:[%v] %v:%v", i, expectedGauge.Name, expectedGauge.Tags)
+				if actualGauge.Name == "lastDeployed" {
+					if actualGauge.Value < 1 {
+						actualGauge.Value = 0
+					}
+				}
 
 				if diff := cmp.Diff(expectedGauge, actualGauge, cmpopts.IgnoreFields(statsd.Event{}, "Timestamp")); diff != "" {
 					t.Errorf("[%d] want %v, got %v, diff (-want +got) %s", i, expectedGauge, actualGauge, diff)
