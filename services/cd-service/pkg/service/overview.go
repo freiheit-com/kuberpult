978856266737cd79d03e7c3c367e2855c10fe6b3
Miguel Crespo <miguel.crespo@freiheit.com>
2025-01-06 13:13:58 +0000

    conflict

--- services/cd-service/pkg/service/overview.go
+++ services/cd-service/pkg/service/overview.go
@@ -314,13 +314,7 @@ func (o *OverviewServiceServer) GetOverview(
 
 func (o *OverviewServiceServer) GetAllAppLocks(ctx context.Context,
 	in *api.GetAllAppLocksRequest) (*api.GetAllAppLocksResponse, error) {
-<<<<<<< HEAD
-
-=======
-	// Get all Environments
-	// For each environment, get all applications
-	// Get all app locks for that environment
->>>>>>> 0a159ccd (Implement new endpoint for getting all app locks)
+	
 	span, ctx := tracer.StartSpanFromContext(ctx, "GetAllAppLocks")
 	defer span.Finish()
 
