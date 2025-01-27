5d4d0c6fa0b56eeaa076175dadbe7b14d5bcc627
Amin Salarkia <amin.salarkia@freiheit.com>
2025-01-27 10:03:58 +0100

    fix(db): fix app locks migrations
    Ref: SRX-HIDTGM

--- database/migrations/postgres/1737539621939701_new_app_locks.up.sql
+++ database/migrations/postgres/1737539621939701_new_app_locks.up.sql
@@ -1,41 +1,12 @@
+-- Active: 1731876050071@@127.0.0.1@5432@postgres
 SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
--- insert data into app_locks table from app_locks_history table if there's no data inside it
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT FROM information_schema.tables 
-        WHERE table_name = 'app_locks'
-    ) AND NOT EXISTS (
-        SELECT 1 FROM app_locks LIMIT 1
-    ) AND EXISTS (
-        SELECT FROM information_schema.tables 
-        WHERE table_name = 'app_locks_history'
-    ) THEN
-        INSERT INTO app_locks (created, lockId, envname, appname, metadata)
-        SELECT DISTINCT
-            app_locks_history.created,
-            app_locks_history.lockId,
-            app_locks_history.envname,
-            app_locks_history.appname,
-            app_locks_history.metadata
-        FROM (
-            SELECT
-                MAX(version) AS latestVersion,
-                appname,
-                envname,
-                lockid
-            FROM
-                "app_locks_history"
-            GROUP BY
-                appname, envname, lockid) AS latest
-        JOIN
-            app_locks_history AS app_locks_history 
-        ON
-            latest.latestVersion=app_locks_history.version
-            AND latest.appname=app_locks_history.appname
-            AND latest.envname=app_locks_history.envname
-            AND latest.lockid=app_locks_history.lockid
-        WHERE deleted=false;
-    END IF;
-END
-$$;
+-- create app_locks table if it doesn't exist
+CREATE TABLE IF NOT EXISTS app_locks
+(
+    created TIMESTAMP,
+    lockid VARCHAR,
+    envname VARCHAR,
+    appname VARCHAR,
+    metadata VARCHAR,
+    PRIMARY KEY(appname, envname, lockid)
+);
