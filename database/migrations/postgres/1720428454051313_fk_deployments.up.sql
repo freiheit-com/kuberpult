6e2b52f4ab3c6c11a655c8442c250909a769c35d
Miguel Crespo <miguel.crespo@freiheit.com>
2025-02-04 21:53:03 +0000

    inmpotnet

--- database/migrations/postgres/1720428454051313_fk_deployments.up.sql
+++ database/migrations/postgres/1720428454051313_fk_deployments.up.sql
@@ -1,10 +1,2 @@
-DO $$ BEGIN IF NOT EXISTS (
-    SELECT 'fk_deployments_transformer_id'
-    FROM information_schema.table_constraints
-    WHERE table_name = 'deployments'
-        AND constraint_name = 'fk_deployments_transformer_id'
-) THEN
 ALTER TABLE deployments
-ADD COLUMN IF NOT EXISTS transformerEslId INTEGER CONSTRAINT fk_deployments_transformer_id REFERENCES event_sourcing_light(eslId) default 0;
-end if;
-END $$;
+ADD COLUMN IF NOT EXISTS transformerEslId INTEGER default 0;
