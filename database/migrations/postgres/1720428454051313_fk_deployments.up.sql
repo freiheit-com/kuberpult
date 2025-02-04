3e5bb33d3922257cdf83fb050990402fc5604c76
Miguel Crespo <miguel.crespo@freiheit.com>
2025-02-04 22:26:24 +0000

    migration

--- database/migrations/postgres/1720428454051313_fk_deployments.up.sql
+++ database/migrations/postgres/1720428454051313_fk_deployments.up.sql
@@ -1,2 +1,9 @@
+DO $$ BEGIN IF NOT EXISTS (
+    SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name='deployments' and column_name='transformerEslversion'
+) THEN
 ALTER TABLE deployments
 ADD COLUMN IF NOT EXISTS transformerEslId INTEGER default 0;
+end if;
+END $$;
