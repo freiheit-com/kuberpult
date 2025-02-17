690ebfc71bd8ad28128d2317d2ef7a79ae7e15bd
Eduardo Espadeiro <eduardo.espadeiro@freiheit.com>
2025-02-17 16:51:09 +0000

    feat(export-service): add git sync status metrics (#2263)
    Add metrics for git sync status.
    Ref: SRX-IOLM7K

--- services/manifest-repo-export-service/pkg/cmd/server_test.go
+++ services/manifest-repo-export-service/pkg/cmd/server_test.go
@@ -17,11 +17,19 @@ Copyright freiheit.com*/
 package cmd
 
 import (
+	"context"
+	"database/sql"
+	"os/exec"
+	"path"
 	"testing"
 	"time"
 
+	"github.com/DataDog/datadog-go/v5/statsd"
 	"github.com/freiheit-com/kuberpult/pkg/db"
 	"github.com/freiheit-com/kuberpult/pkg/testutil"
+	"github.com/freiheit-com/kuberpult/services/manifest-repo-export-service/pkg/repository"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestCalculateProcessDelay(t *testing.T) {
@@ -70,6 +78,178 @@ func TestCalculateProcessDelay(t *testing.T) {
 			}
 			if delay != tc.ExpectedDelay {
 				t.Errorf("expected %f, got %f", tc.ExpectedDelay, delay)
+			}
+		})
+	}
+}
+
+type Gauge struct {
+	Name  string
+	Value float64
+	Tags  []string
+	Rate  float64
+}
+
+type MockClient struct {
+	gauges []Gauge
+	statsd.ClientInterface
+}
+
+func (c *MockClient) Gauge(name string, value float64, tags []string, rate float64) error {
+	c.gauges = append(c.gauges, Gauge{
+		Name:  name,
+		Value: value,
+		Tags:  tags,
+		Rate:  rate,
+	})
+	return nil
+}
+
+// Verify that MockClient implements the ClientInterface.
+// https://golang.org/doc/faq#guarantee_satisfies_interface
+var _ statsd.ClientInterface = &MockClient{}
+
+func SetupRepositoryTestWithDB(t *testing.T) repository.Repository {
+	r, _ := SetupRepositoryTestWithDBOptions(t, false)
+	return r
+}
+
+func SetupRepositoryTestWithDBOptions(t *testing.T, writeEslOnly bool) (repository.Repository, *db.DBHandler) {
+	ctx := context.Background()
+	migrationsPath, err := testutil.CreateMigrationsPath(4)
+	if err != nil {
+		t.Fatalf("CreateMigrationsPath error: %v", err)
+	}
+	dbConfig := &db.DBConfig{
+		DriverName:     "sqlite3",
+		MigrationsPath: migrationsPath,
+		WriteEslOnly:   writeEslOnly,
+	}
+
+	dir := t.TempDir()
+	remoteDir := path.Join(dir, "remote")
+	localDir := path.Join(dir, "local")
+	cmd := exec.Command("git", "init", "--bare", remoteDir)
+	err = cmd.Start()
+	if err != nil {
+		t.Fatalf("error starting %v", err)
+		return nil, nil
+	}
+	err = cmd.Wait()
+	if err != nil {
+		t.Fatalf("error waiting %v", err)
+		return nil, nil
+	}
+	t.Logf("test created dir: %s", localDir)
+
+	repoCfg := repository.RepositoryConfig{
+		URL:                 remoteDir,
+		Path:                localDir,
+		CommitterEmail:      "kuberpult@freiheit.com",
+		CommitterName:       "kuberpult",
+		ArgoCdGenerateFiles: true,
+	}
+	dbConfig.DbHost = dir
+
+	migErr := db.RunDBMigrations(ctx, *dbConfig)
+	if migErr != nil {
+		t.Fatal(migErr)
+	}
+
+	dbHandler, err := db.Connect(ctx, *dbConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	repoCfg.DBHandler = dbHandler
+
+	repo, err := repository.New(
+		testutil.MakeTestContext(),
+		repoCfg,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return repo, dbHandler
+}
+
+func TestMeasureGitSyncStatus(t *testing.T) {
+	tcs := []struct {
+		Name             string
+		SyncedFailedApps []db.EnvApp
+		UnsyncedApps     []db.EnvApp
+		ExpectedGauges   []Gauge
+	}{
+		{
+			Name:             "No unsynced or sync failed apps",
+			SyncedFailedApps: []db.EnvApp{},
+			UnsyncedApps:     []db.EnvApp{},
+			ExpectedGauges: []Gauge{
+				{Name: "git_sync_unsynced", Value: 0, Tags: []string{}, Rate: 1},
+				{Name: "git_sync_failed", Value: 0, Tags: []string{}, Rate: 1},
+			},
+		},
+		{
+			Name: "Some sync failed apps",
+			SyncedFailedApps: []db.EnvApp{
+				{EnvName: "dev", AppName: "app"},
+				{EnvName: "dev", AppName: "app2"},
+			},
+			UnsyncedApps: []db.EnvApp{
+				{EnvName: "staging", AppName: "app"},
+			},
+			ExpectedGauges: []Gauge{
+				{Name: "git_sync_unsynced", Value: 1, Tags: []string{}, Rate: 1},
+				{Name: "git_sync_failed", Value: 2, Tags: []string{}, Rate: 1},
+			},
+		},
+	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			mockClient := &MockClient{}
+			var ddMetrics statsd.ClientInterface = mockClient
+			ctx := testutil.MakeTestContext()
+			repo := SetupRepositoryTestWithDB(t)
+			dbHandler := repo.State().DBHandler
+
+			err := dbHandler.WithTransaction(ctx, false, func(ctx context.Context, transaction *sql.Tx) error {
+				err := dbHandler.DBWriteNewSyncEventBulk(ctx, transaction, 0, tc.SyncedFailedApps, db.SYNC_FAILED)
+				if err != nil {
+					return err
+				}
+
+				err = dbHandler.DBWriteNewSyncEventBulk(ctx, transaction, 0, tc.UnsyncedApps, db.UNSYNCED)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			})
+			if err != nil {
+				t.Fatalf("failed to write sync events to db: %v", err)
+			}
+
+			err = measureGitSyncStatus(ctx, ddMetrics, dbHandler)
+			if err != nil {
+				t.Fatalf("failed to send git sync status metrics: %v", err)
+			}
+
+			cmpGauge := func(i, j Gauge) bool {
+				if len(i.Tags) == 0 && len(j.Tags) == 0 {
+					return i.Name > j.Name
+				} else if len(i.Tags) != len(j.Tags) {
+					return len(i.Tags) > len(j.Tags)
+				} else {
+					for tagIndex := range i.Tags {
+						if i.Tags[tagIndex] != j.Tags[tagIndex] {
+							return i.Tags[tagIndex] > j.Tags[tagIndex]
+						}
+					}
+					return true
+				}
+			}
+			if diff := cmp.Diff(tc.ExpectedGauges, mockClient.gauges, cmpopts.SortSlices(cmpGauge)); diff != "" {
+				t.Errorf("gauges mismatch (-want, +got):\n%s", diff)
 			}
 		})
 	}
