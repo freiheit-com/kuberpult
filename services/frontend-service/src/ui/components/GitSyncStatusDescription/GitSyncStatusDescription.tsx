54940aae7983d0aa0c4e558c25f3a7efbc22c5b3
Miguel Crespo <miguel.crespo@freiheit.com>
2025-02-27 10:18:31 +0000

    Implementation

--- services/frontend-service/src/ui/components/GitSyncStatusDescription/GitSyncStatusDescription.tsx
+++ services/frontend-service/src/ui/components/GitSyncStatusDescription/GitSyncStatusDescription.tsx
@@ -16,19 +16,44 @@ Copyright freiheit.com*/
 
 import React from 'react';
 import { GitSyncStatus } from '../../../api/api';
+import { Tooltip } from '../tooltip/tooltip';
 
+const GIT_SYNC_STATUS_SYNCED_DESCRIPTION =
+    'All changes to this app and environment have been processed and pushed to the manifest repository successfully.';
+
+const GIT_SYNC_STATUS_UNSYNCED_DESCRIPTION =
+    'There are some changes to this app and environment that have been processed by Kuberpult, but are yet to be committed to the manifest repository.';
+const GIT_SYNC_STATUS_FAILED_DESCRIPTION =
+    "There are some changes to this app and environment that have been processed by Kuberpult, but it wasn't possible to push the changes to the manifest repository\nThis requires manual intervention.";
+const GIT_SYNC_STATUS_UNKOWN_DESCRIPTION = 'Kuberpult could not find the git sync status for this app and environment';
 export const GitSyncStatusDescription: React.FC<{ status: number | undefined }> = (props) => {
     const { status } = props;
+    let span = <span className="rollout__description_unknown">? Unknown</span>;
+    let tooltipContent = GIT_SYNC_STATUS_UNKOWN_DESCRIPTION;
     if (status === undefined) {
-        return <span className="rollout__description_unknown">? Unknown</span>;
+        return (
+            <Tooltip tooltipContent={<div className="mdc-tooltip__title_ release__details">{tooltipContent}</div>}>
+                {span}
+            </Tooltip>
+        );
     }
     switch (status) {
         case GitSyncStatus.GIT_SYNC_STATUS_SYNCED:
-            return <span className="rollout__description_successful">✓ Done</span>;
+            span = <span className="rollout__description_successful">✓ Done</span>;
+            tooltipContent = GIT_SYNC_STATUS_SYNCED_DESCRIPTION;
+            break;
         case GitSyncStatus.GIT_SYNC_STATUS_UNSYNCED:
-            return <span className="rollout__description_progressing">↻ In progress</span>;
+            span = <span className="rollout__description_progressing">↻ In progress</span>;
+            tooltipContent = GIT_SYNC_STATUS_UNSYNCED_DESCRIPTION;
+            break;
         case GitSyncStatus.GIT_SYNC_STATUS_ERROR:
-            return <span className="rollout__description_error">! Failed</span>;
+            span = <span className="rollout__description_error">! Failed</span>;
+            tooltipContent = GIT_SYNC_STATUS_FAILED_DESCRIPTION;
+            break;
     }
-    return <span className="rollout__description_unknown">? Unknown</span>;
+    return (
+        <Tooltip tooltipContent={<div className="mdc-tooltip__title_ release__details">{tooltipContent}</div>}>
+            {span}
+        </Tooltip>
+    );
 };
--- services/frontend-service/src/ui/components/RolloutStatusDescription/RolloutStatusDescription.tsx
+++ services/frontend-service/src/ui/components/RolloutStatusDescription/RolloutStatusDescription.tsx
@@ -14,20 +14,50 @@ along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>
 
 Copyright freiheit.com*/
 import { RolloutStatus } from '../../../api/api';
+import React from 'react';
+import { Tooltip } from '../tooltip/tooltip';
+
+const ROLLOUT_STATUS_UNKNOWN_DESCRIPTION =
+    "ArgoCD hasn't reported any information about this application on this environment.";
+
+const ROLLOUT_STATUS_SUCCESFUL_DESCRIPTION = 'ArgoCD has successfully synced this application this environment.';
+const ROLLOUT_STATUS_PROGRESSING_DESCRIPTION =
+    'ArgoCD has picked up these changes for this application on this environment, but has not applied them yet. This process might take a while.';
+const ROLLOUT_STATUS_PENDING_DESCRIPTION = 'ArgoCD has not yet picked up these changes.';
+const ROLLOUT_STATUS_ERROR_DESCRIPTION = 'ArgoCD has applied these changes, but some error has occurred';
+const ROLLOUT_STATUS_UNHEALTHY_DESCRIPTION =
+    'ArgoCD applied the changes successfully, but the application is unhealthy';
 
 export const RolloutStatusDescription: React.FC<{ status: RolloutStatus }> = (props) => {
     const { status } = props;
+
+    let span = <span className="rollout__description_unknown">? Unknown</span>;
+    let tooltipContent = ROLLOUT_STATUS_UNKNOWN_DESCRIPTION;
     switch (status) {
         case RolloutStatus.ROLLOUT_STATUS_SUCCESFUL:
-            return <span className="rollout__description_successful">✓ Done</span>;
+            span = <span className="rollout__description_successful">✓ Done</span>;
+            tooltipContent = ROLLOUT_STATUS_SUCCESFUL_DESCRIPTION;
+            break;
         case RolloutStatus.ROLLOUT_STATUS_PROGRESSING:
-            return <span className="rollout__description_progressing">↻ In progress</span>;
+            span = <span className="rollout__description_progressing">↻ In progress</span>;
+            tooltipContent = ROLLOUT_STATUS_PROGRESSING_DESCRIPTION;
+            break;
         case RolloutStatus.ROLLOUT_STATUS_PENDING:
-            return <span className="rollout__description_pending">⧖ Pending</span>;
+            span = <span className="rollout__description_pending">⧖ Pending</span>;
+            tooltipContent = ROLLOUT_STATUS_PENDING_DESCRIPTION;
+            break;
         case RolloutStatus.ROLLOUT_STATUS_ERROR:
-            return <span className="rollout__description_error">! Failed</span>;
+            span = <span className="rollout__description_error">! Failed</span>;
+            tooltipContent = ROLLOUT_STATUS_ERROR_DESCRIPTION;
+            break;
         case RolloutStatus.ROLLOUT_STATUS_UNHEALTHY:
-            return <span className="rollout__description_unhealthy">⚠ Unhealthy</span>;
+            span = <span className="rollout__description_unhealthy">⚠ Unhealthy</span>;
+            tooltipContent = ROLLOUT_STATUS_UNHEALTHY_DESCRIPTION;
+            break;
     }
-    return <span className="rollout__description_unknown">? Unknown</span>;
+    return (
+        <Tooltip tooltipContent={<div className="mdc-tooltip__title_ release__details">{tooltipContent}</div>}>
+            {span}
+        </Tooltip>
+    );
 };
