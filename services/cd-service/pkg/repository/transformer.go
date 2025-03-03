307dbc25768cf070b07681c5b25240d93276fa02
Amin Salarkia <amin.salarkia@freiheit.com>
2025-03-03 16:01:43 +0100

    feat(cd-service): delete deprecated metrics
    Ref: SRX-EOXNZ3

--- services/cd-service/pkg/repository/transformer.go
+++ services/cd-service/pkg/repository/transformer.go
@@ -101,12 +101,6 @@ func GaugeEnvLockMetric(ctx context.Context, s *State, transaction *sql.Tx, env 
 				Warnf("Error when trying to get the number of environment locks: %w\n", err)
 			return
 		}
-		err = ddMetrics.Gauge("env_lock_count", count, []string{"env:" + env}, 1)
-		if err != nil {
-			logger.FromContext(ctx).
-				Sugar().
-				Warnf("Error when trying to send `env_lock_count` metric to datadog: %w\n", err)
-		}
 		err = ddMetrics.Gauge("environment_lock_count", count, []string{"kuberpult_environment:" + env}, 1)
 		if err != nil {
 			logger.FromContext(ctx).
@@ -117,13 +111,7 @@ func GaugeEnvLockMetric(ctx context.Context, s *State, transaction *sql.Tx, env 
 }
 func GaugeEnvAppLockMetric(ctx context.Context, appEnvLocksCount int, env, app string) {
 	if ddMetrics != nil {
-		err := ddMetrics.Gauge("app_lock_count", float64(appEnvLocksCount), []string{"app:" + app, "env:" + env}, 1)
-		if err != nil {
-			logger.FromContext(ctx).
-				Sugar().
-				Warnf("Error when trying to send `app_lock_count` metric to datadog: %w\n", err)
-		}
-		err = ddMetrics.Gauge("application_lock_count", float64(appEnvLocksCount), []string{"kuberpult_environment:" + env, "kuberpult_application:" + app}, 1)
+		err := ddMetrics.Gauge("application_lock_count", float64(appEnvLocksCount), []string{"kuberpult_environment:" + env, "kuberpult_application:" + app}, 1)
 		if err != nil {
 			logger.FromContext(ctx).
 				Sugar().
