1882868d74a78d3a80c9efc097fcec4d375622ac
Amin Salarkia <amin.salarkia@freiheit.com>
2025-03-03 17:58:26 +0100

    fix(db): fix integration tests
    Ref: SRX-6XL2ML

--- database/migrations/postgres/1720014158596691_fk_commit_events.up.sql
+++ database/migrations/postgres/1720014158596691_fk_commit_events.up.sql
@@ -3,7 +3,10 @@ DO $$ BEGIN IF NOT EXISTS (
     FROM information_schema.table_constraints
     WHERE table_name = 'commit_events'
         AND constraint_name = 'fk_commit_events_transformer_id'
-) THEN
+) AND EXISTS (SELECT 1 
+               FROM information_schema.columns 
+               WHERE table_name = 'event_sourcing_light' 
+                 AND column_name = 'eslId') THEN
 ALTER TABLE commit_events
 ADD COLUMN IF NOT EXISTS transformerEslId INTEGER CONSTRAINT fk_commit_events_transformer_id REFERENCES event_sourcing_light(eslId) default 0;
 end if;
