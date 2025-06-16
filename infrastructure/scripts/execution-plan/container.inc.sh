4e25ccb5e20cbd0148b94da037a1de304bee6e84
Eduardo Espadeiro <eduardo.espadeiro@freiheit.com>
2025-06-16 14:33:23 +0100

    test changing execplan image

--- infrastructure/scripts/execution-plan/container.inc.sh
+++ infrastructure/scripts/execution-plan/container.inc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-BUILDER_IMAGE=eu.gcr.io/freiheit-core/images/execution-plan:1.0-scratch-NG-3
+BUILDER_IMAGE=europe-docker.pkg.dev/fdc-standard-setup-dev-env/all-artifacts/tools/execplan:git4c7bcb1db3a4b623b7555a077d59aee55fff853a
