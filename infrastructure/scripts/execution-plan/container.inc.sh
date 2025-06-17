4151b4d6dbaf574e2a824db03d3269c4d31f4725
Sven Urbanski <sven.urbanski@freiheit.com>
2025-06-17 11:04:55 +0200

    try other execplanner image

--- infrastructure/scripts/execution-plan/container.inc.sh
+++ infrastructure/scripts/execution-plan/container.inc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-BUILDER_IMAGE=europe-docker.pkg.dev/fdc-standard-setup-dev-env/all-artifacts/tools/execplan:git4c7bcb1db3a4b623b7555a077d59aee55fff853a
+BUILDER_IMAGE=europe-docker.pkg.dev/fdc-standard-setup-dev-env/all-artifacts/tools/execplan:git2547386ba6c019ddd24553eb484aadf7ba7f5032
+
