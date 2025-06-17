f42181e7c1772b156774200d87b6932254ea3f80
Sven Urbanski <sven.urbanski@freiheit.com>
2025-06-17 11:38:55 +0200

    new container for execplan

--- infrastructure/scripts/execution-plan/container.inc.sh
+++ infrastructure/scripts/execution-plan/container.inc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-BUILDER_IMAGE=eu.gcr.io/freiheit-core/images/execution-plan:1.0-scratch-NG-3
+BUILDER_IMAGE=europe-docker.pkg.dev/fdc-standard-setup-dev-env/all-artifacts/tools/execplan:gitfe7ebb523ca7cc72a482491ce32e33dc51097dff
