f7a8533e2efacd4ebf22f5bd239f85bbbc79c79a
Miguel Crespo <miguel.crespo@freiheit.com>
2025-07-09 13:51:41 +0100

    Fix conflict

--- database/migrations/postgres/1751468673639069_revision.up.sql
+++ database/migrations/postgres/1751468673639069_revision.up.sql
@@ -2,7 +2,7 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'releases' AND column_name='revision') THEN
     ALTER TABLE IF EXISTS releases ADD COLUMN revision int NOT NULL DEFAULT 0;
-    ALTER TABLE releases DROP CONSTRAINT releases_pkey;
+    ALTER TABLE releases DROP CONSTRAINT releases_pkey1;
     ALTER TABLE releases ADD PRIMARY KEY(releaseVersion, appName, revision);
 END IF;
 END $$;
@@ -11,8 +11,8 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'releases_history' AND column_name='revision') THEN
     ALTER TABLE IF EXISTS releases_history ADD COLUMN revision int NOT NULL DEFAULT 0;
-    ALTER TABLE releases_history DROP CONSTRAINT releases_history_pkey;
-    ALTER TABLE releases_history ADD PRIMARY KEY (eslVersion, eslVersion, releaseVersion, revision);
+    ALTER TABLE releases_history DROP CONSTRAINT releases_pkey;
+    ALTER TABLE releases_history ADD PRIMARY KEY (version, releaseVersion, revision);
 END IF;
 END $$;
 
