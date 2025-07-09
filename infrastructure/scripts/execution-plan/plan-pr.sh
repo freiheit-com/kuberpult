b5e80aae387cfed91e6950d43350851af3df8e15
Sven Urbanski <sven.urbanski@freiheit.com>
2025-07-09 15:08:28 +0200

    cleanup2

--- infrastructure/scripts/execution-plan/plan-pr.sh
+++ infrastructure/scripts/execution-plan/plan-pr.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -ueo pipefail
-set -x
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 # shellcheck disable=SC1091
 source "${script_dir}"/container.inc.sh
-
-make -C infrastructure/tools/execplan exec-plan-compile
 
 main_branch="main"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
@@ -13,4 +10,4 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 base="${1:-$(git merge-base "${main_branch}" "${current_branch}")}"
 head="${2:-$(git rev-parse HEAD)}"
 
-git diff --diff-filter=ACMRDT --name-only "$base" "$head" #| infrastructure/tools/execplan/plan pr
+git diff --diff-filter=ACMRDT --name-only "$base" "$head" | docker run -i -v "$(pwd)":/repo "${BUILDER_IMAGE}" pr
