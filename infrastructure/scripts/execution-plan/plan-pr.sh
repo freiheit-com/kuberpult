7765df1c1004ef7d57f5f95ac381dcde2c5ec9d6
Sven Urbanski <sven.urbanski@freiheit.com>
2025-07-16 17:29:12 +0200

    origin/main instead of main

--- infrastructure/scripts/execution-plan/plan-pr.sh
+++ infrastructure/scripts/execution-plan/plan-pr.sh
@@ -6,7 +6,7 @@ cd "${script_dir}"
 # shellcheck disable=SC1091
 source "${script_dir}"/container.inc.sh
 
-main_branch="main"
+main_branch="origin/main"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 # When called locally with Makefile, the default parameters are used
 base="${2:-$(git merge-base "${main_branch}" "${current_branch}")}"
