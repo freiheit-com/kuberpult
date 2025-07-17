46700984e0045708a2ccc51bfe716ccd31c7198f
Sven Urbanski <sven.urbanski@freiheit.com>
2025-07-17 10:08:02 +0200

    release

--- infrastructure/make/go/include.mk
+++ infrastructure/make/go/include.mk
@@ -55,7 +55,7 @@ release:
 	test -n "$(MAIN_PATH)" || exit 0; docker push $(IMAGE_NAME)
 	test -n "$(MAIN_PATH)" || exit 0; docker tag $(IMAGE_NAME) $(MAIN_IMAGE_NAME); docker push $(MAIN_IMAGE_NAME)
 
-trivy-scan: #release
+trivy-scan: release
 	SERVICE=$(SERVICE) $(MAKE) -C $(ROOT_DIR)/trivy scan-service-pr
 
 .PHONY: datadog-wrapper
