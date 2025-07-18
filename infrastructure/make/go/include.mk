d0c8b9c421a6836ab9ca83226e9c49d8d28640de
Amin Salarkia <amin.salarkia@freiheit.com>
2025-07-18 10:05:01 +0200

    fix(docker): remove deps image
    Ref: SRX-PRMZYC

--- infrastructure/make/go/include.mk
+++ infrastructure/make/go/include.mk
@@ -19,32 +19,32 @@ MAKEFLAGS += --no-builtin-rules
 .PHONY: deps
 deps:
 ifeq ($(SKIP_DEPS),0)
-	@echo "docker build deps image"
-	IMAGE_TAG=latest $(MAKE) -C $(ROOT_DIR)/infrastructure/docker/deps build
+	@echo "docker build builder image"
+	IMAGE_TAG=latest $(MAKE) -C $(ROOT_DIR)/infrastructure/docker/builder build
 else
 	@echo "Skipping docker build for deps image"
 endif
 
 .PHONY: compile
 compile: deps
-	docker run -w $(SERVICE_DIR) --rm  -v ".:$(SERVICE_DIR)" $(DEPS_IMAGE) sh -c 'test -n "$(MAIN_PATH)" || exit 0; cd $(MAIN_PATH) && CGO_ENABLED=$(CGO_ENABLED) GOOS=linux go build -o bin/main . && cd ../.. && if [ "$(CGO_ENABLED)" = "1" ]; then ldd $(MAIN_PATH)/bin/main | tr -s [:blank:] '\n' | grep ^/ | xargs -I % install -D % $(MAIN_PATH)/%; fi'
+	docker run -w $(SERVICE_DIR) --rm  -v ".:$(SERVICE_DIR)" $(BUILDER_IMAGE) sh -c 'test -n "$(MAIN_PATH)" || exit 0; cd $(MAIN_PATH) && CGO_ENABLED=$(CGO_ENABLED) GOOS=linux go build -o bin/main . && cd ../.. && if [ "$(CGO_ENABLED)" = "1" ]; then ldd $(MAIN_PATH)/bin/main | tr -s [:blank:] '\n' | grep ^/ | xargs -I % install -D % $(MAIN_PATH)/%; fi'
 
 .PHONY: unit-test
 unit-test: deps
 	docker compose -f $(ROOT_DIR)/docker-compose-unittest.yml up -d
-	docker run --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" $(DEPS_IMAGE) sh -c "go test $(GO_TEST_ARGS) ./... -coverprofile=coverage.out && go tool cover -html=coverage.out -o coverage.html"
+	docker run --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" $(BUILDER_IMAGE) sh -c "go test $(GO_TEST_ARGS) ./... -coverprofile=coverage.out && go tool cover -html=coverage.out -o coverage.html"
 	$(ROOT_DIR)/infrastructure/coverage/check-coverage-go.sh coverage.out $(MIN_COVERAGE) $(SERVICE)
 	docker compose -f $(ROOT_DIR)/docker-compose-unittest.yml down
 
 .PHONY: bench-test
 bench-test: deps
 	docker compose -f $(ROOT_DIR)/docker-compose-unittest.yml up -d
-	docker run --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" $(DEPS_IMAGE) sh -c "go test $(GO_TEST_ARGS) -bench=. ./..."
+	docker run --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" $(BUILDER_IMAGE) sh -c "go test $(GO_TEST_ARGS) -bench=. ./..."
 	docker compose -f $(ROOT_DIR)/docker-compose-unittest.yml down
 
 .PHONY: lint
 lint: deps
-	docker run --rm -w $(SERVICE_DIR) -v ".:$(SERVICE_DIR)" $(DEPS_IMAGE) sh -c 'golangci-lint run --timeout=15m -j4 --tests=false --skip-files=".*\.pb\.go$$" ./... || $(SKIP_LINT_ERRORS) && exhaustruct -test=false $$(go list ./... | grep -v "github.com/freiheit-com/kuberpult/pkg/api" | grep -v "github.com/freiheit-com/kuberpult/pkg/publicapi")'
+	docker run --rm -w $(SERVICE_DIR) -v ".:$(SERVICE_DIR)" $(BUILDER_IMAGE) sh -c 'golangci-lint run --timeout=15m -j4 --tests=false --skip-files=".*\.pb\.go$$" ./... || $(SKIP_LINT_ERRORS) && exhaustruct -test=false $$(go list ./... | grep -v "github.com/freiheit-com/kuberpult/pkg/api" | grep -v "github.com/freiheit-com/kuberpult/pkg/publicapi")'
 
 .PHONY: docker
 docker: compile
