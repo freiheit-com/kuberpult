5d2e0ba54a45cf6b41914f3feda93b25d1700d39
Bjoern Michaelsen <bjoern.michaelsen@freiheit.com>
2025-07-28 10:47:29 +0200

    fix: rebase fixes

--- pkg/db/db_deployments.go
+++ pkg/db/db_deployments.go
@@ -569,18 +569,15 @@ func (h *DBHandler) DBUpdateOrCreateDeployment(ctx context.Context, tx *sql.Tx, 
 	return nil
 }
 
-func (h *DBHandler) DBWriteDeploymentAttempt(ctx context.Context, tx *sql.Tx, envName types.EnvName, appName string, version *uint64) error {
+func (h *DBHandler) DBWriteDeploymentAttempt(ctx context.Context, tx *sql.Tx, envName types.EnvName, appName string, version types.ReleaseNumbers) error {
 	span, ctx := tracer.StartSpanFromContext(ctx, "DBWriteDeploymentAttempt")
 	defer span.Finish()
 
 	return h.dbWriteDeploymentAttemptInternal(ctx, tx, &QueuedDeployment{
-		Created: time.Time{},
-		Env:     envName,
-		App:     appName,
-		ReleaseNumbers: types.ReleaseNumbers{
-			Version:  version,
-			Revision: 0,
-		},
+		Created:        time.Time{},
+		Env:            envName,
+		App:            appName,
+		ReleaseNumbers: version,
 	})
 }
 
