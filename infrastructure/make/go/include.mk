fd9d166409f715198c34226f2a505380063d7f57
Miguel Crespo <miguel.crespo@freiheit.com>
2025-07-30 16:18:32 +0100

    unit tests

--- infrastructure/make/go/include.mk
+++ infrastructure/make/go/include.mk
@@ -25,7 +25,7 @@ compile:
 
 .PHONY: unit-test
 unit-test:
-	docker run --user "$(shell id -u):$(shell id -g)" --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" -v $(MIGRATION_VOLUME) $(PKG_VOLUME) $(BUILDER_IMAGE) sh -c "cd $(ROOT_DIR)/pkg && buf generate && go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest && oapi-codegen -generate "std-http-server" -o publicapi/server-gen.go -package publicapi publicapi/api.yaml"
+	docker run --user "$(shell id -u):$(shell id -g)" --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" -v $(MIGRATION_VOLUME) $(PKG_VOLUME) $(BUILDER_IMAGE) sh -c "cd $(ROOT_DIR)/pkg && oapi-codegen -generate "std-http-server" -o publicapi/server-gen.go -package publicapi publicapi/api.yaml"
 	docker compose -f $(ROOT_DIR)/docker-compose-unittest.yml up -d
 	docker run --rm -w $(SERVICE_DIR) --network host -v ".:$(SERVICE_DIR)" -v $(MIGRATION_VOLUME) $(PKG_VOLUME) $(BUILDER_IMAGE) sh -c "go test $(GO_TEST_ARGS) ./... -coverprofile=coverage.out && go tool cover -html=coverage.out -o coverage.html"
 	$(ROOT_DIR)/infrastructure/coverage/check-coverage-go.sh coverage.out $(MIN_COVERAGE) $(SERVICE)
