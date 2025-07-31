fb9c09809e57bf5a3665c72910394c01b587c98d
Sven Urbanski <sven.urbanski@freiheit.com>
2025-07-31 14:42:50 +0200

    fix(ci): simplify make targets

--- infrastructure/make/go/include.mk
+++ infrastructure/make/go/include.mk
@@ -81,9 +81,17 @@ gen-pkg:
 
 test: gen-pkg unit-test
 
-build-pr: IMAGE_TAG=pr-$(VERSION)
-build-pr: BUILDER_IMAGE=$(DOCKER_REGISTRY_URI)/infrastructure/docker/builder:pr-$(VERSION)
-build-pr: gen-pkg lint unit-test bench-test docker release trivy-scan
+build-pr-internal: gen-pkg lint unit-test bench-test docker release trivy-scan
 
-build-main: IMAGE_TAG=main-$(VERSION)
-build-main: gen-pkg lint unit-test bench-test docker release-main trivy-scan
+build-pr:
+	IMAGE_TAG=pr-$(VERSION) BUILDER_IMAGE=$(DOCKER_REGISTRY_URI)/infrastructure/docker/builder:pr-$(VERSION) $(MAKE) build-pr-internal
+
+#build-pr: IMAGE_TAG=pr-$(VERSION)
+#build-pr: BUILDER_IMAGE=$(DOCKER_REGISTRY_URI)/infrastructure/docker/builder:pr-$(VERSION)
+#build-pr: gen-pkg lint unit-test bench-test docker release trivy-scan
+
+build-main-internal:
+	IMAGE_TAG=main-$(VERSION) $(MAKE) gen-pkg lint unit-test bench-test docker release-main trivy-scan
+
+#build-main: IMAGE_TAG=main-$(VERSION)
+build-main: build-main-internal
