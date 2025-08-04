72fc3b17eb9105201bf294d4bbcd87202b67add8
Miguel Crespo <miguel.crespo@freiheit.com>
2025-08-04 15:07:36 +0100

    re-instante previous code

--- services/manifest-repo-export-service/pkg/repository/transformer.go
+++ services/manifest-repo-export-service/pkg/repository/transformer.go
@@ -1375,8 +1375,7 @@ func (c *CleanupOldApplicationVersions) Transform(
 	msg := ""
 	for _, oldRelease := range oldVersions {
 		// delete oldRelease:
-		releasesDir := releasesDirectoryWithVersion(fs, c.Application, oldRelease)
-		_, err := fs.Stat(releasesDir)
+		releasesDir, err := state.checkWhichVersionDirectoryExists(fs, c.Application, oldRelease)
 		if err != nil {
 			return "", wrapFileError(err, releasesDir, "CleanupOldApplicationVersions: could not stat")
 		}
