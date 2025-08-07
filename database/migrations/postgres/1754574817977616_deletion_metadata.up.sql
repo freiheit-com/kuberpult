4ec4f194228b7042745767ffc1744ffaf05e28ca
Miguel Crespo <miguel.crespo@freiheit.com>
2025-08-07 16:22:43 +0100

    deletion metadata name

--- database/migrations/postgres/1754574817977616_deletion_metadata.up.sql
+++ database/migrations/postgres/1754574817977616_deletion_metadata.up.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'environment_locks_history' AND column_name='deletion_metadata') THEN
-    ALTER TABLE IF EXISTS environment_locks_history ADD COLUMN deletion_metadata varchar;
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'environment_locks_history' AND column_name='deletionMetadata') THEN
+    ALTER TABLE IF EXISTS environment_locks_history ADD COLUMN deletionMetadata varchar;
 END IF;
 END $$;
