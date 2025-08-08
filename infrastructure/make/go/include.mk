3abf6b5a6084555a17ee11a0a96c7262b32a7ad8
Sven Urbanski <sven.urbanski@freiheit.com>
2025-08-08 10:08:10 +0200

    fix(ci): simplify make targets (#2571)
    Ref: SRX-O4HMIP

--- infrastructure/make/go/include.mk
+++ infrastructure/make/go/include.mk
@@ -85,9 +85,12 @@ gen-pkg:
 
 test: gen-pkg unit-test
 
-build-pr: IMAGE_TAG=pr-$(VERSION)
-build-pr: BUILDER_IMAGE=$(DOCKER_REGISTRY_URI)/infrastructure/docker/builder:pr-$(VERSION)
-build-pr: gen-pkg lint unit-test bench-test docker release trivy-scan
+build-pr-internal: gen-pkg lint unit-test bench-test docker release trivy-scan
 
-build-main: IMAGE_TAG=main-$(VERSION)
-build-main: gen-pkg lint unit-test bench-test docker release-main trivy-scan
+build-pr:
+	IMAGE_TAG=pr-$(VERSION) BUILDER_IMAGE=$(DOCKER_REGISTRY_URI)/infrastructure/docker/builder:pr-$(VERSION) $(MAKE) build-pr-internal
+
+build-main-internal: gen-pkg lint unit-test bench-test docker release-main trivy-scan
+
+build-main:
+	IMAGE_TAG=main-$(VERSION) $(MAKE) build-main-internal
