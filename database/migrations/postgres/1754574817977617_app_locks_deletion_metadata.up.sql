1bc4c39a588f43472cd1f89ebd9576156147ab73
Miguel Crespo <miguel.crespo@freiheit.com>
2025-08-08 10:17:52 +0100

    adding default value

--- database/migrations/postgres/1754574817977617_app_locks_deletion_metadata.up.sql
+++ database/migrations/postgres/1754574817977617_app_locks_deletion_metadata.up.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'app_locks_history' AND column_name='deletionmetadata') THEN
-        ALTER TABLE IF EXISTS app_locks_history ADD COLUMN deletionmetadata varchar;
+        ALTER TABLE IF EXISTS app_locks_history ADD COLUMN deletionmetadata varchar DEFAULT '{}';
     END IF;
 END $$;
