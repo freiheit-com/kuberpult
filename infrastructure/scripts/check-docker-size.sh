03abd63b473ac351d0713eadf5359fdb60d9715b
Sven Urbanski <sven.urbanski@freiheit.com>
2025-08-26 17:27:43 +0200

    fix(ci): measure docker size more accurately
    Ref: SRX-Q21BDN

--- infrastructure/scripts/check-docker-size.sh
+++ infrastructure/scripts/check-docker-size.sh
@@ -2,14 +2,15 @@
 set -euo pipefail
 
 if (( $# < 3 )); then
-    echo "usage: $0 <docker-image> <SIZE_THRESHOLD_MB> <PRODUCT_NAME>" >&2
+    echo "usage: $0 <docker-image-without-tag> <SIZE_THRESHOLD_MB> <PRODUCT_NAME>" >&2
     exit 2
 fi
 
 DOCKER_IMAGE=$1
 SIZE_THRESHOLD_MB=$2
 PRODUCT_NAME=$3
-DISCREPANCY_MB=10
+# Measure the docker image size is rather unprecise, so we allow a big discrepancy for now:
+DISCREPANCY_MB=100
 
 # fail if the threshold is not a number with 1-2 digits before the point and up to 5 after the point:
 re='^[0-9]{1,3}(.[0-9]{1,5})?$'
@@ -23,19 +24,15 @@ if [ "$(awk "BEGIN { print ($SIZE_THRESHOLD_MB < 0.0)? 1 : 0 }")" = 1 ]; then
     exit 0;
 fi
 
-ACTUAL_SIZE=$(docker inspect -f '{{ .Size }}' "${DOCKER_IMAGE}")
+ACTUAL_SIZE=$(docker save "${DOCKER_IMAGE}" | gzip -c | wc -c)
 ACTUAL_SIZE_MB=$(awk -v val="${ACTUAL_SIZE}" 'BEGIN { printf "%.0f\n", val / 1000000 }');
 
 if [ "$(awk "BEGIN { print ($ACTUAL_SIZE_MB > $SIZE_THRESHOLD_MB) ? 1 : 0 }")" = 1 ]; then
     echo "Docker size too high ${ACTUAL_SIZE_MB}MB>${SIZE_THRESHOLD_MB}MB in $PRODUCT_NAME image ${DOCKER_IMAGE}";
-    echo "Running docker history:"
-    docker history "${DOCKER_IMAGE}"
     exit 1;
 else
     if [ "$(awk "BEGIN { print ($ACTUAL_SIZE_MB < $SIZE_THRESHOLD_MB - $DISCREPANCY_MB)? 1 : 0 }")" = 1 ]; then
         echo "The current docker image size ${ACTUAL_SIZE_MB}MB is more than ${DISCREPANCY_MB}MB smaller than the threshold of ${SIZE_THRESHOLD_MB}MB. Please adjust the threshold of ${PRODUCT_NAME} accordingly!"
-        echo "Running docker history:"
-        docker history "${DOCKER_IMAGE}"
         exit 1;
     fi
 
