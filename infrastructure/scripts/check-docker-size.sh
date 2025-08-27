9ef11519e7c354c729f66493f4bb250562678db6
Bjoern Michaelsen <bjoern.michaelsen@freiheit.com>
2025-08-27 18:26:40 +0200

    fix: make check docker size more readable ...
    ... by doing more arithmetic in awk.
    
    Also make the logic in the awk script easier testable like:
    
    > echo "10000000 10 10" | ./infrastructure/scripts/check-docker-size.awk
    
    Ref: SRX-Q21BDN

--- infrastructure/scripts/check-docker-size.sh
+++ infrastructure/scripts/check-docker-size.sh
@@ -8,9 +8,9 @@ fi
 
 DOCKER_IMAGE=$1
 SIZE_THRESHOLD_MB=$2
-PRODUCT_NAME=$3
 # Measure the docker image size is rather unprecise, so we allow a discrepancy in percent (both above and below):
 DISCREPANCY_PERCENT=10
+SCRIPT_DIR=$(dirname "$0")
 
 # fail if the threshold is not a number with 1-2 digits before the point and up to 5 after the point:
 re='^[0-9]{1,3}(.[0-9]{1,5})?$'
@@ -25,18 +25,5 @@ if [ "$(awk "BEGIN { print ($SIZE_THRESHOLD_MB < 0.0)? 1 : 0 }")" = 1 ]; then
 fi
 
 ACTUAL_SIZE=$(docker image inspect --format='{{.Size}}' "${DOCKER_IMAGE}")
-ACTUAL_SIZE_MB=$(awk -v val="${ACTUAL_SIZE}" 'BEGIN { printf "%.0f\n", val / 1000000 }');
-THRESHOLD_PLUS_BUFFER_MB=$(awk -v val="${SIZE_THRESHOLD_MB}" -v discrepancy="${DISCREPANCY_PERCENT}" 'BEGIN { printf "%.0f\n", val * (1+discrepancy/100) }');
-THRESHOLD_MINUS_BUFFER_MB=$(awk -v val="${SIZE_THRESHOLD_MB}" -v discrepancy="${DISCREPANCY_PERCENT}" 'BEGIN { printf "%.0f\n", val * (1-discrepancy/100) }');
 
-if [ "$(awk "BEGIN { print ($ACTUAL_SIZE_MB > $THRESHOLD_PLUS_BUFFER_MB) ? 1 : 0 }")" = 1 ]; then
-    echo "Image too large: Your docker image has ${ACTUAL_SIZE_MB}MB. The configured threshold is ${SIZE_THRESHOLD_MB}MB. Even with the ${DISCREPANCY_PERCENT}% discrepancy buffer, that is ${THRESHOLD_PLUS_BUFFER_MB}MB.";
-    exit 1;
-else
-    if [ "$(awk "BEGIN { print ($ACTUAL_SIZE_MB < $THRESHOLD_MINUS_BUFFER_MB)? 1 : 0 }")" = 1 ]; then
-        echo "Image too small: Your docker image has ${ACTUAL_SIZE_MB}MB. The configured threshold is ${SIZE_THRESHOLD_MB}MB. Even with the ${DISCREPANCY_PERCENT}% discrepancy buffer, that is ${THRESHOLD_MINUS_BUFFER_MB}MB.";
-        exit 1;
-    fi
-
-    echo "Docker image is sufficiently small: ${ACTUAL_SIZE_MB}MB<=${SIZE_THRESHOLD_MB}MB in \"$PRODUCT_NAME\" image ${DOCKER_IMAGE}";
-fi
+echo "${ACTUAL_SIZE} ${SIZE_THRESHOLD_MB} ${DISCREPANCY_PERCENT}"|"${SCRIPT_DIR}/check-docker-size.awk"
