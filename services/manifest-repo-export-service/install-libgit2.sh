115ba1f3667b74e9b1527fc7833b2822fbbb7e3b
Sven Urbanski <sven.urbanski@freiheit.com>
2025-10-02 14:01:23 +0200

    install libgit without pcre

--- services/manifest-repo-export-service/install-libgit2.sh
+++ services/manifest-repo-export-service/install-libgit2.sh
@@ -8,7 +8,7 @@ git checkout v1.5.0
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DUSE_SSH=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DUSE_SSH=ON -DUSE_REGEX=builtin
 cmake --build . --target install
 
 # Clean up
