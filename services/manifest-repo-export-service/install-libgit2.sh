8139bf7462de37689c876b117a6fd2404fdba40c
Sven Urbanski <sven.urbanski@freiheit.com>
2025-10-07 13:09:03 +0200

    fix(manifest-export): CVE-2025-58050 by removing pcre2 (#2641)
    Previously, pcre2 was actually used by 2 different dependencies:
    git - which was just installed in order to clone libgit2
    libgit2 - which did use pcre2, but this is not a hard requirement.
    
    With this change, the installation of "git" is removed after it's not
    needed anymore.
    And libgit2 is built without pcre2, so that the resulting image of the
    manifest-export does not contain pcre2 anymore.
    
    Ref: SRX-RL35GY

--- services/manifest-repo-export-service/install-libgit2.sh
+++ services/manifest-repo-export-service/install-libgit2.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e 
 
+apk add --no-cache git
+
 # Clone and install libgit2 v1.5.0
 git clone https://github.com/libgit2/libgit2.git
 cd libgit2
 git checkout v1.5.0
 
+# git comes with "pcre2" which currently has a vulnerability, so we remove it again
+apk del git
+
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DUSE_SSH=ON
+
+# USE_REGEX=builtin stops libgit from using pcre:
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DUSE_SSH=ON -DUSE_REGEX=builtin
 cmake --build . --target install
 
 # Clean up
