5478abfc744577e0d10dd6b9cb3be78419abfcca
Bjoern Michaelsen <bjoern.michaelsen@freiheit.com>
2025-11-06 17:56:15 +0100

    fix: sync basket (#2669)
    When a user opens the "Delete Environments" dialog for an application,
    any environments they have already selected for deletion (which are
    stored as pending batch actions) will now be checked by default. This
    improves the user experience by making the dialog's initial state
    consistent with the current batch action list. Just keeping the state
    from the last open dialog would not be sufficient as batch actions can
    be _removed_ from the basket when the dialog is closed.
    
    Ref: SRX-GJ3E9Y

--- services/frontend-service/src/ui/components/dialog/EnvDelDialog.tsx
+++ services/frontend-service/src/ui/components/dialog/EnvDelDialog.tsx
@@ -14,11 +14,11 @@ along with kuberpult. If not, see <https://directory.fsf.org/wiki/License:Expat>
 
 Copyright freiheit.com*/
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../button';
 import { PlainDialog } from './ConfirmationDialog';
 import { Checkbox } from '../dropdown/checkbox';
-import { addAction, deleteAction } from '../../utils/store';
+import { addAction, deleteAction, useDeleteEnvironmentActionsForApp } from '../../utils/store';
 import { BatchAction } from '../../../api/api';
 
 export type EnvDelDialogProps = {
@@ -34,6 +34,13 @@ export type EnvDelDialogProps = {
  */
 export const EnvDelDialog: React.FC<EnvDelDialogProps> = (props) => {
     const [selectedEnvs, setSelectedEnvs] = useState<string[]>([]);
+    const deleteEnvActions = useDeleteEnvironmentActionsForApp(props.app);
+    useEffect(() => {
+        if (props.open) {
+            setSelectedEnvs(deleteEnvActions);
+        }
+        return () => {};
+    }, [props.open]);
     const toggleEnv = React.useCallback(
         (env: string) => {
             const action: BatchAction = {
