2b2756514f408ae538b8f59f9b1aff6286563df7
David Bui <david.bui@freiheit.com>
2026-02-06 15:03:18 +0000

    handle in all main endpoints

--- services/rollout-service/pkg/cmd/server.go
+++ services/rollout-service/pkg/cmd/server.go
@@ -143,6 +143,7 @@ func (config *Config) RevolutionConfig() (revolution.Config, error) {
 func RunServer() {
 	var config Config
 	err := logger.Wrap(context.Background(), func(ctx context.Context) error {
+		defer logger.LogPanics(ctx)
 		err := envconfig.Process("kuberpult", &config)
 		if err != nil {
 			logger.FromContext(ctx).Fatal("config.parse", zap.Error(err))
