46f8616f4b70338da34ad008e136b6c0ee02186c
David Bui <david.bui@freiheit.com>
2026-02-18 12:07:44 +0100

    up

--- database/migrations/postgres/1770815602569279_create_fk_releases_deployments.up.sql
+++ database/migrations/postgres/1770815602569279_create_fk_releases_deployments.up.sql
@@ -1,13 +1,19 @@
-ALTER TABLE deployments
-DROP CONSTRAINT IF EXISTS fk_releases_deployments;
-
-ALTER TABLE deployments
-ADD CONSTRAINT fk_releases_deployments FOREIGN KEY (
-    appname,
-    releaseVersion,
-    revision
-) REFERENCES releases (
-    appname,
-    releaseVersion,
-    revision
-) NOT VALID;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM pg_constraint 
+        WHERE conname = 'fk_releases_deployments'
+    ) THEN
+        ALTER TABLE deployments
+        ADD CONSTRAINT fk_releases_deployments FOREIGN KEY (
+            appname,
+            releaseVersion,
+            revision
+        ) REFERENCES releases (
+            appname,
+            releaseVersion,
+            revision
+        ) NOT VALID;
+    END IF;
+END $$;
--- database/migrations/postgres/1770822324979453_force_non_null_releaseversion.up.sql
+++ database/migrations/postgres/1770822324979453_force_non_null_releaseversion.up.sql
@@ -1,5 +1,11 @@
-ALTER TABLE deployments
-DROP CONSTRAINT IF EXISTS releaseversion_not_null;
-
-ALTER TABLE deployments
-ADD CONSTRAINT releaseversion_not_null CHECK (releaseversion IS NOT NULL) NOT VALID;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM pg_constraint 
+        WHERE conname = 'releaseversion_not_null'
+    ) THEN
+        ALTER TABLE deployments
+        ADD CONSTRAINT releaseversion_not_null CHECK (releaseversion IS NOT NULL) NOT VALID;
+    END IF;
+END $$;
