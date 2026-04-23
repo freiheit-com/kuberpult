902a38f20c50500931bd7bfb751f21affe4cf8be
David Bui <bqcuong@users.noreply.github.com>
2026-04-23 13:57:47 +0200

    fix: fetch correct releases for render environment transformer (#2866)
    Ref: SRX-UGP7S2

--- services/manifest-repo-export-service/pkg/repository/transformer.go
+++ services/manifest-repo-export-service/pkg/repository/transformer.go
@@ -1448,9 +1448,19 @@ func (c *RenderEnvironment) Transform(
 		if err != nil {
 			return "", err
 		}
+		if release == nil || release.Deleted {
+			logging.Info(ctx, "release not found or is deleted for app", zap.Any("appName", appName), zap.Any("releaseNumbers", releaseNumbers), zap.Any("releaseIsNil", release == nil))
+			// skip if there is no release found matching the version numbers for this app
+			// or if the release has been undeployed
+			continue
+		}
+
 		envManifest, ok := release.Manifests.Manifests[c.Environment]
 		if !ok {
-			return "", fmt.Errorf("no manifests found for app '%s' on env '%s'", appName, c.Environment)
+			logging.Info(ctx, "app doesn't have any manifests on this environment", zap.Any("appName", appName), zap.Any("releaseNumbers", releaseNumbers), zap.Any("env", c.Environment))
+			// skip an app if it doesn't have any manifests on this environment
+			// this might happen if the app was already undeployed from the environment at the given timestamp
+			continue
 		}
 
 		appToManifestMap[appName] = envManifest
