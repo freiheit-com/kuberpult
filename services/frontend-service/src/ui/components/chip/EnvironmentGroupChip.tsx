9999d9523dbc1773d59f18de160c3f69bcd54b46
David Bui <david.bui@freiheit.com>
2026-08-14 15:12:24 +0200

    fix: argocd icon not shown when argocdConfigs is empty

--- services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.tsx
+++ services/frontend-service/src/ui/components/chip/EnvironmentGroupChip.tsx
@@ -116,7 +116,7 @@ export const EnvironmentChip = (props: EnvironmentChipProps): JSX.Element => {
     );
     const chip = smallEnvChip ? (
         name[0].toUpperCase()
-    ) : argocdConfigs !== undefined ? (
+    ) : argocdConfigs !== undefined && argocdConfigs.configs.length > 1 ? (
         <ArgoAppMultiEnvLink
             app={app}
             env={name}
