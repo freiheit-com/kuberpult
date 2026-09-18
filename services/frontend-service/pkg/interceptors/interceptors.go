c92545a429296c66d78de767366fbaad164f13f3
Christopher Reinartz <christopher.reinartz@freiheit.com>
2026-09-18 11:08:40 +0200

    fix: do not panic when valid jwts do not include name and email fields

--- services/frontend-service/pkg/interceptors/interceptors.go
+++ services/frontend-service/pkg/interceptors/interceptors.go
@@ -57,10 +57,12 @@ func authorize(ctx context.Context, jwks *keyfunc.JWKS, clientId string, tenantI
 	// here, everything is valid, but we way still have empty strings, so we use the defaultUser here
 	var u *auth.User = nil
 	if _, ok := claims["aud"]; ok && claims["aud"] == clientId {
+		email, _ := claims["email"].(string)
+		name, _ := claims["name"].(string)
 		u = &auth.User{
 			DexAuthContext: nil,
-			Email:          claims["email"].(string),
-			Name:           claims["name"].(string),
+			Email:          email,
+			Name:           name,
 		}
 	}
 
